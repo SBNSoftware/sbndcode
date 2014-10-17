ae689b0e0b96ef07d625639f1a1ab4cac98a08d3
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2014-10-17 08:38:48 -0500

    Allow the user to decide what percent of reflector layer coverage they want in front of TPC walls/cathode (0-100%).

--- lar1ndcode/Geo/gdml/LAr1/GenerateLAr1GDML.sh
+++ lar1ndcode/Geo/gdml/LAr1/GenerateLAr1GDML.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 #Generate geometry without wires
-./generate_gdml.pl -w 0 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
+./generate_gdml.pl -w 0 -t 1 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
 ./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd_nowires.gdml
 
 #Generate geometry with wires
 ./generate_gdml.pl -w 1 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
 ./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd.gdml
-
-#Generate geometry with 16 pmts, behind wires
-#./generate_gdml.pl -w 1 -p16 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
-#./make_gdml.pl -i lar1-gdml-fragments.xml -o lar1nd_16pmts.gdml
 
 #Generate geometry with 32 pmts, behind wires and on sides
 #./generate_gdml.pl -w 1 -p32 -i lar1-gdml-parameters.xml -o lar1-gdml-fragments.xml
