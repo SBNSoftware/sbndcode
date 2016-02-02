cf0fba0270fe28fabcb7d96510c381b3c925430e
Andrew Blake <blake@lar1ndgpvm01.fnal.gov>
2016-02-02 11:04:36 -0600

    Adding SBNDPandora package to lar1ndcode repository

---
+++ lar1ndcode/SBNDPandora/SBNDPseudoLayerPlugin.cxx
@@ -0,0 +1,18 @@
+/**
+ *  @file   lar1ndcode/SBNDPandora/SBNDPseudoLayerPlugin.cxx
+ *
+ *  @brief  Implementation of the SBND pseudo layer Plugin class.
+ *
+ *  $Log: $
+ */
+
+#include "lar1ndcode/SBNDPandora/SBNDPseudoLayerPlugin.h"
+
+namespace lar_pandora
+{
+
+SBNDPseudoLayerPlugin::SBNDPseudoLayerPlugin() : LArPseudoLayerPlugin(0.3f)
+{
+}
+
+} // namespace lar_pandora
