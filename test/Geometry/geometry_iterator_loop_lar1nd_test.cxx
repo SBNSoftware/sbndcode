d7297e7617be5f712489d9367150068483c12a68
Gianluca Petrillo <petrillo@fnal.gov>
2016-06-08 19:05:51 -0500

    Added geometry tests
    A couple of them fail, probably for some geometry problem.
    Need to be investigated, but might as well wait for the new geometry.

---
+++ test/Geometry/geometry_iterator_loop_lar1nd_test.cxx
@@ -0,0 +1,120 @@
+/**
+ * @file   geometry_iterator_loop_lar1nd_test.cxx
+ * @brief  Test for geometry iterator loops on SBND detector
+ * @date   May 11th, 2015
+ * @author petrillo@fnal.gov
+ * 
+ * Usage:
+ *   `geometry_iterator_loop_lar1nd_test [ConfigurationFile [GeometryTestParameterSet]]`
+ * 
+ * By default, an internal configuration is used or GeometryTestParameterSet is
+ * set to "physics.analyzers.geotest".
+ * 
+ */
+
+
+// LArSoft libraries
+#include "test/Geometry/geometry_unit_test_lar1nd.h"
+#include "test/Geometry/GeometryIteratorLoopTestAlg.h"
+#include "larcore/Geometry/GeometryCore.h"
+#include "larcore/Geometry/ChannelMapStandardAlg.h"
+
+// utility libraries
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+
+//------------------------------------------------------------------------------
+//---  The test environment
+//---
+
+// we define here all the configuration that is needed;
+// we use an existing class provided for this purpose, since our test
+// environment allows us to tailor it at run time.
+using LAr1NDGeometryConfiguration
+  = lar1nd::testing::LAr1NDGeometryEnvironmentConfiguration
+    <geo::ChannelMapStandardAlg>;
+
+/*
+ * GeometryTesterFixture, configured with the object above, is used in a
+ * non-Boost-unit-test context.
+ * It provides:
+ * - `geo::GeometryCore const* Geometry()`
+ * - `geo::GeometryCore const* GlobalGeometry()` (static member)
+ */
+using LAr1NDGeometryTestEnvironment
+  = testing::GeometryTesterEnvironment<LAr1NDGeometryConfiguration>;
+
+
+//------------------------------------------------------------------------------
+//---  The tests
+//---
+
+/** ****************************************************************************
+ * @brief Runs the test
+ * @param argc number of arguments in argv
+ * @param argv arguments to the function
+ * @return number of detected errors (0 on success)
+ * @throw cet::exception most of error situations throw
+ * 
+ * The arguments in argv are:
+ * 0. name of the executable ("Geometry_test")
+ * 1. path to the FHiCL configuration file
+ * 2. FHiCL path to the configuration of the geometry test
+ *    (default: physics.analysers.geotest)
+ * 3. FHiCL path to the configuration of the geometry
+ *    (default: services.Geometry)
+ * 
+ */
+//------------------------------------------------------------------------------
+int main(int argc, char const** argv) {
+  
+  LAr1NDGeometryConfiguration config
+    ("geometry_iterator_loop_test_LAr1ND");
+  config.SetMainTesterParameterSetName("geotest");
+  
+  //
+  // parameter parsing
+  //
+  int iParam = 0;
+  
+  // first argument: configuration file (mandatory)
+  if (++iParam < argc) config.SetConfigurationPath(argv[iParam]);
+  
+  // second argument: path of the parameter set for geometry test configuration
+  // (optional; default: "physics.analyzers.geotest");
+  // if no path is provided, we have a empty default configuration;
+  // if path is provided, we don't have any default configuration
+  // and if the configuration is missing there will be an error
+  if (++iParam < argc) config.SetMainTesterParameterSetPath(argv[iParam]);
+  else                 config.AddDefaultTesterConfiguration("");
+  
+  // third argument: path of the parameter set for geometry configuration
+  // (optional; default: "services.Geometry" from the inherited object)
+  if (++iParam < argc) config.SetGeometryParameterSetPath(argv[iParam]);
+  
+  //
+  // testing environment setup
+  //
+  LAr1NDGeometryTestEnvironment TestEnvironment(config);
+  
+  //
+  // run the test algorithm
+  //
+  
+  // 1. we initialize it from the configuration in the environment,
+  geo::GeometryIteratorLoopTestAlg Tester(TestEnvironment.TesterParameters());
+  
+  // 2. we set it up with the geometry from the environment
+  Tester.Setup(*TestEnvironment.Geometry());
+  
+  // 3. then we run it!
+  unsigned int nErrors = Tester.Run();
+  
+  // 4. And finally we cross fingers.
+  if (nErrors > 0) {
+    mf::LogError("geometry_iterator_loop_test_LAr1ND")
+      << nErrors << " errors detected!";
+  }
+  
+  return nErrors;
+} // main()
