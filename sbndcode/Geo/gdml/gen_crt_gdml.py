c0d080886dc5dd6e43b98d67be3289a0764fdcbf
Andrew Mastbaum <mastbaum@uchicago.edu>
2016-11-23 17:48:28 -0600

    reorganize gdml and add bottom tagger
    Add the hand-crafted bottom tagger array, based on Figure 2.7 of the SBN
    CRT tech note (DocDB 685). Also, reorganize the GDML a bit to make the
    volume hierarchy shallower: now taggers have modules as daughters, rather
    than having CRTPlanes which have the modules. This is necessary because
    larcore has a hard-coded maximum volume recursion depth of 8, which is
    exceeded with the old arrangement.

--- sbndcode/Geo/gdml/gen_crt_gdml.py
+++ sbndcode/Geo/gdml/gen_crt_gdml.py
@@ -10,50 +10,49 @@ by LArG4 to keep track of energy depositions. The solids, however, can safely
 be referenced many times, and so are stored only once (using a hash keyed on
 the the linear dimensions).
 
-The default is that the "unit cell" is a module (made of a cover and one
-monolithic chunk of scintillator, where downstream code will figure out which
-strip should be hit). To build the individual strips as part of the geometry,
-set do_strips to True (this will require changes in the aforementioned
-downstream code).
-
 The output of this code is a file "crt.gdml" which contains the GDML snippets
 to paste into the full SBND geometry.
-
-:todo: Add bottom tagger (which is not simply a grid)
-:todo: Make volume names based on something repeatable, not UUIDs
 
 A. Mastbaum <mastbaum@uchicago.edu>, 2016/10/27
 '''
 
 import xml.etree.cElementTree as ET
 from xml.dom import minidom
-import uuid
 from hashlib import md5
 
 # Options
 PAD = 0.4  # Padding between strips and module (Al thickness)
-do_strips = False  # Build individual scintillator strips
 
 ##########
 
-solids = ET.Element('solids')
-structure = ET.Element('structure')
+gdml = ET.Element('gdml')
+solids = ET.SubElement(gdml, 'solids')
+structure = ET.SubElement(gdml, 'structure')
 solids_store = {}
+
+seq_id = 0
+
+def sequential_id():
+    global seq_id
+    s = str(seq_id)
+    seq_id += 1
+    return s
+
 
 def strip(x, y, z=1.0):
     '''Build one scintillator strip.'''
-    x = str(x)
-    y = str(y - 2 * PAD)
-    z = str(z)
+    xx = str(x)
+    yy = str(y - 2 * PAD)
+    zz = str(z)
     sname = 'CRTStrip_' + md5('_'.join((xx, yy, zz))).hexdigest()[:8]
 
     if not sname in solids_store:
-        s = ET.SubElement(solids, 'box', name=sname, lunit="cm", x=x, y=y, z=z)
+        s = ET.SubElement(solids, 'box', name=sname, lunit="cm", x=xx, y=yy, z=zz)
         solids_store[sname] = s
     else:
-        s = solids_store[name]
+        s = solids_store[sname]
 
-    name = 'CRTStrip_' + uuid.uuid4().hex[:8]
+    name = 'CRTStrip_' + sequential_id()
     vname = 'volAuxDetSensitive' + name
     v = ET.SubElement(structure, 'volume', name=vname)
     ET.SubElement(v, 'materialref', ref='Polystyrene')
@@ -75,25 +74,27 @@ def strip_array(x, y, z=1.0, nx=16):
     else:
         s = solids_store[sname]
 
-    if do_strips:
-        for i in range(nx):
-            es, ev = strip(x, y, z)
+    strips = []
+    for i in range(nx):
+        strips.append(strip(x, y, z))
 
-            pv = ET.SubElement(v, 'physvol')
-            ET.SubElement(pv, 'volumeref', ref=ev.attrib['name'])
-
-            dx = 1.0 * (2*i - nx + 1) / 2 * x
-            posname = 'pos' + ev.attrib['name']
-            ET.SubElement(pv, 'position', name=posname,
-                          unit="cm", x=str(dx), y='0', z='0')
-
-    name = 'CRTStripArray_' + uuid.uuid4().hex[:8]
-    vname = 'vol' + '' if do_strips else 'AuxDetSensitive' + name
+    name = 'CRTStripArray_' + sequential_id()
+    vname = 'volAuxDet' + name
     v = ET.SubElement(structure, 'volume', name=vname)
     ET.SubElement(v, 'materialref', ref='Polystyrene')
     ET.SubElement(v, 'solidref', ref=sname)
 
+    for i, (es, ev) in enumerate(strips):
+        pv = ET.SubElement(v, 'physvol')
+        ET.SubElement(pv, 'volumeref', ref=ev.attrib['name'])
+
+        dx = 1.0 * (2*i - nx + 1) / 2 * x
+        posname = 'pos' + ev.attrib['name']
+        ET.SubElement(pv, 'position', name=posname,
+                      unit="cm", x=str(dx), y='0', z='0')
+
     return s, v
+
 
 def module(x, y, z=1.0, nx=16):
     '''Build a module: a strip array and an aluminum cover.'''
@@ -110,13 +111,12 @@ def module(x, y, z=1.0, nx=16):
         s = solids_store[sname]
 
     # Strip array
-    es, ev = strip_array(x, y, z)
+    es, ev = strip_array(x, y, z, nx)
 
     # Cover (module - strip array)
     cover_sname = 'CRTModuleCover_' + md5('_'.join((xx, yy, zz))).hexdigest()[:8]
 
     if not cover_sname in solids_store:
-        #s = ET.SubElement(solids, 'box', name=cover_sname, lunit="cm", x=xx, y=yy, z=zz)
         scover = ET.SubElement(solids, 'subtraction', name=cover_sname)
         ET.SubElement(scover, 'first', ref=sname)
         ET.SubElement(scover, 'second', ref=es.attrib['name'])
@@ -126,14 +126,14 @@ def module(x, y, z=1.0, nx=16):
 
     # Volumes
     # Cover
-    cover_name = 'CRTModuleCover_' + uuid.uuid4().hex[:8]
+    cover_name = 'CRTModuleCover_' + sequential_id()
     vcname = 'vol' + cover_name
     vc = ET.SubElement(structure, 'volume', name=vcname)
     ET.SubElement(vc, 'materialref', ref='ALUMINUM_Al')
     ET.SubElement(vc, 'solidref', ref=cover_sname)
 
     # Module
-    name = 'CRTModule_' + uuid.uuid4().hex[:8]
+    name = 'CRTModule_' + sequential_id()
     vname = 'vol' + name
     v = ET.SubElement(structure, 'volume', name=vname)
     ET.SubElement(v, 'materialref', ref='ALUMINUM_Al')
@@ -152,12 +152,13 @@ def module(x, y, z=1.0, nx=16):
     return s, v
 
 
-def plane(x, y, nx, ny, z=1.0, nxs=16):
-    '''Build a plane: an Nx by Ny grid of modules.'''
-    xx = str((x * nxs + 2 * PAD) * nx)
-    yy = str(y * ny)
-    zz = str(z + 2 * PAD)
-    sname = 'CRTPlane_' + md5('_'.join((xx, yy, zz))).hexdigest()[:8]
+def tagger(name, x1, y1, nx1, ny1, x2, y2, nx2, ny2, z=1.0, nxs=16):
+    '''Build a tagger: a stack of two perpendicular planes.'''
+    # Tagger solid
+    xx = str((x1 * nxs + 2 * PAD) * nx1)
+    yy = str(y1 * ny1)
+    zz = str(2.0 * (z + 2 * PAD))
+    sname = 'Tagger' + name + '_' + md5('_'.join((xx, yy, zz))).hexdigest()[:8]
 
     if not sname in solids_store:
         s = ET.SubElement(solids, 'box', name=sname, lunit="cm", x=xx, y=yy, z=zz)
@@ -165,86 +166,172 @@ def plane(x, y, nx, ny, z=1.0, nxs=16):
     else:
         s = solids_store[sname]
 
-    modules = []
-    for i in range(nx):
-        for j in range(ny):
-            modules.append(module(x, y, z, nxs))
+    # Solids for planes of modules
+    planes = []
+    pd = ((x1, y1, nx1, ny1), (x2, y2, nx2, ny2))
+    for plane in pd:
+        px, py, pnx, pny = plane
+
+        p1xx = str((px * nxs + 2 * PAD) * pnx)
+        p1yy = str(py * pny)
+        p1zz = str(z + 2 * PAD)
+
+        modules = []
+        for i in range(pnx):
+            for j in range(pny):
+                modules.append(module(px, py, z, nxs))
  
-    name = 'CRTPlane_' + uuid.uuid4().hex[:8]
+        planes.append(modules)
+
+    # Tagger volume
+    name = 'Tagger' + name #+ '_' + sequential_id()
     vname = 'vol' + name
     v = ET.SubElement(structure, 'volume', name=vname)
     ET.SubElement(v, 'materialref', ref='ALUMINUM_Al')
     ET.SubElement(v, 'solidref', ref=sname)
 
-    for i in range(nx):
-        for j in range(ny):
-            es, ev = modules[j + i*ny]
+    # Module volumes
+    for k, (p, modules) in enumerate(zip(*(pd, planes))):
+        px, py, pnx, pny = p
+        for i in range(pnx):
+            for j in range(pny):
+                es, ev = modules[j + i*pny]
 
-            pv = ET.SubElement(v, 'physvol')
-            ET.SubElement(pv, 'volumeref', ref=ev.attrib['name'])
+                pv = ET.SubElement(v, 'physvol')
+                ET.SubElement(pv, 'volumeref', ref=ev.attrib['name'])
 
-            dx = 1.0 * (2*i - nx + 1) / 2 * (x * nxs + 2 * PAD)
-            dy = 1.0 * (2*j - ny + 1) / 2 * y
+                dx = 1.0 * (2*i - pnx + 1) / 2 * (px * nxs + 2 * PAD)
+                dy = 1.0 * (2*j - pny + 1) / 2 * py
+                dz = (z + 2 * PAD) / 2
+                if k == 0:
+                    dz *= -1
+                else:
+                    dt = dx
+                    dx = dy
+                    dy = dt
 
-            posname = 'pos' + ev.attrib['name']
-            ET.SubElement(pv, 'position', name=posname,
-                          unit="cm", x=str(dx), y=str(dy), z='0')
+                posname = 'pos' + ev.attrib['name']
+                ET.SubElement(pv, 'position', name=posname,
+                              unit="cm", x=str(dx), y=str(dy), z=str(dz))
+
+                if k == 1:
+                    posname = 'rot' + ev.attrib['name']
+                    ET.SubElement(pv, 'rotation', name=posname,
+                                  unit="deg", x='0', y='0', z='90')
 
     return s, v
 
 
-def tagger(x1, y1, nx1, ny1, x2, y2, nx2, ny2, z=1.0, nxs=16):
-    '''Build a tagger: a stack of two perpendicular planes.'''
-    xx = str((x1 * nxs + 2 * PAD) * nx1)
-    yy = str(y1 * ny1)
-    zz = str(2.0 * (z + 2 * PAD))
-    sname = 'Tagger_' + md5('_'.join((xx, yy, zz))).hexdigest()[:8]
+def tagger_bot(name='TaggerBot'):
+    '''Build the bottom tagger.
 
+    The bottom tagger is not a simple grid, but modules are specially placed
+    to avoid the structural steel holding up the cryostat. Since this is so
+    specific, the dimensions are not customizable.
+
+    :param name: Base name for the volume
+    :param nxs: Number of scintillator strips in a module
+    '''
+    # Strips dimensions
+    sx = 5.95
+    sy = 272.0
+    sz = 1.0
+    nxs = 16
+
+    # Overall array dimensions (see CRT tech note Figure 2.7)
+    x = 215.0 + 2 * 19.0 + 2 * 1.0 + 2 * 272.0 + 40.0
+    y = 3 * 98.0 + 4 * 19.0 + 2 * 1.0 + 2 * 272.0 + 40.0
+    z = 2.0 * (sz + 2 * PAD)
+
+    # Tagger solid
+    sname = name
     if not sname in solids_store:
-        s = ET.SubElement(solids, 'box', name=sname, lunit="cm", x=xx, y=yy, z=zz)
-        solids_store[sname] = s
-    else:
-        s = solids_store[sname]
+        s = ET.SubElement(solids, 'box', name=sname, lunit="cm",
+                          x=str(x), y=str(y), z=(str(z)))
 
-    s1, v1 = plane(x1, y1, nx1, ny1)
-    s2, v2 = plane(x2, y2, nx2, ny2)
- 
-    name = 'Tagger_' + uuid.uuid4().hex[:8]
-    vname = 'volAuxDet' + name
+    # Heap of modules
+    modules = [module(sx, sy, sz, nxs) for i in range(34)]
+
+    w, l, h = map(lambda dim: float(modules[0][0].attrib[dim]), ('x', 'y', 'z'))
+
+    # Tagger volume
+    vname = 'vol' + name
     v = ET.SubElement(structure, 'volume', name=vname)
     ET.SubElement(v, 'materialref', ref='ALUMINUM_Al')
     ET.SubElement(v, 'solidref', ref=sname)
 
-    dz = (z + 2 * PAD) / 2
+    # Positions and rotations for each module in the array
+    pos_rotz = (
+        # Top row
+        (-215.0/2 - 19.0 - 1.0 - 1*w/2,  (y/2-l/2), -h/2, 0),
+        (-215.0/2 - 19.0 - 1.0 - 3*w/2,  (y/2-l/2), -h/2, 0),
+        (-215.0/2 - 19.0 - 1.0 - 5*w/2,  (y/2-l/2), -h/2, 0),
+        (                         -w/2,  (y/2-l/2), -h/2, 0),
+        (                          w/2,  (y/2-l/2), -h/2, 0),
+        ( 215.0/2 + 19.0 + 1.0 + 1*w/2,  (y/2-l/2), -h/2, 0),
+        ( 215.0/2 + 19.0 + 1.0 + 3*w/2,  (y/2-l/2), -h/2, 0),
+        ( 215.0/2 + 19.0 + 1.0 + 5*w/2,  (y/2-l/2), -h/2, 0),
 
-    # Plane 1: 0 degrees
-    pv = ET.SubElement(v, 'physvol')
-    ET.SubElement(pv, 'volumeref', ref=v1.attrib['name'])
-    posname = 'pos' + v1.attrib['name']
-    ET.SubElement(pv, 'position', name=posname,
-                  unit="cm", x='0', y='0', z=str(-dz))
+        # Bottom row
+        (-215.0/2 - 19.0 - 1.0 - 1*w/2, -(y/2-l/2), -h/2, 0),
+        (-215.0/2 - 19.0 - 1.0 - 3*w/2, -(y/2-l/2), -h/2, 0),
+        (-215.0/2 - 19.0 - 1.0 - 5*w/2, -(y/2-l/2), -h/2, 0),
+        (                         -w/2, -(y/2-l/2), -h/2, 0),
+        (                          w/2, -(y/2-l/2), -h/2, 0),
+        ( 215.0/2 + 19.0 + 1.0 + 1*w/2, -(y/2-l/2), -h/2, 0),
+        ( 215.0/2 + 19.0 + 1.0 + 3*w/2, -(y/2-l/2), -h/2, 0),
+        ( 215.0/2 + 19.0 + 1.0 + 5*w/2, -(y/2-l/2), -h/2, 0),
+
+        # Left column
+        (-215.0/2 - 19.0 - 1.0 - l/2,   2*98.0+2*19.0,  h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,   3*98.0+2*19.0,  h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,   4*98.0+2*19.0,  h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,       98.0+19.0, -h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,               0, -h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,      -98.0-19.0, -h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,  -2*98.0-2*19.0,  h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,  -3*98.0-2*19.0,  h/2, 90),
+        (-215.0/2 - 19.0 - 1.0 - l/2,  -4*98.0-2*19.0,  h/2, 90),
+
+        # Right column
+        ( 215.0/2 + 19.0 + 1.0 + l/2,   2*98.0+2*19.0,  h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,   3*98.0+2*19.0,  h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,   4*98.0+2*19.0,  h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,       98.0+19.0, -h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,               0, -h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,      -98.0-19.0, -h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,  -2*98.0-2*19.0,  h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,  -3*98.0-2*19.0,  h/2, 90),
+        ( 215.0/2 + 19.0 + 1.0 + l/2,  -4*98.0-2*19.0,  h/2, 90),
+    )
+
+    for i, pr in enumerate(pos_rotz):
+        es, ev = modules[i]
+        pv = ET.SubElement(v, 'physvol')
+        ET.SubElement(pv, 'volumeref', ref=ev.attrib['name'])
     
-    # Plane 2: 90 degrees
-    pv = ET.SubElement(v, 'physvol')
-    ET.SubElement(pv, 'volumeref', ref=v2.attrib['name'])
-    posname = 'pos' + v2.attrib['name']
-    ET.SubElement(pv, 'position', name=posname,
-                  unit="cm", x='0', y='0', z=str(dz))
-    posname = 'rot' + v2.attrib['name']
-    ET.SubElement(pv, 'rotation', name=posname,
-                  unit="deg", x='0', y='0', z='90')
+        pxs, pys, pzs, przs = map(str, pr)
+
+        posname = 'pos' + ev.attrib['name']
+        ET.SubElement(pv, 'position', name=posname,
+                      unit="cm", x=pxs, y=pys, z=pzs)
+
+        if pr[-1] != 0:
+            posname = 'rot' + ev.attrib['name']
+            ET.SubElement(pv, 'rotation', name=posname,
+                          unit="deg", x='0', y='0', z=przs)
 
     return s, v
 
 
 # Build the taggers
-tths, tthv = tagger(11.2, 450.0, 5, 2, 11.2, 450.0, 5, 2)  # Top H
-ttls, ttlv = tagger(11.2, 450.0, 5, 2, 11.2, 450.0, 5, 2)  # Top L
-tsls, tslv = tagger(11.2, 360.0, 5, 2, 11.2, 450.0, 4, 2)  # Side L
-tsrs, tsrv = tagger(11.2, 360.0, 5, 2, 11.2, 450.0, 4, 2)  # Side R
-tffs, tffv = tagger(11.2, 360.0, 4, 2, 11.2, 360.0, 4, 2)  # Face F
-tfbs, tfbv = tagger(11.2, 360.0, 4, 2, 11.2, 360.0, 4, 2)  # Face B
-# FIXME: Add bottom tagger, which isn't simply a grid.
+tths, tthv = tagger('TopHigh',   11.2, 450.0, 5, 2, 11.2, 450.0, 5, 2)
+ttls, ttlv = tagger('TopLow',    11.2, 450.0, 5, 2, 11.2, 450.0, 5, 2)
+tsls, tslv = tagger('SideLeft',  11.2, 360.0, 5, 2, 11.2, 450.0, 4, 2)
+tsrs, tsrv = tagger('SideRight', 11.2, 360.0, 5, 2, 11.2, 450.0, 4, 2)
+tffs, tffv = tagger('FaceFront', 11.2, 360.0, 4, 2, 11.2, 360.0, 4, 2)
+tfbs, tfbv = tagger('FaceBack',  11.2, 360.0, 4, 2, 11.2, 360.0, 4, 2)
+tbbs, tbbv = tagger_bot()
 
 # Generate GDML for the world volume, for testing
 #ws = ET.SubElement(solids, 'box', name='World', lunit="cm", x='10000', y='10000', z='10000')
@@ -254,10 +341,9 @@ tfbs, tfbv = tagger(11.2, 360.0, 4, 2, 11.2, 360.0, 4, 2)  # Face B
 #pv = ET.SubElement(w, 'physvol')
 #ET.SubElement(pv, 'volumeref', ref=tffv.attrib['name'])
 #ET.SubElement(pv, 'position', name='posA', unit="cm", x='0', y='0', z='0')
-#setup = ET.Element('setup', name='Default', version='1.0')
+#setup = ET.SubElement(gdml, 'setup', name='Default', version='1.0')
 #ET.SubElement(setup, 'world', ref='volWorld')
 
 with open('crt.gdml', 'w') as f:
-    f.write(minidom.parseString(ET.tostring(solids)).toprettyxml(indent=' '))
-    f.write(minidom.parseString(ET.tostring(structure)).toprettyxml(indent=' '))
+    f.write(minidom.parseString(ET.tostring(gdml)).toprettyxml(indent=' '))
 
