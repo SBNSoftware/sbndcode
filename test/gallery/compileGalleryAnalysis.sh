17acbe50506b9f75e40af34e45d2e935b7b69eef
Gianluca Petrillo <petrillo@fnal.gov>
2018-04-10 16:33:12 -0500

    Added unit test for gallery macro compilation.
    By necessity, this tests only compilation in the build environment.
    Also, the CMakeLists.txt file that is provided by galleryAnalysis is only compatible with GCC, not Clang.
    Therefore the test is enabled only for GCC.
    
    This solves issue 19073.

---
+++ test/gallery/compileGalleryAnalysis.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# Purpose: exercise compilation of gallery in a compatible environment.
+# This script will return a non-zero exit code on compilation error.
+#
+
+# the first simple command exiting with an error will cause the shell to exit;
+# sometimes this interferes with the commands (e.g. `setup`); in that case,
+# some trick is needed.
+set -e
+
+declare -r CWD="$(pwd)"
+declare -r WorkDir="${CWD}/galleryTest"
+
+#
+# check that `setup` function is available
+#
+if ! declare -f setup >& /dev/null ; then
+  if [[ -z "$SETUP_UPS" ]]; then
+    echo "UPS not correctly set up!!"
+  else
+    setup ${SETUP_UPS} || exit $?
+  fi
+fi
+
+#
+# create a working area
+#
+declare -r SourceDir="${WorkDir}/source"
+declare -r BuildDir="${WorkDir}/build"
+
+declare -r SBNDgalleryBaseDir="${SBNDCODE_DIR}/sbndcode/gallery"
+
+rm -Rf "$WorkDir"
+mkdir "$WorkDir"
+cd "$WorkDir"
+
+cp -a "${SBNDgalleryBaseDir}/galleryAnalysis" "$SourceDir"
+
+rm -Rf "$BuildDir"
+mkdir "$BuildDir"
+
+#
+# set up gallery
+#
+source "${SBNDgalleryBaseDir}/helpers/sbnd_gallery_setup"
+
+#
+# proceed with compilation
+#
+cmake "$SourceDir"
+make
+
