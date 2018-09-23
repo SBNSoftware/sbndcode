2254d3ecd70b879f797f748b910dddc1025e93f0
Arbin Timilsina <arbint@bnl.gov>
2018-09-23 13:44:07 -0500

    SpaceChargeTest successful- able to make parameterized plots to compare with input

--- sbndcode/SpaceChargeTest/SpaceChargeTest_module.cc
+++ sbndcode/SpaceChargeTest/SpaceChargeTest_module.cc
@@ -1,7 +1,7 @@
 #ifndef SPACE_CHARGE_TEST
 #define SPACE_CHARGE_TEST
 
-/// Framework includes
+// Framework includes
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
@@ -11,6 +11,9 @@
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/Optional/TFileService.h"
 #include "fhiclcpp/ParameterSet.h"
+
+// Root includes
+#include <TH1.h>
 
 // Larsoft includes
 #include "larevt/SpaceChargeServices/SpaceChargeService.h"
@@ -34,6 +37,15 @@ public:
 
     // Required function
     void analyze(art::Event const & e) override;
+
+private:
+
+    TH1D *hDx;
+    TH1D *hDy;
+    TH1D *hDz;
+    TH1D *hEx;
+    TH1D *hEy;
+    TH1D *hEz;
 };
 
 SpaceChargeTools::SpaceChargeTest::SpaceChargeTest(fhicl::ParameterSet const & p) : EDAnalyzer(p)
@@ -41,14 +53,74 @@ SpaceChargeTools::SpaceChargeTest::SpaceChargeTest(fhicl::ParameterSet const & p
 }
 
 SpaceChargeTools::SpaceChargeTest::~SpaceChargeTest() {}
-void SpaceChargeTools::SpaceChargeTest::beginJob() {}
+
+void SpaceChargeTools::SpaceChargeTest::beginJob()
+{
+    // Access ART's TFileService, which will handle creating and writing histograms and n-tuples
+    art::ServiceHandle<art::TFileService> fileServiceHandle;
+
+    hDx = fileServiceHandle->make<TH1D>("hDx", "", 150, -1.0, 0.6);
+    hDy = fileServiceHandle->make<TH1D>("hDy", "", 150, -6.0, 6.0);
+    hDz = fileServiceHandle->make<TH1D>("hDz", "", 150, -6.0, 6.0);
+    hEx = fileServiceHandle->make<TH1D>("hEx", "", 100, -0.06, 0.06);
+    hEy = fileServiceHandle->make<TH1D>("hEy", "", 100, -0.04, 0.04);
+    hEz = fileServiceHandle->make<TH1D>("hEz", "", 100, -0.04, 0.04);
+}
+
 void SpaceChargeTools::SpaceChargeTest::endJob() {}
 
 void SpaceChargeTools::SpaceChargeTest::analyze(art::Event const & evt)
 {
+    int xMin = -206;
+    int xMax = 206;
+    int yMin = -210;
+    int yMax = 210;
+    int zMin = -10;
+    int zMax = 510;
+
     auto const* SCE = lar::providerFrom<spacecharge::SpaceChargeService>();
     cout << "Is Spatial SCE enabled? " << bool(SCE->EnableSimSpatialSCE()) << endl;
     cout << "Is E-field SCE enabled? " << bool(SCE->EnableSimEfieldSCE()) << endl;
+<<<<<<< HEAD
+=======
+
+    int nSkip = 5;
+    for(int iX = xMin; iX <= xMax - nSkip; iX++)
+        {
+            iX = iX + nSkip;
+            for(int iY = yMin; iY <= yMax - nSkip; iY++)
+                {
+                    iY = iY + nSkip;
+                    for(int iZ = zMin; iZ <= zMax - nSkip; iZ++)
+                        {
+                            iZ = iZ + nSkip;
+                            cout << iX << ", " << iY << ", " << iZ << endl;
+
+                            geo::Point_t point = {double(iX), double(iY), double(iZ)};
+
+                            geo::Vector_t spatialOffsets = SCE->GetPosOffsets(point);
+                            if(!((spatialOffsets.X() == spatialOffsets.X()) &&
+				 (spatialOffsets.Y() == spatialOffsets.Y()) &&
+				 (spatialOffsets.Z() == 0.0)))
+                                {
+                                    hDx->Fill(spatialOffsets.X());
+                                    hDy->Fill(spatialOffsets.Y());
+                                    hDz->Fill(spatialOffsets.Z());
+                                }
+
+                            geo::Vector_t efieldOffsets = SCE->GetEfieldOffsets(point);
+                            if(!((efieldOffsets.X() == efieldOffsets.X()) &&
+				 (efieldOffsets.Y() == efieldOffsets.Y()) &&
+				 (efieldOffsets.Z() == 0.0)))
+                                {
+                                    hEx->Fill(efieldOffsets.X());
+                                    hEy->Fill(efieldOffsets.Y());
+                                    hEz->Fill(efieldOffsets.Z());
+                                }
+                        }
+                }
+        }
+>>>>>>> SpaceChargeTest successful- able to make parameterized plots to compare with input
 }
 
 DEFINE_ART_MODULE(SpaceChargeTools::SpaceChargeTest)
