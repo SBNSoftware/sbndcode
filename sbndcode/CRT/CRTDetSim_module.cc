404edbd5209f630334c616e8edcddbc870b51aad
Thomas Brooks <tbrooks@sbndgpvm03.fnal.gov>
2018-10-04 16:33:12 -0500

    Modified CRT simulation and added CRT hit reconstruction for MCP 0.9

--- sbndcode/CRT/CRTDetSim_module.cc
+++ sbndcode/CRT/CRTDetSim_module.cc
@@ -30,7 +30,7 @@
 #include "TNtuple.h"
 #include "TGeoManager.h"
 #include "TGeoNode.h"
-#include "CRTData.hh"
+#include "CRTProducts/CRTData.hh"
 #include "CRTDetSim.h"
 
 #include <cmath>
@@ -66,7 +66,9 @@ void CRTDetSim::reconfigure(fhicl::ParameterSet const & p) {
   fQRMS = p.get<double>("QRMS");
   fQThreshold = p.get<double>("QThreshold");
   fStripCoincidenceWindow = p.get<double>("StripCoincidenceWindow");
+  fTaggerPlaneCoincidenceWindow = p.get<double>("TaggerPlaneCoincidenceWindow");
   fAbsLenEff = p.get<double>("AbsLenEff");
+  fSipmTimeResponse = p.get<double>("SipmTimeResponse");
 }
 
 
@@ -119,7 +121,7 @@ uint32_t CRTDetSim::getChannelTriggerTicks(CLHEP::HepRandomEngine* engine,
 
 
 struct Tagger {
-  std::set<unsigned> planesHit;
+  std::vector<std::pair<unsigned, uint32_t> > planesHit;
   std::vector<sbnd::crt::CRTData> data;
 };
 
@@ -149,45 +151,82 @@ void CRTDetSim::produce(art::Event & e) {
     const geo::AuxDetSensitiveGeo& adsGeo = \
         adGeo.SensitiveVolume(adsc.AuxDetSensitiveID());
 
-    // Simulate the CRT response for each hit
-    for (auto ide : adsc.AuxDetIDEs()) {
-      // Find the path to the strip geo node, to locate it in the hierarchy
-      std::set<std::string> volNames = { adsGeo.TotalVolume()->GetName() };
-      std::vector<std::vector<TGeoNode const*> > paths = \
-        geoService->FindAllVolumePaths(volNames);
+    // Return the vector of IDEs
+    std::vector<sim::AuxDetIDE> ides = adsc.AuxDetIDEs();
+    std::sort(ides.begin(), ides.end(), 
+              [](const sim::AuxDetIDE & a, const sim::AuxDetIDE & b) -> bool{
+                return ((a.entryT + a.exitT)/2) < ((b.entryT + b.exitT)/2);
+              });
 
-      std::string path = "";
-      for (size_t inode=0; inode<paths.at(0).size(); inode++) {
-        path += paths.at(0).at(inode)->GetName();
-        if (inode < paths.at(0).size() - 1) {
-          path += "/";
-        }
+    // Find the path to the strip geo node, to locate it in the hierarchy
+    std::set<std::string> volNames = { adsGeo.TotalVolume()->GetName() };
+    std::vector<std::vector<TGeoNode const*> > paths = \
+      geoService->FindAllVolumePaths(volNames);
+
+    std::string path = "";
+    for (size_t inode=0; inode<paths.at(0).size(); inode++) {
+      path += paths.at(0).at(inode)->GetName();
+      if (inode < paths.at(0).size() - 1) {
+        path += "/";
       }
+    }
 
-      TGeoManager* manager = geoService->ROOTGeoManager();
-      manager->cd(path.c_str());
+    TGeoManager* manager = geoService->ROOTGeoManager();
+    manager->cd(path.c_str());
 
-      TGeoNode* nodeStrip = manager->GetCurrentNode();
-      TGeoNode* nodeArray = manager->GetMother(1);
-      TGeoNode* nodeModule = manager->GetMother(2);
-      TGeoNode* nodeTagger = manager->GetMother(3);
+    TGeoNode* nodeStrip = manager->GetCurrentNode();
+    TGeoNode* nodeArray = manager->GetMother(1);
+    TGeoNode* nodeModule = manager->GetMother(2);
+    TGeoNode* nodeTagger = manager->GetMother(3);
 
-      // Module position in parent (tagger) frame
-      double origin[3] = {0, 0, 0};
-      double modulePosMother[3];
-      nodeModule->LocalToMaster(origin, modulePosMother);
+    // Module position in parent (tagger) frame
+    double origin[3] = {0, 0, 0};
+    double modulePosMother[3];
+    nodeModule->LocalToMaster(origin, modulePosMother);
 
-      // Determine plane ID (1 for z > 0, 0 for z < 0 in local coordinates)
-      unsigned planeID = (modulePosMother[2] > 0);
+    // Determine plane ID (1 for z > 0, 0 for z < 0 in local coordinates)
+    unsigned planeID = (modulePosMother[2] > 0);
 
-      // Determine module orientation: which way is the top (readout end)?
-      bool top = (planeID == 1) ? (modulePosMother[1] > 0) : (modulePosMother[0] < 0);
+    // Determine module orientation: which way is the top (readout end)?
+    bool top = (planeID == 1) ? (modulePosMother[1] > 0) : (modulePosMother[0] < 0);
+
+    // Simulate the CRT response for each hit
+    for (size_t ide_i = 0; ide_i < ides.size(); ide_i++) {
+      
+      sim::AuxDetIDE ide = ides[ide_i];
+
+      int trackID = ide.trackID;
 
       // Finally, what is the distance from the hit (centroid of the entry
       // and exit points) to the readout end?
       double x = (ide.entryX + ide.exitX) / 2;
       double y = (ide.entryY + ide.exitY) / 2;
       double z = (ide.entryZ + ide.exitZ) / 2;
+
+      double tTrue = (ide.entryT + ide.exitT) / 2 + fGlobalT0Offset;
+      double eDep = ide.energyDeposited;
+      double maxEdep = eDep;
+
+      //ADD UP HITS AT THE SAME TIME - 2NS DIFF IS A GUESS -VERY APPROXIMATE
+      if(ide_i < ides.size() - 1){
+        while(ide_i < ides.size() - 1 && std::abs(tTrue-((ides[ide_i+1].entryT + ides[ide_i+1].exitT) / 2 + fGlobalT0Offset)) < fSipmTimeResponse){
+          ide_i++;
+          x = (x + ((ides[ide_i].entryX + ides[ide_i].exitX) / 2))/2;
+          y = (y + ((ides[ide_i].entryY + ides[ide_i].exitY) / 2))/2;
+          z = (z + ((ides[ide_i].entryZ + ides[ide_i].exitZ) / 2))/2;
+          eDep += ides[ide_i].energyDeposited;
+          tTrue = (tTrue + ((ides[ide_i].entryT + ides[ide_i].exitT) / 2))/2;
+          if(ides[ide_i].energyDeposited > maxEdep && ides[ide_i].trackID > 0){
+            trackID = ides[ide_i].trackID;
+            maxEdep = ides[ide_i].energyDeposited;
+          }
+          if(trackID < 0 && (ides[ide_i].energyDeposited > maxEdep || ides[ide_i].trackID > 0)){
+            trackID = ides[ide_i].trackID;
+            maxEdep = ides[ide_i].energyDeposited;
+          }
+        }
+      }
+
       double world[3] = {x, y, z};
       double svHitPosLocal[3];
       adsGeo.WorldToLocal(world, svHitPosLocal);
@@ -202,7 +241,7 @@ void CRTDetSim::produce(art::Event & e) {
 
       // The expected number of PE, using a quadratic model for the distance
       // dependence, and scaling linearly with deposited energy.
-      double qr = fUseEdep ? 1.0 * ide.energyDeposited / fQ0 : 1.0;
+      double qr = fUseEdep ? 1.0 * eDep / fQ0 : 1.0;
 
       double npeExpected = \
         fNpeScaleNorm / pow(distToReadout - fNpeScaleShift, 2) * qr;
@@ -222,7 +261,6 @@ void CRTDetSim::produce(art::Event & e) {
 
       // Time relative to trigger, accounting for propagation delay and 'walk'
       // for the fixed-threshold discriminator
-      double tTrue = (ide.entryT + ide.exitT) / 2 + fGlobalT0Offset;
       uint32_t t0 = \
         getChannelTriggerTicks(engine, trigClock, tTrue, npe0, distToReadout);
       uint32_t t1 = \
@@ -253,9 +291,9 @@ void CRTDetSim::produce(art::Event & e) {
           q1 > fQThreshold &&
           util::absDiff(t0, t1) < fStripCoincidenceWindow) {
         Tagger& tagger = taggers[nodeTagger->GetName()];
-        tagger.planesHit.insert(planeID);
-        tagger.data.push_back(sbnd::crt::CRTData(channel0ID, t0, ppsTicks, q0));
-        tagger.data.push_back(sbnd::crt::CRTData(channel1ID, t1, ppsTicks, q1));
+        tagger.planesHit.push_back({planeID, t0});
+        tagger.data.push_back(sbnd::crt::CRTData(channel0ID, t0, ppsTicks, q0, trackID));
+        tagger.data.push_back(sbnd::crt::CRTData(channel1ID, t1, ppsTicks, q1, trackID));
       }
 
       double poss[3];
@@ -286,8 +324,25 @@ void CRTDetSim::produce(art::Event & e) {
   // Logic: For normal taggers, require at least one hit in each perpendicular
   // plane. For the bottom tagger, any hit triggers read out.
   for (auto trg : taggers) {
-    if (trg.first.find("TaggerBot") != std::string::npos ||
-        trg.second.planesHit.size() > 1) {
+    bool trigger = false;
+
+    // Loop over pairs of hits
+    for (auto t1 : trg.second.planesHit) {
+      for (auto t2 : trg.second.planesHit) {
+        if (t1 == t2) {
+          continue;
+        }
+
+        // Two hits on different planes with proximal t0 times
+        if (t1.first != t2.first && util::absDiff(t1.second, t2.second) < fTaggerPlaneCoincidenceWindow) {
+          trigger = true;
+          break;
+        }
+      }
+    }
+
+    if (trigger || trg.first.find("TaggerBot") != std::string::npos) {
+      // Write out all hits on a tagger when there is any coincidence
       for (auto d : trg.second.data) {
         triggeredCRTHits->push_back(d);
       }
