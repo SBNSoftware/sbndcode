296e1050a450e0fa49a3374adf3834adb292a362
gputnam <gputnam@sbndgpvm01.fnal.gov>
2019-09-12 11:14:44 -0500

    Add in API to provide services anddata products directly to algorithms. Remove dependency on TPCGeo class and add in helper functions to new TPCGeoUtil file.

--- sbndcode/CRT/CRTUtils/CRTT0MatchAlg.h
+++ sbndcode/CRT/CRTUtils/CRTT0MatchAlg.h
@@ -25,8 +25,11 @@
 #include "lardataobj/RecoBase/Track.h"
 #include "larcore/Geometry/Geometry.h"
 #include "larcorealg/Geometry/GeometryCore.h"
+#include "lardataalg/DetectorInfo/DetectorProperties.h"
 #include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 #include "lardata/DetectorInfoServices/DetectorClocksService.h"
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/GeometryCore.h"
 #include "lardataobj/Simulation/AuxDetSimChannel.h"
 #include "larcore/Geometry/AuxDetGeometry.h"
 
@@ -40,6 +43,7 @@
 #include "sbndcode/CRT/CRTProducts/CRTHit.hh"
 #include "sbndcode/CRT/CRTUtils/CRTCommonUtils.h"
 #include "sbndcode/Geometry/GeometryWrappers/TPCGeoAlg.h"
+#include "sbndcode/CRT/CRTUtils/TPCGeoUtil.h"
 
 // c++
 #include <iostream>
@@ -88,9 +92,13 @@ namespace sbnd{
     };
 
     CRTT0MatchAlg(const Config& config);
+    CRTT0MatchAlg(const Config& config, geo::GeometryCore const *GeometryService, detinfo::DetectorProperties const* DetectorProperties);
 
     CRTT0MatchAlg(const fhicl::ParameterSet& pset) :
       CRTT0MatchAlg(fhicl::Table<Config>(pset, {})()) {}
+
+    CRTT0MatchAlg(const fhicl::ParameterSet& pset, geo::GeometryCore const *GeometryService, detinfo::DetectorProperties const* DetectorProperties) :
+      CRTT0MatchAlg(fhicl::Table<Config>(pset, {})(), GeometryService, DetectorProperties) {}
 
     CRTT0MatchAlg();
 
@@ -107,13 +115,15 @@ namespace sbnd{
     std::pair<TVector3, TVector3> TrackDirectionAverage(recob::Track track, double frac);
 
     std::pair<crt::CRTHit, double> ClosestCRTHit(recob::Track tpcTrack, std::vector<sbnd::crt::CRTHit> crtHits, const art::Event& event);
+    std::pair<crt::CRTHit, double> ClosestCRTHit(recob::Track tpcTrack, std::vector<art::Ptr<recob::Hit>> hits, std::vector<sbnd::crt::CRTHit> crtHits);
 
     double T0FromCRTHits(recob::Track tpcTrack, std::vector<sbnd::crt::CRTHit> crtHits, const art::Event& event);
+    double T0FromCRTHits(recob::Track tpcTrack, std::vector<art::Ptr<recob::Hit>> hits, std::vector<sbnd::crt::CRTHit> crtHits);
 
   private:
 
     detinfo::DetectorProperties const* fDetectorProperties;
-    TPCGeoAlg fTpcGeo;
+    geo::GeometryCore const* fGeometryService;
 
     double fMinTrackLength;
     double fTrackDirectionFrac;
