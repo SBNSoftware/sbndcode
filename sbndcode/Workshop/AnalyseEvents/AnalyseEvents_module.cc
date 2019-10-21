e7b6e3842488166c9fb07df860fbb68f4a7e595a
Rhiannon Jones <rjones0211@hotmail.com>
2019-10-20 19:22:27 -0500

    Added track length histogram to the output of the analysis module

--- sbndcode/Workshop/AnalyseEvents/AnalyseEvents_module.cc
+++ sbndcode/Workshop/AnalyseEvents/AnalyseEvents_module.cc
@@ -28,6 +28,7 @@
 
 // ROOT includes
 #include <TTree.h>
+#include <TH1D.h>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -63,12 +64,15 @@ private:
   // Output tree declaration
   TTree *fTree;
 
+  // Output histogram declaration
+  TH1D  *fTrackLengthHist;
+
   // Variables to access and analyse
   unsigned int fEventID;
   unsigned int fNPFParticles;
   unsigned int fNPrimaries;
   int fNPrimaryDaughters;
-  float fDaughterTrackLength;
+  std::vector<float> *fDaughterTrackLengths;
 
   // Module labels 
   std::string fPFParticleLabel;
@@ -79,11 +83,18 @@ private:
 
 
 sbnd::AnalyseEvents::AnalyseEvents(fhicl::ParameterSet const& p)
-  : EDAnalyzer{p}  // ,
-  // More initializers here.
+  : EDAnalyzer{p},
+    fTree(nullptr),
+    fTrackLengthHist(nullptr),
+    fEventID(99999),
+    fNPFParticles(99999),
+    fNPrimaries(99999),
+    fNPrimaryDaughters(-99999),
+    fDaughterTrackLengths(nullptr)
+    // Initialising all variables to obviously unphysical values 
+    // so that we know when something just hasn't been filled
 {
-  // Call appropriate consumes<>() for any products to be retrieved by this module.
-  // Get the producer name for the PFParticles from the configuration fhicl file 
+  // Get the producer name for all reco objects from the configuration fhicl file 
   //    analysisConfig.fcl
   fPFParticleLabel  = p.get<std::string>("PFParticleLabel");
   fTrackLabel       = p.get<std::string>("TrackLabel");
@@ -97,8 +108,9 @@ void sbnd::AnalyseEvents::analyze(art::Event const& e)
   fEventID = e.id().event();
   
   // Initialise our counters for this event
-  fNPFParticles = 0;
-  fNPrimaries   = 0;
+  fNPFParticles         = 0;
+  fNPrimaries           = 0;
+  fDaughterTrackLengths->clear(); // Make sure the vector is empty at the beginning of the event
 
   // Access the PFParticles from Pandora
   art::Handle< std::vector<recob::PFParticle> > pfparticleHandle;
@@ -111,41 +123,70 @@ void sbnd::AnalyseEvents::analyze(art::Event const& e)
   std::vector< art::Ptr<recob::Track> > trackVect;
   if(e.getByLabel(fTrackLabel, trackHandle)) // Make sure the handle is valid
     art::fill_ptr_vector(trackVect, trackHandle); // Fill the vector with art::Ptr Tracks
+  
+  // Initiate neutrinoID to be non-physical so we can check if we've found it later
+  size_t neutrinoID = 99999;
+
+  // Let's find the ID of our neutrino so we can find it's daughters later on
+  //
+  //    NOTE: For this example, we will only search for a single neutrino in each event.
+  //
+  //    In a real, data-like analysis this would not be the case.
+  //    Then we should really look for a list of neutrinos in each event and analyse them all.
+  //
+  //    However, we have only asked Pandora to reconstruct a single neutrino.
+  //
+  if(!pfparticleVect.size()) {
+    std::cerr << " Error: No reconstructed particles found in the event. Skipping. " << std::endl;
+    return;
+  }
+  
+  // Find the neutrino ID
+  for(const art::Ptr<recob::PFParticle> &pfp : pfparticleVect){
+    // Check if we are looking at a primary PFParticle & that it has a neutrino PdgCode
+    // If we aren't move onto the next particle in the list
+    if(!(pfp->IsPrimary() && (std::abs(pfp->PdgCode()) == 14 || std::abs(pfp->PdgCode()) == 12))) continue;
+    
+    // We've found the neutrino! So let's define the neutrinoID variable
+    neutrinoID         = pfp->Self();
+    fNPrimaryDaughters = pfp->NumDaughters();
+    fNPrimaries++;
+
+    std::cout << " Found the neutrino! PFParticle ID: " << neutrinoID << std::endl;
+    break;
+  }
+
+  // Make sure we've found at the neutrino, if we haven't skip the event
+  if(neutrinoID == 99999){
+    std::cerr << " Error: No reconstructed neutrino found in the event. Skipping." << std::endl;
+    return;
+  }
 
   // Get the vector of vectors of tracks for each PFParticle
   // The vector size of associated tracks to a single PFParticle should be 0 or 1
   art::FindManyP<recob::Track> trackAssoc(pfparticleVect, e, fTrackLabel);
-  
-  // Define a vector of IDs for the location of the neutrino daughters
-  std::vector<size_t> daughterIDs;
-  daughterIDs.clear();
 
-  // Now, let's loop over our particles!
-  if(!pfparticleVect.size()) return;
-  for(art::Ptr<recob::PFParticle> &pfp : pfparticleVect){
+  // Let's loop over our particles again and analyse the neutrino daughters
+  for(const art::Ptr<recob::PFParticle> &pfp : pfparticleVect){
     fNPFParticles++;
-    if(pfp->IsPrimary() && (std::abs(pfp->PdgCode()) == 14 || std::abs(pfp->PdgCode()) == 12)){ // Check we are looking at a primary PFParticle
-      fNPrimaries++;
-      fNPrimaryDaughters = pfp->NumDaughters();
 
-      // Get the list of PFParticle IDs which correspond to neutrino daughters
-      daughterIDs = pfp->Daughters();
+    // Check that the PFParticle is a daughter of the neutrino
+    // If it isn't, move onto the next particle in the list
+    if(pfp->Parent() != neutrinoID) continue;
+      
+    // Check whether there is an associated track to this PFParticle
+    std::vector< art::Ptr<recob::Track> > pfpTracks = trackAssoc.at(pfp.key());
 
-      // Loop over the daughters and break out once finished
-      for(unsigned int i = 0; i < daughterIDs.size(); ++i){
-        art::Ptr<recob::PFParticle> daughter = pfparticleVect.at(daughterIDs[i]);
+    if(pfpTracks.size()){
+      for(art::Ptr<recob::Track> &trk : pfpTracks){
+        // Get some information about the tracks
+        // Fill the TTree variable
+        fDaughterTrackLengths->push_back(trk->Length());
 
-        // Check whether there is an associated track to this PFParticle
-        std::vector< art::Ptr<recob::Track> > pfpTracks = trackAssoc.at(daughter->Self());
-
-        if(pfpTracks.size()){
-          for(art::Ptr<recob::Track> &trk : pfpTracks){
-            // Get some information about the tracks
-            fDaughterTrackLength = trk->Length();
-          }
-        } // nTracks > 0
-      } // Daughters
-    } // IsPrimary
+        // Fill the histogram
+        fTrackLengthHist->Fill(trk->Length());
+      }
+    } // nTracks > 0
   } // pfparticleVect
 
   // Fill the output TTree with all relevant variables
@@ -159,12 +200,17 @@ void sbnd::AnalyseEvents::beginJob()
   art::ServiceHandle<art::TFileService> tfs;
   fTree = tfs->make<TTree>("tree","Analyser Output Tree");
 
+  // Add a histogram to hold our track lengths to the output file
+  // Since we've seen that they are distributed in ~5cm bins between 0 & 340cm 
+  // we should set these as the definitions in the histogram constructor
+  fTrackLengthHist = tfs->make<TH1D>("trackLengthHist","Reconstructed track lengths",70,0,340);
+
   // Add branches to our TTree
   fTree->Branch("eventID",&fEventID,"eventID/i");
   fTree->Branch("nPFParticles",&fNPFParticles,"nPFParticles/i");
   fTree->Branch("nPrimaries",&fNPrimaries,"nPrimaries/i");
   fTree->Branch("nPrimaryDaughters",&fNPrimaryDaughters,"nPrimaryDaughters/I");
-  fTree->Branch("daughterTrackLength",&fDaughterTrackLength,"daughterTrackLength/F");
+  fTree->Branch("daughterTrackLengths",&fDaughterTrackLengths);
 
 }
 
