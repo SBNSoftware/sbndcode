e4c8f741cb5ae289a111dc32fb625e7faa1df165
Gianluca Petrillo <petrillo@slac.stanford.edu>
2020-01-10 13:44:13 -0600

    Moved dictionary definitions into `lardataobj`, where they belong.

--- sbndcode/OpDetSim/FlashFinder/classes.h
+++
@@ -1,6 +0,0 @@
-#include "canvas/Persistency/Common/PtrVector.h" 
-#include "canvas/Persistency/Common/Wrapper.h"
-#include "canvas/Persistency/Common/Assns.h"
-
-#include "lardataobj/RecoBase/OpFlash.h"
-#include "lardataobj/RecoBase/OpHit.h"
