e26b8f0101ec273ee5e8ff7fc72428e629ed3e6f
Iker de Icaza Astiz <icaza@fnal.gov>
2020-02-20 04:24:13 -0600

    Formatting with autopep8

--- sbndcode/FlashMatch/template_generators/generate_simple_weighted_template.py
+++ sbndcode/FlashMatch/template_generators/generate_simple_weighted_template.py
@@ -26,28 +26,29 @@
 #
 ######################################################################
 
-import sys, os, string
+import sys
+import os
+import string
 import numpy as np
 from time import sleep
 from array import array
+
+from ROOT import TStyle, TCanvas, TGraph, TGraphErrors
+from ROOT import TH1D, TH2D, TProfile, TFile
+import ROOT
 # import project_utilities
 import larbatch_posix
 
-# Import ROOT module.
 # Globally turn off root warnings.
 # Don't let root see our command line options.
-
 myargv = sys.argv
 sys.argv = myargv[0:1]
 if 'TERM' in os.environ:
     del os.environ['TERM']
-import ROOT
-from ROOT import TH1D, TH2D, TProfile, TFile
-from ROOT import TStyle, TCanvas, TGraph, TGraphErrors
 ROOT.gErrorIgnoreLevel = ROOT.kError
 sys.argv = myargv
 
-# Print help.
+# Print help
 def help():
 
     filename = sys.argv[0]
@@ -69,98 +70,98 @@ def help():
 
 def generator(input_file, rootfile, gtrees, gbranches):
     dir = rootfile.Get(input_file+":/fmatch")
-    nuslice_tree = dir.Get("nuslicetree")#, nuslice_tree)
+    nuslice_tree = dir.Get("nuslicetree")  # , nuslice_tree)
     # nuslice_tree.Print()
 
     # TODO: un-hardcode this whole block
     n_bins = 40
-    xvals = np.arange(2.5, 200, 5) # TODO: un hardcode
+    xvals = np.arange(2.5, 200, 5)  # TODO: un hardcode
     xerrs = np.array([2.5] * len(xvals))
-    dist_to_anode_bins =  n_bins
-    dist_to_anode_low = 0. # TODO: un hardcode
-    dist_to_anode_up = 200. # TODO: un hardcode
+    dist_to_anode_bins = n_bins
+    dist_to_anode_low = 0.  # TODO: un hardcode
+    dist_to_anode_up = 200.  # TODO: un hardcode
     profile_bins = n_bins
-    profile_option = 's' # errors are the standard deviation
+    profile_option = 's'  # errors are the standard deviation
 
     dy_spreads = [None] * n_bins
     dy_means = [None] * n_bins
-    dy_bins =  100
-    dy_low = -200. # TODO: un hardcode
-    dy_up = 200. # TODO: un hardcode
+    dy_bins = 100
+    dy_low = -200.  # TODO: un hardcode
+    dy_up = 200.  # TODO: un hardcode
     dy_hist = TH2D("dy_hist", "#Delta y",
-               dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
-               dy_bins, dy_low, dy_up)
-    dy_hist.GetXaxis().SetTitle("distance from anode (cm)");
-    dy_hist.GetYaxis().SetTitle("y_flash - y_TPC (cm)");
-    dy_prof = TProfile("dy_prof","Profile of dy_spreads in #Delta y",
+                   dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
+                   dy_bins, dy_low, dy_up)
+    dy_hist.GetXaxis().SetTitle("distance from anode (cm)")
+    dy_hist.GetYaxis().SetTitle("y_flash - y_TPC (cm)")
+    dy_prof = TProfile("dy_prof", "Profile of dy_spreads in #Delta y",
                        profile_bins, dist_to_anode_low, dist_to_anode_up,
-                       dy_low*2, dy_up*2, profile_option);
-    dy_prof.GetXaxis().SetTitle("distance from anode (cm)");
-    dy_prof.GetYaxis().SetTitle("y_flash - y_TPC (cm)");
+                       dy_low*2, dy_up*2, profile_option)
+    dy_prof.GetXaxis().SetTitle("distance from anode (cm)")
+    dy_prof.GetYaxis().SetTitle("y_flash - y_TPC (cm)")
     dy_h1 = TH1D("dy_h1", "",
-                 profile_bins, dist_to_anode_low, dist_to_anode_up);
-    dy_h1.GetXaxis().SetTitle("distance from anode (cm)");
-    dy_h1.GetYaxis().SetTitle("y_flash - y_TPC (cm)");
+                 profile_bins, dist_to_anode_low, dist_to_anode_up)
+    dy_h1.GetXaxis().SetTitle("distance from anode (cm)")
+    dy_h1.GetYaxis().SetTitle("y_flash - y_TPC (cm)")
 
     dz_spreads = [None] * n_bins
     dz_means = [None] * n_bins
-    dz_bins =  100
-    dz_low = -200. # TODO: un hardcode
-    dz_up = 200. # TODO: un hardcode
+    dz_bins = 100
+    dz_low = -200.  # TODO: un hardcode
+    dz_up = 200.  # TODO: un hardcode
     dz_hist = TH2D("dz_hist", "#Delta z",
-               dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
-               dz_bins, dz_low, dz_up)
-    dz_hist.GetXaxis().SetTitle("distance from anode (cm)");
-    dz_hist.GetYaxis().SetTitle("z_flash - z_TPC (cm)");
-    dz_prof = TProfile("dz_prof","Profile of dz_spreads in #Delta z",
+                   dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
+                   dz_bins, dz_low, dz_up)
+    dz_hist.GetXaxis().SetTitle("distance from anode (cm)")
+    dz_hist.GetYaxis().SetTitle("z_flash - z_TPC (cm)")
+    dz_prof = TProfile("dz_prof", "Profile of dz_spreads in #Delta z",
                        profile_bins, dist_to_anode_low, dist_to_anode_up,
-                       dz_low*2.5, dz_up*2.5, profile_option);
-    dz_prof.GetXaxis().SetTitle("distance from anode (cm)");
-    dz_prof.GetYaxis().SetTitle("z_flash - z_TPC (cm)");
+                       dz_low*2.5, dz_up*2.5, profile_option)
+    dz_prof.GetXaxis().SetTitle("distance from anode (cm)")
+    dz_prof.GetYaxis().SetTitle("z_flash - z_TPC (cm)")
     dz_h1 = TH1D("dz_h1", "",
-                 profile_bins, dist_to_anode_low, dist_to_anode_up);
-    dz_h1.GetXaxis().SetTitle("distance from anode (cm)");
-    dz_h1.GetYaxis().SetTitle("z_flash - z_TPC (cm)");
+                 profile_bins, dist_to_anode_low, dist_to_anode_up)
+    dz_h1.GetXaxis().SetTitle("distance from anode (cm)")
+    dz_h1.GetYaxis().SetTitle("z_flash - z_TPC (cm)")
 
     rr_spreads = [None] * n_bins
     rr_means = [None] * n_bins
-    rr_bins =  100
-    rr_low = 0. # TODO: un hardcode
-    rr_up = 200. # TODO: un hardcode
+    rr_bins = 100
+    rr_low = 0.  # TODO: un hardcode
+    rr_up = 200.  # TODO: un hardcode
     rr_hist = TH2D("rr_hist", "PE Spread",
-               dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
-               rr_bins, rr_low, rr_up)
-    rr_hist.GetXaxis().SetTitle("distance from anode (cm)");
-    rr_hist.GetYaxis().SetTitle("RMS flash (cm)");
-    rr_prof = TProfile("rr_prof","Profile of PE Spread",
+                   dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
+                   rr_bins, rr_low, rr_up)
+    rr_hist.GetXaxis().SetTitle("distance from anode (cm)")
+    rr_hist.GetYaxis().SetTitle("RMS flash (cm)")
+    rr_prof = TProfile("rr_prof", "Profile of PE Spread",
                        profile_bins, dist_to_anode_low, dist_to_anode_up,
-                       rr_low, rr_up, profile_option);
-    rr_prof.GetXaxis().SetTitle("distance from anode (cm)");
-    rr_prof.GetYaxis().SetTitle("RMS flash (cm)");
+                       rr_low, rr_up, profile_option)
+    rr_prof.GetXaxis().SetTitle("distance from anode (cm)")
+    rr_prof.GetYaxis().SetTitle("RMS flash (cm)")
     rr_h1 = TH1D("rr_h1", "",
-                 profile_bins, dist_to_anode_low, dist_to_anode_up);
-    rr_h1.GetXaxis().SetTitle("distance from anode (cm)");
-    rr_h1.GetYaxis().SetTitle("RMS flash (cm)");
+                 profile_bins, dist_to_anode_low, dist_to_anode_up)
+    rr_h1.GetXaxis().SetTitle("distance from anode (cm)")
+    rr_h1.GetYaxis().SetTitle("RMS flash (cm)")
 
     pe_spreads = [None] * n_bins
     pe_means = [None] * n_bins
-    pe_bins =  50
-    pe_low = 0. # TODO: un hardcode
-    pe_up = 50. # TODO: un hardcode
+    pe_bins = 50
+    pe_low = 0.  # TODO: un hardcode
+    pe_up = 50.  # TODO: un hardcode
     pe_hist = TH2D("pe_hist", "Uncoated/Coated Ratio",
-               dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
-               pe_bins, pe_low, pe_up)
-    pe_hist.GetXaxis().SetTitle("distance from anode (cm)");
-    pe_hist.GetYaxis().SetTitle("ratio_{uncoated/coated}");
-    pe_prof = TProfile("pe_prof","Profile of Uncoated/Coated Ratio",
+                   dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
+                   pe_bins, pe_low, pe_up)
+    pe_hist.GetXaxis().SetTitle("distance from anode (cm)")
+    pe_hist.GetYaxis().SetTitle("ratio_{uncoated/coated}")
+    pe_prof = TProfile("pe_prof", "Profile of Uncoated/Coated Ratio",
                        profile_bins, dist_to_anode_low, dist_to_anode_up,
-                       pe_low, pe_up, profile_option);
-    pe_prof.GetXaxis().SetTitle("distance from anode (cm)");
-    pe_prof.GetYaxis().SetTitle("ratio_{uncoated/coated}");
+                       pe_low, pe_up, profile_option)
+    pe_prof.GetXaxis().SetTitle("distance from anode (cm)")
+    pe_prof.GetYaxis().SetTitle("ratio_{uncoated/coated}")
     pe_h1 = TH1D("pe_h1", "",
-                 profile_bins, dist_to_anode_low, dist_to_anode_up);
-    pe_h1.GetXaxis().SetTitle("distance from anode (cm)");
-    pe_h1.GetYaxis().SetTitle("ratio_{uncoated/coated}");
+                 profile_bins, dist_to_anode_low, dist_to_anode_up)
+    pe_h1.GetXaxis().SetTitle("distance from anode (cm)")
+    pe_h1.GetYaxis().SetTitle("ratio_{uncoated/coated}")
 
     score_hist_bins = 100
     score_hist_low = 0.
@@ -169,7 +170,7 @@ def generator(input_file, rootfile, gtrees, gbranches):
                                dist_to_anode_bins, dist_to_anode_low, dist_to_anode_up,
                                score_hist_bins, score_hist_low, score_hist_up*(3./5.))
     match_score_scatter.GetXaxis().SetTitle("distance from anode (cm)")
-    match_score_scatter.GetYaxis().SetTitle("match score (arbitrary)");
+    match_score_scatter.GetYaxis().SetTitle("match score (arbitrary)")
     match_score_hist = TH1D("match_score", "Match Score",
                             score_hist_bins, score_hist_low, score_hist_up)
     match_score_hist.GetXaxis().SetTitle("match score (arbitrary)")
@@ -210,24 +211,28 @@ def generator(input_file, rootfile, gtrees, gbranches):
         slice = e.nuvtx_x
         uncoated_coated_ratio = 100.*e.flash_unpe/e.flashpe
         # calculate match score
-        isl = int(slice/5.) # TODO: un-hardcode
+        isl = int(slice/5.)  # TODO: un-hardcode
         # if (isl>) isl=19;
         # if (isl<0) isl=0;
         score = 0.
         if dy_spreads[isl] <= 1e-8:
-            print(f"Warning zero.\n slice: {slice}. isl {isl}. dy_spreads[isl]{dy_spreads[isl]} ")
+            print("Warning zero spread.\n",
+                  f"slice: {slice}. isl: {isl}. dy_spreads[isl]: {dy_spreads[isl]} ")
             dy_spreads[isl] = dy_spreads[isl+1]
         if dz_spreads[isl] <= 1e-8:
-            print(f"Warning zero.\n slice: {slice}. isl {isl}. dz_spreads[isl]{dz_spreads[isl]} ")
+            print("Warning zero spread.\n",
+                  f"slice: {slice}. isl: {isl}. dz_spreads[isl]: {dz_spreads[isl]} ")
             dz_spreads[isl] = dz_spreads[isl+1]
         if rr_spreads[isl] <= 1e-8:
-            print(f"Warning zero.\n slice: {slice}. isl {isl}. rr_spreads[isl]{rr_spreads[isl]} ")
+            print("Warning zero spread.\n",
+                  f"slice: {slice}. isl: {isl}. rr_spreads[isl]: {rr_spreads[isl]} ")
             rr_spreads[isl] = rr_spreads[isl+1]
         if pe_spreads[isl] <= 1e-8:
-            print(f"Warning zero.\n slice: {slice}. isl {isl}. pe_spreads[isl]{pe_spreads[isl]} ")
+            print("Warning zero spread.\n",
+                  f"slice: {slice}. isl: {isl}. pe_spreads[isl]: {pe_spreads[isl]} ")
             pe_spreads[isl] = pe_spreads[isl+1]
-        score += abs(abs(e.flash_y-e.nuvtx_y)- dy_means[isl])/dy_spreads[isl]
-        score += abs(abs(e.flash_z-e.nuvtx_z)- dz_means[isl])/dz_spreads[isl]
+        score += abs(abs(e.flash_y-e.nuvtx_y) - dy_means[isl])/dy_spreads[isl]
+        score += abs(abs(e.flash_z-e.nuvtx_z) - dz_means[isl])/dz_spreads[isl]
         score += abs(e.flash_r-rr_means[isl])/rr_spreads[isl]
         # score += abs(uncoated_coated_ratio-pe_means[isl])/pe_spreads[isl]
         match_score_scatter.Fill(slice, score)
@@ -235,51 +240,60 @@ def generator(input_file, rootfile, gtrees, gbranches):
     hfile = ROOT.gROOT.FindObject('fm_metrics_sbnd.root')
     if hfile:
         hfile.Close()
-    hfile = TFile('fm_metrics_sbnd.root', 'RECREATE', 'Simple flash matching metrics for SBND')
-    dy_hist.Write();
-    dy_prof.Write();
-    dy_h1.Write();
-    dz_hist.Write();
-    dz_prof.Write();
-    dz_h1.Write();
-    rr_hist.Write();
-    rr_prof.Write();
-    rr_h1.Write();
-    pe_hist.Write();
-    pe_prof.Write();
-    pe_h1.Write();
-    match_score_scatter.Write();
-    match_score_hist.Write();
-    hfile.Close();
+    hfile = TFile('fm_metrics_sbnd.root', 'RECREATE',
+                  'Simple flash matching metrics for SBND')
+    dy_hist.Write()
+    dy_prof.Write()
+    dy_h1.Write()
+    dz_hist.Write()
+    dz_prof.Write()
+    dz_h1.Write()
+    rr_hist.Write()
+    rr_prof.Write()
+    rr_h1.Write()
+    pe_hist.Write()
+    pe_prof.Write()
+    pe_h1.Write()
+    match_score_scatter.Write()
+    match_score_hist.Write()
+    hfile.Close()
 
-    canv = TCanvas("canv");
+    canv = TCanvas("canv")
 
-    dy_hist.Draw();
-    crosses = TGraphErrors(n_bins, array('f',xvals), array('f',dy_means), array('f',xerrs), array('f',dy_spreads))
+    dy_hist.Draw()
+    crosses = TGraphErrors(n_bins,
+                           array('f', xvals), array('f', dy_means),
+                           array('f', xerrs), array('f', dy_spreads))
     crosses.SetLineColor(9)
     crosses.SetLineWidth(3)
     crosses.Draw("Psame")
     canv.Print("dy.pdf")
-    canv.Update();
+    canv.Update()
 
-    dz_hist.Draw();
-    crosses = TGraphErrors(n_bins, array('f',xvals), array('f',dz_means), array('f',xerrs), array('f',dz_spreads))
+    dz_hist.Draw()
+    crosses = TGraphErrors(n_bins,
+                           array('f', xvals), array('f', dz_means),
+                           array('f', xerrs), array('f', dz_spreads))
     crosses.SetLineColor(9)
     crosses.SetLineWidth(3)
     crosses.Draw("Psame")
     canv.Print("dz.pdf")
-    canv.Update();
+    canv.Update()
 
-    rr_hist.Draw();
-    crosses = TGraphErrors(n_bins, array('f',xvals), array('f',rr_means), array('f',xerrs), array('f',rr_spreads))
+    rr_hist.Draw()
+    crosses = TGraphErrors(n_bins,
+                           array('f', xvals), array('f', rr_means),
+                           array('f', xerrs), array('f', rr_spreads))
     crosses.SetLineColor(9)
     crosses.SetLineWidth(3)
     crosses.Draw("Psame")
     canv.Print("rr.pdf")
-    canv.Update();
+    canv.Update()
 
-    pe_hist.Draw();
-    crosses = TGraphErrors(n_bins, array('f',xvals), array('f',pe_means), array('f',xerrs), array('f',pe_spreads))
+    pe_hist.Draw()
+    crosses = TGraphErrors(n_bins,
+                           array('f', xvals), array('f', pe_means),
+                           array('f', xerrs), array('f', pe_spreads))
     crosses.SetLineColor(9)
     crosses.SetLineWidth(3)
     crosses.Draw("Psame")
