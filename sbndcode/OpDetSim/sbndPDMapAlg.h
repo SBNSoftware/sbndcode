1d117528a4f492636c4895ca4d1f04eb99a4f338
Iker de Icaza Astiz <icaza@fnal.gov>
2020-03-06 09:50:34 -0600

    Add authorship and some comments about these changes

--- sbndcode/OpDetSim/sbndPDMapAlg.h
+++ sbndcode/OpDetSim/sbndPDMapAlg.h
@@ -1,9 +1,20 @@
 ////////////////////////////////////////////////////////////////////////
 // File:        sbndPDMapAlg.h
-// Authors: Laura Paulucci and Franciole Marinho
+// Authors: Laura Paulucci, Franciole Marinho, and Iker de Icaza
 //
-// This class stores the SBND PDS type map and implements a few functions
+// Updates: 2020-03, v08_45_00. Iker de Icaza icaza@fnal.gov
+//          Added properties to PDS MAP and functions to access these.
 //
+// This class stores the SBND PDS Map and channel's properties;
+// also implements functions to access these.
+//
+// As of version v08_45_00 the PDS Map has:
+// channel: 0 to 503
+// pd_type: pmt, barepmt, xarapuca, xarapucaT1, xarapucaT2, arapucaT1, arapucaT2
+// pds_box: -12, to 12; skipping 0
+// sensible_to: VUV or VIS
+// tpc: 0, 1
+// xarapuca_pos: top, bottom, null
 ////////////////////////////////////////////////////////////////////////
 
 #ifndef SBND_OPDETSIM_SBNDPDMAPALG_H
