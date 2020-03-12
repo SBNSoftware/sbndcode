1a73d9bbcd9b9ba075ca70bee3338875e5e03156
Iker de Icaza Astiz <icaza@fnal.gov>
2020-03-12 08:48:41 -0500

    Revert "Reading data from files in digitizer"
    This reverts commit 520937d0f4efe96507fa7e8090a53d1f3958ef10
    and effectevely also commit aab96f799a35693de5218d3cfeaf005a6571729a

--- sbndcode/OpDetSim/DigiPMTSBNDAlg.h
+++ sbndcode/OpDetSim/DigiPMTSBNDAlg.h
@@ -34,6 +34,7 @@
 #include "lardata/DetectorInfoServices/LArPropertiesService.h"
 
 #include "TMath.h"
+//#include "TRandom3.h"
 #include "TF1.h"
 #include "TH1D.h"
 
@@ -56,8 +57,8 @@ namespace opdet {
       double PMTSaturation; //in number of p.e.
       double QEDirect; //PMT quantum efficiency for direct (VUV) light
       double QERefl; //PMT quantum efficiency for reflected (TPB converted) light
-      std::string TPBFile; //File containing timing emission structure for TPB
-      std::string TestBenchPEFile; //File containing single PE profile from data
+      // std::string TPBFile; //File containing timing emission structure for TPB
+      // std::string TestBenchPEFile; //File containing single PE profile from data
       bool SinglePEmodel; //Model for single pe response, false for ideal, true for test bench meas
 
       detinfo::LArProperties const* larProp = nullptr; //< LarProperties service provider.
@@ -88,9 +89,6 @@ namespace opdet {
     //int fSinglePEmodel;
     double sigma1;
     double sigma2;
-    std::string fTPBFile;
-    std::string fTestBenchPEFile;
-    std::vector<double> xvec; //auxiliar vector for creating histograms
 
     CLHEP::HepRandomEngine* fEngine; //!< Reference to art-managed random-number engine
 
@@ -110,7 +108,6 @@ namespace opdet {
     void AddDarkNoise(std::vector<double>& wave); //add dark noise
     double FindMinimumTime(sim::SimPhotons const&, int ch, std::string pdtype, std::map<int, sim::SimPhotons> auxmap);
     double FindMinimumTimeLite(sim::SimPhotonsLite const& litesimphotons, int ch, std::string pdtype, std::map<int, sim::SimPhotonsLite> auxmap);
-    std::vector<double> AssignVector(std::string filename);
   };//class DigiPMTSBNDAlg
 
   class DigiPMTSBNDAlgMaker {
@@ -184,16 +181,6 @@ namespace opdet {
         Name("SinglePEmodel"),
         Comment("Model used for single PE response of PMT. =0 is ideal, =1 is testbench")
       };
-
-       fhicl::Atom<std::string> tpbFile {
-          Name("TPBFile"),
-          Comment("File containing timing emission distribution for TPB")
-       };
-
-       fhicl::Atom<std::string> testbenchpeFile {
-          Name("TestBenchPEFile"),
-          Comment("File containing single pe pulse from data")
-       };
 
     };    //struct Config
 
