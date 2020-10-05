9802c71717140deceaffd352824fc4d841d7e484
Diana Patricia Mendez <dmendezme@bnl.gov>
2020-10-05 13:03:41 -0500

    Adding directory for track reconstruction validation.

---
+++ sbndcode/RecoValidation/ValidateTracks_module.cc
@@ -0,0 +1,95 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       ValidateTracks
+// Plugin Type: analyzer (art v3_05_01)
+// File:        ValidateTracks_module.cc
+//
+// Generated at Sat Oct  3 17:53:53 2020 by Diana Mendez mendez using cetskelgen
+// from cetlib version v3_10_00.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "canvas/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// Additional framework includes
+#include "art_root_io/TFileService.h"
+
+// ROOT includes
+#include <TTree.h>
+
+namespace testValidation {
+  class ValidateTracks;
+}
+
+
+class testValidation::ValidateTracks : public art::EDAnalyzer {
+public:
+  explicit ValidateTracks(fhicl::ParameterSet const& p);
+  // The compiler-generated destructor is fine for non-base
+  // classes without bare pointers or other resource use.
+
+  // Plugins should not be copied or assigned.
+  ValidateTracks(ValidateTracks const&) = delete;
+  ValidateTracks(ValidateTracks&&) = delete;
+  ValidateTracks& operator=(ValidateTracks const&) = delete;
+  ValidateTracks& operator=(ValidateTracks&&) = delete;
+
+  // Required functions.
+  void analyze(art::Event const& e) override;
+
+  // Selected optional functions.
+  void beginJob() override;
+  void endJob() override;
+
+private:
+
+  // Declare member data here.
+  // Output tree declaration
+  TTree *fTree;
+
+  // Variable to fill the output tree with
+  unsigned int fEventID;
+
+};
+
+
+testValidation::ValidateTracks::ValidateTracks(fhicl::ParameterSet const& p)
+  : EDAnalyzer{p}  // ,
+  // More initializers here.
+{
+  // Call appropriate consumes<>() for any products to be retrieved by this module.
+}
+
+void testValidation::ValidateTracks::analyze(art::Event const& e)
+{
+  // Implementation of required member function here.
+  // Define out event ID variable
+  fEventID = e.id().event();
+
+  // Fill the output tree with all the relevant variables
+  fTree->Fill();
+}
+
+void testValidation::ValidateTracks::beginJob()
+{
+  // Implementation of optional member function here.
+  // The TFileService is used to define the TTree and writing it to the output file
+  art::ServiceHandle<art::TFileService> tfs;
+  fTree = tfs->make<TTree>("tree", "Analysis Output Tree");
+
+  //Add branches to out tree
+  fTree->Branch("eventID", &fEventID, "eventID/i");
+}
+
+void testValidation::ValidateTracks::endJob()
+{
+  // Implementation of optional member function here.
+}
+
+DEFINE_ART_MODULE(testValidation::ValidateTracks)
