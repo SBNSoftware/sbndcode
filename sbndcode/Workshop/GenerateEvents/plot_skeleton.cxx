80356c5befccfc385bfffe6b77e72c983092c702
Krishan Mistry krishan.mistry-5@postgrad.manchester.ac.uk <kmistry@wn1205240.in.tier2.hep.manchester.ac.uk>
2020-11-02 18:52:37 +0000

    plot_skeleton.cxx

--- sbndcode/Workshop/GenerateEvents/plot_skeleton.cxx
+++ sbndcode/Workshop/GenerateEvents/plot_skeleton.cxx
@@ -23,7 +23,7 @@ void plot_skeleton() {
   // Each entry is an art::Event
   TTreeReader anatree_reader("analysistree/anatree", &f);
 
-  // Set up a reader for values in the tree
+  // Set up a reader for values in the tree -- use this as an example for retrieving values from the TTree
   // You can find the names by opening the tree in a TBrowser
   TTreeReaderValue<int> n_particles (anatree_reader, "geant_list_size"); // Number of particles simulated by g4
   TTreeReaderArray<float> P (anatree_reader, "P"); // Array of momenta
@@ -31,16 +31,27 @@ void plot_skeleton() {
   // Define histograms
   TH1D* hMomentum = new TH1D("hMomentum", "", 20, 0, 2);
 
-  // Loop over the analysis tree
+  // Loop over the analysis tree entries
   while (anatree_reader.Next()) {
 
     // Loop over the number of particles in the event
     for(size_t i = 0; i < *n_particles; i++){
+
+      // Check if its a muon or proton originating from the vertex
+      // and store their properties in a variable for use outside
+      // this loop
+     
+      // Fill a histogram with all particle momentums -- just an example
       hMomentum->Fill(P[i]);
     }
+      
+    // Calculate the angle between the muon and proton 
+      
+    // Fill a histogram with the angle
 
   }
 
+  // Save your histograms with the DrawHist funtion here
   DrawHist(hMomentum, "momentum", "P [GeV]");
 
 }
