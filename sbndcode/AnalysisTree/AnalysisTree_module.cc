dff4906a9a923d7b8fe157edc2c524203489b90a
Will Foreman <wforeman@iit.edu>
2021-03-07 14:41:30 -0600

    Enclose problematic section of code in a try bracket

--- sbndcode/AnalysisTree/AnalysisTree_module.cc
+++ sbndcode/AnalysisTree/AnalysisTree_module.cc
@@ -2156,15 +2156,15 @@ void sbnd::AnalysisTree::analyze(const art::Event& evt)
           // tbrooks: use TrackIDEs rather than eveTrackIDEs because the eve ID doesn't always seem to correspond to the g4 track FIXME may need further investigation
                 std::vector<sim::TrackIDE> eveIDs = bt_serv->HitToTrackIDEs(clockData, hitlist[i]);
 	        for (size_t e = 0; e<eveIDs.size(); e++){
-	          try {
+		  try {
 		    art::Ptr<simb::MCTruth> ev_mctruth = pi_serv->TrackIdToMCTruth_P(eveIDs[e].trackID);
 		    //mctruthemap[ev_mctruth]+=eveIDs[e].energy;
 		    if (ev_mctruth->Origin() == simb::kCosmicRay) isCosmics = true;
-		  } 
+		  }
 		  catch(...){
 		    std::cout<<"Exception thrown matching eveTrackId "<<eveIDs[e].trackID<<" to MCTruth\n";
 		  }
-		}
+	        }
 	        //}
 	      }
 	      isfirsttime = false;
