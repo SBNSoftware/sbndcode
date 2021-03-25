4f96c6128343051ac08bcc3a9216fac21c40e7b4
Marco Del Tutto <mdeltutt@fnal.gov>
2021-03-25 16:44:27 -0500

    Update

--- sbndcode/SBNDPrism/PrismAnalyzer_module.cc
+++ sbndcode/SBNDPrism/PrismAnalyzer_module.cc
@@ -21,6 +21,7 @@
 
 #include "TTree.h"
 #include "TVector3.h"
+#include "TDatabasePDG.h"
 
 #include "nusimdata/SimulationBase/MCTruth.h"
 #include "nusimdata/SimulationBase/MCParticle.h"
@@ -49,10 +50,16 @@ public:
   void endJob();
 
 private:
+  /// Returns the off-axis angle in radians
   float GetOffAxisAngle(float x, float y, float z);
+
+  /// Returns the reco energy using QE formula
+  float GetEnergyQE(float muon_energy, float muon_px, float muon_py, float muon_pz);
 
   std::string _mctruth_producer = "generator";
   std::vector<float> _beam_origin = {-0.457 * 100, 0 * 100, 110 * 100}; // cm
+
+  const TDatabasePDG *_pdg_db = TDatabasePDG::Instance();
 
   TTree* _tree;
 
@@ -73,6 +80,13 @@ private:
   float _nu_roaa; ///< Real Off-Axis Angle (angle between neutrino parent p and neutrino p)
   float _nu_l; ///< L [cm]: Distance the neutrino travelled from production to interaction point
   int _nu_decay; ///< Neutrino parent decay type (see dkproc_t in dk2nu)
+  float _nu_mu_e; ///< Final state muon (if any) energy
+  float _nu_mu_px; ///< Final state muon (if any) px
+  float _nu_mu_py; ///< Final state muon (if any) py
+  float _nu_mu_pz; ///< Final state muon (if any) pz
+  float _nu_e_reco; ///< Neutrino reconstructe energy using QE formula
+  std::vector<int> _pars_pdg; ///< All other particles produced - pdg code
+  std::vector<float> _pars_e; ///< All other particles produced - energy
 
   float _nu_prod_vtx_x;
   float _nu_prod_vtx_y;
@@ -86,7 +100,9 @@ private:
   float _p_dpy; ///< Neutrino parent py at neutrino production vertex
   float _p_dpz; ///< Neutrino parent pz at neutrino production vertex
 
-  int _n_pi0;
+  int _nu_pip_mult; ///< Pi0 multiplicity
+  int _nu_pi0_mult; ///< Pi plus multiplicity
+  int _nu_p_mult; ///< Proton multiplicity
 
   TTree* _sr_tree;
   int _sr_run, _sr_subrun;
@@ -122,6 +138,11 @@ PrismAnalyzer::PrismAnalyzer(fhicl::ParameterSet const& p)
   _tree->Branch("nu_roaa", &_nu_roaa, "nu_roaa/F");
   _tree->Branch("nu_l", &_nu_l, "nu_l/F");
   _tree->Branch("nu_decay", &_nu_decay, "nu_decay/I");
+  _tree->Branch("nu_mu_e", &_nu_mu_e, "nu_mu_e/F");
+  _tree->Branch("nu_mu_px", &_nu_mu_px, "nu_mu_px/F");
+  _tree->Branch("nu_mu_py", &_nu_mu_py, "nu_mu_py/F");
+  _tree->Branch("nu_mu_pz", &_nu_mu_pz, "nu_mu_pz/F");
+  _tree->Branch("nu_e_reco", &_nu_e_reco, "nu_e_reco/F");
 
   _tree->Branch("nu_prod_vtx_x", &_nu_prod_vtx_x, "nu_prod_vtx_x/F");
   _tree->Branch("nu_prod_vtx_y", &_nu_prod_vtx_y, "nu_prod_vtx_y/F");
@@ -130,12 +151,17 @@ PrismAnalyzer::PrismAnalyzer(fhicl::ParameterSet const& p)
   _tree->Branch("nu_prod_vtx_y_beam", &_nu_prod_vtx_y_beam, "nu_prod_vtx_y_beam/F");
   _tree->Branch("nu_prod_vtx_z_beam", &_nu_prod_vtx_z_beam, "nu_prod_vtx_z_beam/F");
 
+  _tree->Branch("pars_pdg", "std::vector<int>", &_pars_pdg);
+  _tree->Branch("pars_e", "std::vector<float>", &_pars_e);
+
   _tree->Branch("p_type", &_p_type, "p_type/I");
   _tree->Branch("p_dpx", &_p_dpx, "p_dpx/F");
   _tree->Branch("p_dpy", &_p_dpy, "p_dpy/F");
   _tree->Branch("p_dpz", &_p_dpz, "p_dpz/F");
 
-  _tree->Branch("n_pi0", &_n_pi0, "n_pi0/I");
+  _tree->Branch("nu_pip_mult", &_nu_pip_mult, "nu_pip_mult/I");
+  _tree->Branch("nu_pi0_mult", &_nu_pi0_mult, "nu_pi0_mult/I");
+  _tree->Branch("nu_p_mult", &_nu_p_mult, "nu_p_mult/I");
 
   _sr_tree = fs->make<TTree>("pottree","");
   _sr_tree->Branch("run", &_sr_run, "run/I");
@@ -168,12 +194,16 @@ void PrismAnalyzer::analyze(art::Event const& e)
   //
   art::FindManyP<simb::MCFlux> mct_to_mcf (mct_h, e, _mctruth_producer);
 
+  _pars_pdg.clear();
+  _pars_e.clear();
+  _nu_pi0_mult = _nu_pip_mult = _nu_p_mult = 0;
+
   //
   // Loop over the neutrino interactions in this event
   //
   for (size_t i = 0; i < mct_v.size(); i++) {
     if (mct_v.at(i)->Origin() != simb::kBeamNeutrino) {
-      std::cout << "[PrismAnalyzer] MCTruth from generator does not gave neutrino origin?!" << std::endl;
+      std::cout << "[PrismAnalyzer] MCTruth from generator does not have neutrino origin?!" << std::endl;
     }
 
     _nu_e = mct_v[i]->GetNeutrino().Nu().E();
@@ -218,20 +248,49 @@ void PrismAnalyzer::analyze(art::Event const& e)
     _p_dpz = mcf->fpdpz;
     _nu_roaa = TVector3(_nu_px, _nu_py, _nu_pz).Angle(TVector3(_p_dpx, _p_dpy, _p_dpz));
 
+    _nu_mu_e = -9999;
+    _nu_mu_px = -9999;
+    _nu_mu_py = -9999;
+    _nu_mu_pz = -9999;
+    _nu_e_reco = -9999;
 
-    _n_pi0 = 0;
     for (int p = 0; p < mct_v[i]->NParticles(); p++) {
       auto const & mcp = mct_v[i]->GetParticle(p);
+
       if (mcp.StatusCode() != 1) continue;
-      if (mcp.PdgCode() != 111) continue;
-      _n_pi0++;
+
+      _pars_pdg.push_back(mcp.PdgCode());
+      _pars_e.push_back(mcp.E());
+
+      if (mcp.PdgCode() == 111) {
+        _nu_pi0_mult++;
+      } else if (std::abs(mcp.PdgCode()) == 211) {
+        _nu_pip_mult++;
+      }
+      else if (std::abs(mcp.PdgCode()) == 2112) {
+        _nu_p_mult++;
+      } else if (std::abs(mcp.PdgCode()) == 13) {
+        _nu_mu_e = mcp.E();
+        _nu_mu_px = mcp.Px();
+        _nu_mu_py = mcp.Py();
+        _nu_mu_pz = mcp.Pz();
+        _nu_e_reco = GetEnergyQE(mcp.E(), mcp.Px(), mcp.Py(), mcp.Pz());
+      }
     }
-    for (int p = 0; p < mct_v[i]->NParticles(); p++) {
-      auto const & mcp = mct_v[i]->GetParticle(p);
-      if (mcp.StatusCode() != 1) continue;
-      if (mcp.PdgCode() != 2112) continue;
-      std::cout << "Got neutron, mother is " << mcp.Mother() << ", energy is " << mcp.E() << std::endl;
-    }
+
+    // _n_pi0 = 0;
+    // for (int p = 0; p < mct_v[i]->NParticles(); p++) {
+    //   auto const & mcp = mct_v[i]->GetParticle(p);
+    //   if (mcp.StatusCode() != 1) continue;
+    //   if (mcp.PdgCode() != 111) continue;
+    //   _n_pi0++;
+    // }
+    // for (int p = 0; p < mct_v[i]->NParticles(); p++) {
+    //   auto const & mcp = mct_v[i]->GetParticle(p);
+    //   if (mcp.StatusCode() != 1) continue;
+    //   if (mcp.PdgCode() != 2112) continue;
+    //   std::cout << "Got neutron, mother is " << mcp.Mother() << ", energy is " << mcp.E() << std::endl;
+    // }
 
     _tree->Fill();
   }
@@ -265,6 +324,27 @@ float PrismAnalyzer::GetOffAxisAngle(float x, float y, float z) {
 
   return beam.Angle(nu_vtx_beam);
 }
+
+float PrismAnalyzer::GetEnergyQE(float muon_energy, float px, float py, float pz) {
+  float p_mass = _pdg_db->GetParticle(2212)->Mass(); // proton mass
+  float n_mass = _pdg_db->GetParticle(2112)->Mass(); // neutron mass
+  float m_mass = _pdg_db->GetParticle(13)->Mass(); // muon mass
+  float Eb = 0.030; // binding energy
+
+  float p = std::sqrt(px * px + py * py + pz * pz);
+  // float pxy = TMath::Sqrt( px*px + py*py )
+  float theta = TMath::ACos(pz / p);
+
+  float num = p_mass * p_mass;
+  num -= m_mass * m_mass;
+  num -= (n_mass - Eb) * (n_mass - Eb);
+  num += 2 * (n_mass - Eb) * muon_energy;
+
+  float den = 2. * (n_mass - Eb - muon_energy + p * std::cos(theta));
+
+  return num / den;
+}
+
 
 void PrismAnalyzer::beginSubRun(art::SubRun const& sr) {
 
