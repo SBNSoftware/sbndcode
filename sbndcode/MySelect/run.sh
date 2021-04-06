67f3f7a5832fca6e3ccc2bdfa1d0c4b7195a9f2d
Lan Nguyen <vclnguyen1@sheffield.ac.uk>
2021-04-06 05:16:30 -0500

    change output path

--- sbndcode/MySelect/run.sh
+++ sbndcode/MySelect/run.sh
@@ -1,6 +1,8 @@
 mrbsetenv && mrbslp
 
-lar -c run_Select.fcl -S /pnfs/sbnd/scratch/users/lnguyen/prodcorsika_cosmics_job/v09_09_00/gen/prodcorsika_cosmics/crossing_muons_etime/files.list
+#lar -c run_Select.fcl -S /pnfs/sbnd/scratch/users/lnguyen/prodcorsika_cosmics_job/v09_09_00/gen/prodcorsika_cosmics/crossing_muons_etime/files.list
+
+lar -c run_Select.fcl -S /pnfs/sbnd/scratch/users/lnguyen/prodcorsika_cosmics_no_diffusion_job/v09_09_00/gen/prodcorsika_cosmics/no_diffusion/files.list
 
 #lar -c run_Select.fcl $(samweb -e sbnd get-file-access-url --schema root reco2-6c529450-fcb6-4c24-b440-001c46b12a3d.root )
 
