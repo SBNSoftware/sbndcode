d01652f2caf019e857e76624eb4fce24088948c2
Will Foreman <wforeman@iit.edu>
2021-06-15 13:14:41 -0500

    Removed all unused noise-related configurations from SimWire, and made ADC saturation fcl-configurable

--- sbndcode/DetectorSim/SimWireSBND_module.cc
+++ sbndcode/DetectorSim/SimWireSBND_module.cc
@@ -81,15 +81,9 @@ public:
 
 private:
 
-  void GenNoiseInTime(std::vector<float> &noise, double noise_factor) const;
-  void GenNoiseInFreq(std::vector<float> &noise, double noise_factor) const;
-
   std::string            fDriftEModuleLabel;///< module making the ionization electrons
   raw::Compress_t        fCompression;      ///< compression type to use
 
-  double                 fNoiseWidth;       ///< exponential noise width (kHz)
-  double                 fNoiseRand;        ///< fraction of random "wiggle" in noise in freq. spectrum
-  double                 fLowCutoff;        ///< low frequency filter cutoff (kHz)
   size_t                 fNTicks;           ///< number of ticks of the clock
   double                 fSampleRate;       ///< sampling rate in ns
   unsigned int           fNTimeSamples;     ///< number of ADC readout samples in all readout frames (per event)
@@ -97,24 +91,17 @@ private:
   float                  fInductionPed;     ///< ADC value of baseline for induction plane
   float                  fCollectionSat;    ///< ADC value of pre-amp saturation for collection plane
   float                  fInductionSat;     ///< ADC value of pre-amp saturation for induction plane
+  float                  fAdcSaturation;    ///< ADC saturation value
   float                  fBaselineRMS;      ///< ADC value of baseline RMS within each channel
   TH1D*                  fNoiseDist;        ///< distribution of noise counts
-  bool fGetNoiseFromHisto;                  ///< if True -> Noise from Histogram of Freq. spectrum
-  bool fGenNoiseInTime;                     ///< if True -> Noise with Gaussian dsitribution in Time-domain
-  bool fGenNoise;                           ///< if True -> Gen Noise. if False -> Skip noise generation entierly
-
+  bool                   fGenNoise;         ///< if True -> Gen Noise. if False -> Skip noise generation entierly
+  
   art::ServiceHandle<ChannelNoiseService> noiseserv;
 
-
-  std::string fNoiseFileFname;
-  std::string fNoiseHistoName;
-  TH1D*                  fNoiseHist = nullptr; ///< distribution of noise counts
-
-  std::map< double, int > fShapingTimeOrder;
   std::string fTrigModName;                 ///< Trigger data product producer name
   //define max ADC value - if one wishes this can
   //be made a fcl parameter but not likely to ever change
-  static constexpr float adcsaturation{4095};
+  //static constexpr float adcsaturation{4095};
 
   //CLHEP::HepRandomEngine& fNoiseEngine;
   CLHEP::HepRandomEngine& fPedestalEngine;
@@ -143,56 +130,49 @@ SimWireSBND::SimWireSBND(fhicl::ParameterSet const& pset)
 //-------------------------------------------------
 SimWireSBND::~SimWireSBND()
 {
-  delete fNoiseHist;
 }
 
 //-------------------------------------------------
 void SimWireSBND::reconfigure(fhicl::ParameterSet const& p)
 {
   fDriftEModuleLabel = p.get< std::string         >("DriftEModuleLabel");
-  fNoiseWidth        = p.get< double              >("NoiseWidth");
-  fNoiseRand         = p.get< double              >("NoiseRand");
-  fLowCutoff         = p.get< double              >("LowCutoff");
-  fGetNoiseFromHisto = p.get< bool                >("GetNoiseFromHisto");
-  fGenNoiseInTime    = p.get< bool                >("GenNoiseInTime");
   fGenNoise          = p.get< bool                >("GenNoise");
   fCollectionPed     = p.get< float               >("CollectionPed",690.);
   fInductionPed      = p.get< float               >("InductionPed",2100.);
   fCollectionSat     = p.get< float               >("CollectionSat",2922.);
   fInductionSat      = p.get< float               >("InductionSat",1247.);
   fBaselineRMS       = p.get< float               >("BaselineRMS");
-
+  fAdcSaturation     = p.get< float               >("AdcSaturation",4095);
   fTrigModName       = p.get< std::string         >("TrigModName");
 
   //Map the Shaping times to the entry position for the noise ADC
   //level in fNoiseFactInd and fNoiseFactColl
-  fShapingTimeOrder = { {0.5, 0 }, {1.0, 1}, {2.0, 2}, {3.0, 3} };
+  //fShapingTimeOrder = { {0.5, 0 }, {1.0, 1}, {2.0, 2}, {3.0, 3} };
 
+  //if (fGetNoiseFromHisto)
+  //{
+  //  fNoiseHistoName = p.get< std::string         >("NoiseHistoName");
 
-  if (fGetNoiseFromHisto)
-  {
-    fNoiseHistoName = p.get< std::string         >("NoiseHistoName");
+  //  cet::search_path sp("FW_SEARCH_PATH");
+  //  sp.find_file(p.get<std::string>("NoiseFileFname"), fNoiseFileFname);
 
-    cet::search_path sp("FW_SEARCH_PATH");
-    sp.find_file(p.get<std::string>("NoiseFileFname"), fNoiseFileFname);
+  //  TFile in(fNoiseFileFname.c_str(), "READ");
+  //  if (!in.IsOpen()) {
+  //    throw art::Exception(art::errors::FileOpenError)
+  //      << "Could not find Root file '" << fNoiseHistoName
+  //      << "' for noise histogram\n";
+  //  }
+  //  fNoiseHist = (TH1D*) in.Get(fNoiseHistoName.c_str());
 
-    TFile in(fNoiseFileFname.c_str(), "READ");
-    if (!in.IsOpen()) {
-      throw art::Exception(art::errors::FileOpenError)
-        << "Could not find Root file '" << fNoiseHistoName
-        << "' for noise histogram\n";
-    }
-    fNoiseHist = (TH1D*) in.Get(fNoiseHistoName.c_str());
-
-    if (!fNoiseHist) {
-      throw art::Exception(art::errors::NotFound)
-        << "Could not find noise histogram '" << fNoiseHistoName
-        << "' in Root file '" << in.GetPath() << "'\n";
-    }
-    // release the histogram from its file; now we own it
-    fNoiseHist->SetDirectory(nullptr);
-    in.Close();
-  }
+  //  if (!fNoiseHist) {
+  //    throw art::Exception(art::errors::NotFound)
+  //      << "Could not find noise histogram '" << fNoiseHistoName
+  //      << "' in Root file '" << in.GetPath() << "'\n";
+  //  }
+  //  // release the histogram from its file; now we own it
+  //  fNoiseHist->SetDirectory(nullptr);
+  //  in.Close();
+  //}
 
   //detector properties information
   auto const detProp = art::ServiceHandle<detinfo::DetectorPropertiesService const>()->DataForJob();
@@ -305,53 +285,6 @@ void SimWireSBND::produce(art::Event& evt)
 
     }
     std::vector<float> noisetmp(fNTicks, 0.);
-    /*
-    //If not using new noise, use old method.
-    if(fUseNewNoise==false) {
-      //Generate Noise:
-      //std::cout<<"Using old noise method"<<std::endl;
-      size_t view = (size_t)geo->View(chan);
-
-      double noise_factor;
-      auto tempNoiseVec = sss->GetNoiseFactVec();
-      double shapingTime = 2.0; //sss->GetShapingTime(chan);
-      double asicGain = sss->GetASICGain(chan);
-
-      //std::cout << "Sim params: " << chan << " " << shapingTime << " " << asicGain << std::endl;
-
-      if (fShapingTimeOrder.find( shapingTime ) != fShapingTimeOrder.end() ) {
-	noise_factor = tempNoiseVec[view].at( fShapingTimeOrder.find( shapingTime )->second );
-	noise_factor *= asicGain/4.7;
-      }
-      else {
-	throw cet::exception("SimWireSBND")
-	  << "\033[93m"
-	  << "Shaping Time recieved from signalshapingservices_sbnd.fcl is not one of the allowed values"
-	  << std::endl
-	  << "Allowed values: 0.5, 1.0, 2.0, 3.0 us"
-	  << "\033[00m"
-	  << std::endl;
-      }
-
-
-      if (fGenNoise) {
-	if (fGenNoiseInTime) {
-	  GenNoiseInTime(noisetmp, noise_factor);
-	} else {
-	  GenNoiseInFreq(noisetmp, noise_factor);
-	}
-      }
-
-    } // End of old noise.
-
-    // Else if doing new noise calculations.
-    //else if(fUseNewNoise==true){
-    else {  
-      //std::cout<<"Using new noise method"<<std::endl;
-      noiseserv->addNoise(chan,noisetmp);
-    } // End of new noise.
-
-*/
 
     // Add noise to channel.
     if( fGenNoise ) noiseserv->addNoise(clockData, chan,noisetmp);
@@ -383,8 +316,8 @@ void SimWireSBND::produce(art::Event& evt)
         fNoiseDist->Fill(noisetmp.at(i));
 
       //allow for ADC saturation
-      if ( adcval > adcsaturation )
-        adcval = adcsaturation;
+      if ( adcval > fAdcSaturation )
+        adcval = fAdcSaturation;
       //don't allow for "negative" saturation
       if ( adcval < 0 )
         adcval = 0;
@@ -410,90 +343,9 @@ void SimWireSBND::produce(art::Event& evt)
   }// end loop over channels
 
   evt.put(std::move(digcol));
-}
+
+}//produce()
 
 
-  /*
-//-------------------------------------------------
-  void SimWireSBND::GenNoiseInTime(std::vector<float> &noise, double noise_factor) const
-{
-  CLHEP::RandGaussQ rGauss(fNoiseEngine, 0.0, noise_factor);
 
-  //In this case fNoiseFact is a value in ADC counts
-  //It is going to be the Noise RMS
-  //loop over all bins in "noise" vector
-  //and insert random noise value
-  for (unsigned int i = 0; i < noise.size(); i++)
-    noise.at(i) = rGauss.fire();
-}
-
-
-//-------------------------------------------------
-  void SimWireSBND::GenNoiseInFreq(std::vector<float> &noise, double noise_factor) const
-{
-  CLHEP::RandFlat flat(fNoiseEngine, -1, 1);
-
-  if (noise.size() != fNTicks)
-    throw cet::exception("SimWireSBND")
-        << "\033[93m"
-        << "Frequency noise vector length must match fNTicks (FFT size)"
-        << " ... " << noise.size() << " != " << fNTicks
-        << "\033[00m"
-        << std::endl;
-
-  // noise in frequency space
-  std::vector<TComplex> noiseFrequency(fNTicks / 2 + 1, 0.);
-
-  double pval = 0.;
-  double lofilter = 0.;
-  double phase = 0.;
-  double rnd[2] = {0.};
-
-  // width of frequencyBin in kHz
-  double binWidth = 1.0 / (fNTicks * fSampleRate * 1.0e-6);
-
-  for (size_t i = 0; i < fNTicks / 2 + 1; ++i) {
-    // exponential noise spectrum
-    flat.fireArray(2, rnd, 0, 1);
-    //if not from histo or in time --> then hardcoded freq. spectrum
-    if ( !fGetNoiseFromHisto )
-    {
-      pval = noise_factor * exp(-(double)i * binWidth / fNoiseWidth);
-      // low frequency cutoff
-      lofilter = 1.0 / (1.0 + exp(-(i - fLowCutoff / binWidth) / 0.5));
-      // randomize 10%
-
-      pval *= lofilter * ((1 - fNoiseRand) + 2 * fNoiseRand * rnd[0]);
-    }
-
-
-    else
-    {
-
-      pval = fNoiseHist->GetBinContent(i) * ((1 - fNoiseRand) + 2 * fNoiseRand * rnd[0]) * noise_factor;
-      //mf::LogInfo("SimWireSBND")  << " pval: " << pval;
-    }
-
-    phase = rnd[1] * 2.*TMath::Pi();
-    TComplex tc(pval * cos(phase), pval * sin(phase));
-    noiseFrequency.at(i) += tc;
-  }
-
-
-  // mf::LogInfo("SimWireSBND") << "filled noise freq";
-
-  // inverse FFT MCSignal
-  art::ServiceHandle<util::LArFFT> fFFT;
-  fFFT->DoInvFFT(noiseFrequency, noise);
-
-  noiseFrequency.clear();
-
-  // multiply each noise value by fNTicks as the InvFFT
-  // divides each bin by fNTicks assuming that a forward FFT
-  // has already been done.
-  for (unsigned int i = 0; i < noise.size(); ++i) noise.at(i) *= 1.*fNTicks;
-
-}
-  */
-
-}
+}//namespace detsim
