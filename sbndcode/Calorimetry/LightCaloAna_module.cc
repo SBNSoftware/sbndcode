bd5b1604cc456ef3c3cfb7feeed163a9b8f08c42
lynnt-uchicago <lynnt@uchicago.edu>
2022-10-24 10:25:05 -0500

    first commit of light reco attempt using visibility map

--- sbndcode/Calorimetry/LightCaloAna_module.cc
+++ sbndcode/Calorimetry/LightCaloAna_module.cc
@@ -19,7 +19,6 @@
 
 // Additional framework includes
 #include "art_root_io/TFileService.h"
-
 #include "canvas/Persistency/Common/FindMany.h"
 #include "canvas/Persistency/Common/FindManyP.h"
 #include "canvas/Persistency/Common/FindOne.h"
@@ -28,19 +27,28 @@
 #include "canvas/Persistency/Common/PtrVector.h"
 
 // LArSoft includes 
-#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardata/Utilities/AssociationUtil.h"
 #include "lardataobj/RecoBase/Slice.h"
-#include "lardataobj/RecoBase/OpHit.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardataobj/RecoBase/PFParticleMetadata.h"
+#include "lardataobj/RecoBase/SpacePoint.h"
+#include "lardataobj/RecoBase/Hit.h"
 #include "lardataobj/RecoBase/OpFlash.h"
 #include "lardataobj/AnalysisBase/T0.h"
-#include "lardataobj/RecoBase/PFParticleMetadata.h"
+#include "larcore/CoreUtils/ServiceUtil.h"
+#include "larsim/PhotonPropagation/SemiAnalyticalModel.h"
 
 // SBND includes
 #include "sbnobj/Common/Reco/SimpleFlashMatchVars.h"
+#include "sbndcode/OpDetSim/sbndPDMapAlg.hh"
 
+// ROOT includes
 #include "TFile.h"
 #include "TTree.h"
 
+// C++ includes
+#include <numeric>
+#include <memory>
 
 namespace sbnd {
   class LightCaloAna;
@@ -70,11 +78,16 @@ private:
 
   std::vector<art::Ptr<recob::OpFlash>> MatchOpFlash(std::vector<art::Ptr<sbn::SimpleFlashMatch>> fm_v,
                                                      std::vector<art::Ptr<recob::OpFlash>> flash_v);
+  // std::vector<double> CalcVisibility(std::vector<geo::Point_t> xyz_v, std::vector<double> charge_v, std::string light_type);
   std::vector<std::string> _opflash_producer_v; ///< The OpFlash producers (to be set)
   std::string _slice_producer; ///< The Slice producer (to be set)
   std::string _flashmatch_producer;
   float _nuscore_cut; 
   float _fmscore_cut;
+
+  std::unique_ptr<SemiAnalyticalModel> _semi_model;
+  fhicl::ParameterSet _vuv_params;
+  fhicl::ParameterSet _vis_params;
 
   TTree* _tree;
   int _run, _subrun, _event;
@@ -89,6 +102,10 @@ sbnd::LightCaloAna::LightCaloAna(fhicl::ParameterSet const& p)
   : EDAnalyzer{p}  // ,
   // More initializers here.
 {
+  _vuv_params = p.get<fhicl::ParameterSet>("VUVHits");
+  _vis_params = p.get<fhicl::ParameterSet>("VIVHits");
+  _semi_model = std::make_unique<SemiAnalyticalModel>(_vuv_params, _vis_params, true, false);
+
   _opflash_producer_v = p.get<std::vector<std::string>>("OpFlashProducers");
   _slice_producer = p.get<std::string>("SliceProducer");
   _flashmatch_producer = p.get<std::string>("FlashMatchProducer");
@@ -127,6 +144,13 @@ void sbnd::LightCaloAna::analyze(art::Event const& e)
     return;
   }
 
+  ::art::Handle<std::vector<recob::SpacePoint>> spacepoint_h;
+  e.getByLabel(_slice_producer, spacepoint_h);
+  if(!spacepoint_h.isValid() || spacepoint_h->empty()) {
+    std::cout << "Don't have good SpacePoints!" << std::endl;
+    return;
+  }
+
   auto const & flash0_h = e.getValidHandle<std::vector<recob::OpFlash>>(_opflash_producer_v[0]);
   auto const & flash1_h = e.getValidHandle<std::vector<recob::OpFlash>>(_opflash_producer_v[1]);
   if( (!flash0_h.isValid() || flash0_h->empty()) && (!flash1_h.isValid() || flash1_h->empty())) {
@@ -139,9 +163,11 @@ void sbnd::LightCaloAna::analyze(art::Event const& e)
   art::fill_ptr_vector(slice_v, slice_h);
 
   // Get associations 
-  art::FindManyP<recob::PFParticle> slice_to_pfps (slice_h, e, _slice_producer);
+  art::FindManyP<recob::PFParticle> slice_to_pfp (slice_h, e, _slice_producer);
   art::FindManyP<larpandoraobj::PFParticleMetadata> pfp_to_meta(pfp_h, e, _slice_producer);
-  art::FindManyP<sbn::SimpleFlashMatch> pfps_to_sfm (pfp_h, e, _flashmatch_producer);
+  art::FindManyP<sbn::SimpleFlashMatch> pfp_to_sfm (pfp_h, e, _flashmatch_producer);
+  art::FindManyP<recob::SpacePoint> pfp_to_spacepoint(pfp_h, e, _slice_producer);
+  art::FindManyP<recob::Hit> spacepoint_to_hit(spacepoint_h, e, _slice_producer);
 
   _slc_pfpid.clear();
   _slc_nuscore.clear();
@@ -156,7 +182,7 @@ void sbnd::LightCaloAna::analyze(art::Event const& e)
     float fm_time  = -9999;
     auto slice = slice_v[n_slice];
     bool found_fm = false;
-    std::vector<art::Ptr<recob::PFParticle>> pfp_v = slice_to_pfps.at(n_slice);
+    std::vector<art::Ptr<recob::PFParticle>> pfp_v = slice_to_pfp.at(n_slice);
     for (size_t n_pfp=0; n_pfp < pfp_v.size(); n_pfp++){
       auto pfp = pfp_v[n_pfp];
 
@@ -174,7 +200,7 @@ void sbnd::LightCaloAna::analyze(art::Event const& e)
       _slc_nuscore.push_back(nu_score);
       
       // get fm-score 
-      std::vector<art::Ptr<sbn::SimpleFlashMatch>> fm_v = pfps_to_sfm.at(pfp.key());
+      std::vector<art::Ptr<sbn::SimpleFlashMatch>> fm_v = pfp_to_sfm.at(pfp.key());
       if (fm_v.empty()){
         _slc_fmscore.push_back(-999);
         continue;
@@ -199,7 +225,8 @@ void sbnd::LightCaloAna::analyze(art::Event const& e)
     std::cout << "slice and flashmatch vector length mismatch!" << std::endl;
     return;
   }
-
+  
+  // get relevant opflashes
   // tpc0
   std::vector<art::Ptr<recob::OpFlash>> flash0_v;
   art::fill_ptr_vector(flash0_v, flash0_h);
@@ -209,6 +236,39 @@ void sbnd::LightCaloAna::analyze(art::Event const& e)
   std::vector<art::Ptr<recob::OpFlash>> flash1_v;
   art::fill_ptr_vector(flash1_v, flash1_h);
   std::vector<art::Ptr<recob::OpFlash>> match_op1 = MatchOpFlash(match_fm_v,flash1_v);
+
+  // calculate total Q 
+
+  // calculate total L 
+  // first get the visibilities of each PMT for the relevant slice 
+
+  // get information to do charge-weighted average 
+  std::vector<geo::Point_t> slice_xyz;
+  std::vector<double> slice_charge; 
+  
+  for (size_t n_slice=0; n_slice < match_slices_v.size(); n_slice++){
+    auto slice = match_slices_v[n_slice];
+    std::vector<art::Ptr<recob::PFParticle>> pfp_v = slice_to_pfp.at(slice.key());
+    for (size_t n_pfp=0; n_pfp < pfp_v.size(); n_pfp++){
+      auto pfp = pfp_v[n_pfp];
+      std::vector<art::Ptr<recob::SpacePoint>> sp_v = pfp_to_spacepoint.at(pfp.key());
+      for (size_t n_sp=0; n_sp < sp_v.size(); n_sp++){
+        auto sp = sp_v[n_sp];
+        const auto &position(sp->XYZ());
+        geo::Point_t xyz(position[0],position[1],position[2]);
+        slice_xyz.push_back(xyz);
+
+        // get charge information
+        double sp_charge = 0;
+        std::vector<art::Ptr<recob::Hit>> hit_v = spacepoint_to_hit.at(sp.key());
+        for (size_t n_hit=0; n_hit < hit_v.size(); n_hit++){
+          auto hit = hit_v[n_hit];
+          sp_charge += (1/.0201293)*hit->Integral();
+        }
+        slice_charge.push_back(sp_charge);
+      }
+    }
+  } // end slice loop 
 
   _tree->Fill();
 } // end analyze
@@ -237,5 +297,39 @@ std::vector<art::Ptr<recob::OpFlash>> sbnd::LightCaloAna::MatchOpFlash(std::vect
   if (matched_opflashes.size() != fm_v.size()) std::cout << "mismatched opflash and simpleflash vector sizes!" << std::endl;
   return matched_opflashes;
 }
+
+/*
+std::vector<double> sbnd::LightCaloAna::CalcVisibility(std::vector<geo::Point_t> xyz_v,
+                                                       std::vector<double> charge_v,
+                                                       std::string light_type){
+  // returns of a vector (len is # of opdet) for the visibility for every opdet                                     
+  if (xyz_v.size() != charge_v.size()) std::cout << "spacepoint coord and charge vector size mismatch" << std::endl;
+
+  std::vector<double> visibility;
+  double sum_charge = std::accumulate(charge_v.begin(), charge_v.end(), 0.0);
+
+  for (size_t i=0; i<xyz_v.size(); i++){
+    geo::Point_t const xyz = xyz_v[i];
+    auto charge = charge_v[i];
+    std::vector<double> temp_visibility;
+    std::cout << light_type << std::endl;
+    _semi_model->detectedDirectVisibilities(temp_visibility, xyz);
+    // if (light_type == "reflected")
+    //   _semi_model->detectedReflectedVisibilities(temp_visibility, xyz);
+
+    // weight the temp visibility map by charge 
+    std::transform(temp_visibility.begin(), temp_visibility.end(), 
+                   temp_visibility.begin(), [charge](double &k){ return charge*k; });
+
+    // add the visibility map for this spacepoint to the total 
+    std::transform(visibility.begin(), visibility.end(), temp_visibility.begin(),
+                   visibility.begin(), std::plus<double>());
+  } // end spacepoint loop
+  // normalize by the total charge 
+  std::transform(visibility.begin(), visibility.end(), 
+                 visibility.begin(), [sum_charge](double &k){ return k/sum_charge; });
+  return visibility;
+}
+*/
 
 DEFINE_ART_MODULE(sbnd::LightCaloAna)
