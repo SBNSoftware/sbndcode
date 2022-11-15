aecce88938429ec74cb69bc2c68b85ec463694f0
Henry Lay <h.lay@lancaster.ac.uk>
2022-11-15 00:07:02 -0600

    Ensure module saves skeleton clusters

--- sbndcode/CRT/CRTReco/CRTClusterProducer_module.cc
+++ sbndcode/CRT/CRTReco/CRTClusterProducer_module.cc
@@ -43,7 +43,7 @@ public:
   void produce(art::Event& e) override;
 
   std::map<std::string, std::vector<art::Ptr<sbnd::crt::CRTStripHit>>> GroupStripHits(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &CRTStripHitVec);
-  std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<uint16_t>>> CreateClusters(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &stripHits);
+  std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<art::Ptr<sbnd::crt::CRTStripHit>>>> CreateClusters(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &stripHits);
   sbnd::crt::CRTCluster CharacteriseCluster(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &clusteredHits);
 
 private:
@@ -61,13 +61,13 @@ sbnd::CRTClusterProducer::CRTClusterProducer(fhicl::ParameterSet const& p)
   , fCoincidenceTimeRequirement(p.get<uint32_t>("CoincidenceTimeRequirement"))
   {
     produces<std::vector<sbnd::crt::CRTCluster>>();
-    produces<art::Assns<sbnd::crt::CRTStripHit, sbnd::crt::CRTCluster>>();
+    produces<art::Assns<sbnd::crt::CRTCluster, sbnd::crt::CRTStripHit>>();
   }
 
 void sbnd::CRTClusterProducer::produce(art::Event& e)
 {
   auto clusterVec          = std::make_unique<std::vector<sbnd::crt::CRTCluster>>();
-  auto clusterStripHitAssn = std::make_unique<art::Assns<sbnd::crt::CRTStripHit, sbnd::crt::CRTCluster>>();
+  auto clusterStripHitAssn = std::make_unique<art::Assns<sbnd::crt::CRTCluster, sbnd::crt::CRTStripHit>>();
   
   art::Handle<std::vector<sbnd::crt::CRTStripHit>> CRTStripHitHandle;
   e.getByLabel(fCRTStripHitModuleLabel, CRTStripHitHandle);
@@ -80,10 +80,15 @@ void sbnd::CRTClusterProducer::produce(art::Event& e)
   for(auto& [tagger, stripHits] : taggerStripHitsMap)
     {
       std::sort(stripHits.begin(), stripHits.end(), [](art::Ptr<sbnd::crt::CRTStripHit> &a, art::Ptr<sbnd::crt::CRTStripHit> &b)->bool{
-	  return a->Ts1() < b->Ts1();});
+          return a->Ts1() < b->Ts1();});
 
-      std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<uint16_t>>> ClustersAndHitIDs = CreateClusters(stripHits);
+      std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<art::Ptr<sbnd::crt::CRTStripHit>>>> clustersAndHits = CreateClusters(stripHits);
 
+      for(auto const& [cluster, clusteredHits] : clustersAndHits)
+        {
+          clusterVec->push_back(cluster);
+          util::CreateAssn(*this, e, *clusterVec, clusteredHits, *clusterStripHitAssn);
+        }
     }
 
   e.put(std::move(clusterVec));
@@ -96,24 +101,23 @@ std::map<std::string, std::vector<art::Ptr<sbnd::crt::CRTStripHit>>> sbnd::CRTCl
 
   for(const art::Ptr<sbnd::crt::CRTStripHit> &stripHit : CRTStripHitVec)
     {
-      CRTStripGeo strip   = fCRTGeoAlg.GetStrip(stripHit->Channel());
-      CRTTaggerGeo tagger = fCRTGeoAlg.GetTagger(strip.name);
+      std::string taggerName = fCRTGeoAlg.ChannelToTaggerName(stripHit->Channel());
 
-      if(taggerStripHitsMap.find(tagger.name) != taggerStripHitsMap.end())
-	taggerStripHitsMap[tagger.name].push_back(stripHit);
+      if(taggerStripHitsMap.find(taggerName) != taggerStripHitsMap.end())
+        taggerStripHitsMap[taggerName].push_back(stripHit);
       else
-	{
-	  taggerStripHitsMap[tagger.name] = std::vector<art::Ptr<sbnd::crt::CRTStripHit>>();
-	  taggerStripHitsMap[tagger.name].push_back(stripHit);
-	}
+        {
+          taggerStripHitsMap[taggerName] = std::vector<art::Ptr<sbnd::crt::CRTStripHit>>();
+          taggerStripHitsMap[taggerName].push_back(stripHit);
+        }
     }
 
   return taggerStripHitsMap;
 }
 
-std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<uint16_t>>> sbnd::CRTClusterProducer::CreateClusters(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &stripHits)
+std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<art::Ptr<sbnd::crt::CRTStripHit>>>> sbnd::CRTClusterProducer::CreateClusters(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &stripHits)
 {
-  std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<uint16_t>>> clustersAndHitIDs;
+  std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<art::Ptr<sbnd::crt::CRTStripHit>>>> clustersAndHits;
 
   std::vector<bool> used(stripHits.size(), false);
 
@@ -122,32 +126,29 @@ std::vector<std::pair<sbnd::crt::CRTCluster, std::vector<uint16_t>>> sbnd::CRTCl
       const art::Ptr<sbnd::crt::CRTStripHit> &initialStripHit = stripHits[i];
 
       if(!used[i])
-	{
-	  std::vector<art::Ptr<sbnd::crt::CRTStripHit>> clusteredHits;
-	  std::vector<uint16_t> hitIDs;
-	  clusteredHits.push_back(initialStripHit);
-	  hitIDs.push_back(i);
-	  used[i] = true;
+        {
+          std::vector<art::Ptr<sbnd::crt::CRTStripHit>> clusteredHits;
+          clusteredHits.push_back(initialStripHit);
+          used[i] = true;
 
-	  for(uint16_t ii = i+1; ii < stripHits.size(); ++ii)
-	    {
-	      const art::Ptr<sbnd::crt::CRTStripHit> &stripHit = stripHits[i];
-	      
-	      if(!used[ii])
-		{
-		  if(stripHit->Ts1() - initialStripHit->Ts1() < fCoincidenceTimeRequirement)
-		    {
-		      clusteredHits.push_back(stripHit);
-		      hitIDs.push_back(ii);
-		      used[ii] = true;
-		    }
-		}
-	      const sbnd::crt::CRTCluster &cluster = CharacteriseCluster(clusteredHits);
-	      clustersAndHitIDs.push_back(std::pair<sbnd::crt::CRTCluster, std::vector<uint16_t>>(cluster, hitIDs));
-	    }
-	}      
+          for(uint16_t ii = i+1; ii < stripHits.size(); ++ii)
+            {
+              const art::Ptr<sbnd::crt::CRTStripHit> &stripHit = stripHits[ii];
+    
+              if(!used[ii])
+                {
+                  if(stripHit->Ts1() - initialStripHit->Ts1() < fCoincidenceTimeRequirement)
+                    {
+                      clusteredHits.push_back(stripHit);
+                      used[ii] = true;
+                    }
+                }
+            }
+          const sbnd::crt::CRTCluster &cluster = CharacteriseCluster(clusteredHits);
+          clustersAndHits.emplace_back(cluster, clusteredHits);
+        }      
     }
-  return clustersAndHitIDs;
+  return clustersAndHits;
 }
 
 sbnd::crt::CRTCluster sbnd::CRTClusterProducer::CharacteriseCluster(const std::vector<art::Ptr<sbnd::crt::CRTStripHit>> &clusteredHits)
@@ -156,8 +157,7 @@ sbnd::crt::CRTCluster sbnd::CRTClusterProducer::CharacteriseCluster(const std::v
   uint16_t s = 0, ts0 = 0, ts1 = 0;
   uint32_t adc = 0, adcCorr = 0;
 
-  CRTStripGeo strip   = fCRTGeoAlg.GetStrip(clusteredHits.at(0)->Channel());
-  CRTTaggerGeo tagger = fCRTGeoAlg.GetTagger(strip.name);
+  std::string taggerName = fCRTGeoAlg.ChannelToTaggerName(clusteredHits.at(0)->Channel());
   
   for(auto const& hit : clusteredHits)
     {
@@ -174,7 +174,7 @@ sbnd::crt::CRTCluster sbnd::CRTClusterProducer::CharacteriseCluster(const std::v
   ts0 /= nHits;
   ts1 /= nHits;
 
-  return sbnd::crt::CRTCluster(s, ts0, ts1, 0., 0., 0., 0., 0., 0., nHits, adc, adcCorr, tagger.name);
+  return sbnd::crt::CRTCluster(s, ts0, ts1, 0., 0., 0., 0., 0., 0., nHits, adc, adcCorr, taggerName);
 }
 
 DEFINE_ART_MODULE(sbnd::CRTClusterProducer)
