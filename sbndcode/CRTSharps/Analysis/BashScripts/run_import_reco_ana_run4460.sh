22f81e88159b4c0a34463a2deb73d04f53c5722b
Henry Lay <h.lay@lancaster.ac.uk>
2022-11-16 09:48:56 -0600

    Add run script for 4460

---
+++ sbndcode/CRTSharps/Analysis/BashScripts/run_import_reco_ana_run4460.sh
@@ -0,0 +1,9 @@
+for FILE in `ls /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4460/raw_files/*.root`
+do
+    export FILE=$FILE
+    export base_file_name=$(basename $FILE ".root")
+    echo $base_file_name
+    lar -c import_crt_sharps_data_sbnd.fcl -s $FILE -o /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4460/imported_data/${base_file_name}_imported_data.root
+    lar -c reco_data_crt_sharps_sbnd.fcl -s /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4460/imported_data/${base_file_name}_imported_data.root -o /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4460/reco/${base_file_name}_reco.root
+    lar -c ana_crt_sharps_sbnd.fcl -s /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4460/reco/${base_file_name}_reco.root -T /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4460/ana/${base_file_name}_ana.root
+done
