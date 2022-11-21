167d2c0155dd80e3fa209ba33e94a87f56cc1fb2
Henry Lay <h.lay@lancaster.ac.uk>
2022-11-21 14:05:54 -0600

    Variable scan script

---
+++ sbndcode/CRTSharps/Analysis/BashScripts/run_bucket_substructure_period_scan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+periods=(18.50 18.60 18.70 18.80 18.90 18.92 18.93 18.94 18.95 18.96 19.00 19.10 19.20 19.30 19.40 19.50)
+for ((i=0; i<${#periods[@]}; ++i))
+do
+    export period=${periods[i]}
+    root -l -b -q "../PlottingScripts/BucketStructure.C(${period})"
+done
