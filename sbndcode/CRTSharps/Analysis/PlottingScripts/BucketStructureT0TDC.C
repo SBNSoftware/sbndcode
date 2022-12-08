86681f3eee4b19a1682515cc9b222f0e48aeff0d
Henry Lay <h.lay@lancaster.ac.uk>
2022-12-08 11:10:42 -0600

    Update bucket timing scripts

--- sbndcode/CRTSharps/Analysis/PlottingScripts/BucketStructureT0TDC.C
+++ sbndcode/CRTSharps/Analysis/PlottingScripts/BucketStructureT0TDC.C
@@ -8,10 +8,10 @@ void BucketStructureT0TDC(const double period = 18.94)
   gROOT->ForceStyle();
 
   const TString run_name = "run4500";
-  const double beam_start = 332899.25;
-  const double beam_end   = beam_start + 1500;
-  const double beam_start_rwm = -1045.25;
-  const double beam_end_rwm   = beam_start_rwm + 1500;
+  const double beam_start = 332895.97;
+  const double beam_end   = beam_start + 81 * period;
+  const double beam_start_rwm = -1048.03;
+  const double beam_end_rwm   = beam_start_rwm + 81 * period;
 
   std::stringstream ss; ss.precision(4); ss << period;
   const TString period_s = ss.str();
@@ -27,14 +27,16 @@ void BucketStructureT0TDC(const double period = 18.94)
 
   gSystem->Exec("mkdir -p " + saveDir + "/" + run_name);
 
-  std::vector<double> *chit_t0 = 0, *chit_unix_s = 0;
-  std::vector<std::vector<uint16_t>> *chit_sipm_raw_adc = 0;
+  std::vector<double> *chit_t0 = 0, *chit_unix_s = 0, *chit_t1 = 0;
+  std::vector<std::vector<uint16_t>> *chit_sipm_raw_adc = 0, *chit_sipm_feb_mac5 = 0;
   std::vector<uint32_t> *tdc_channel = 0;
   std::vector<uint64_t> *tdc_timestamp = 0;
 
   tree->SetBranchAddress("chit_t0", &chit_t0);
+  tree->SetBranchAddress("chit_t1", &chit_t1);
   tree->SetBranchAddress("chit_unix_s", &chit_unix_s);
   tree->SetBranchAddress("chit_sipm_raw_adc", &chit_sipm_raw_adc);
+  tree->SetBranchAddress("chit_sipm_feb_mac5", &chit_sipm_feb_mac5);
   tree->SetBranchAddress("tdc_channel", &tdc_channel);
   tree->SetBranchAddress("tdc_timestamp", &tdc_timestamp);
 
@@ -44,9 +46,11 @@ void BucketStructureT0TDC(const double period = 18.94)
 
   TH1F *ts0_to_bes_hist = new TH1F("ts0_to_bes_hist", ";CRT t0 - TDC BES [#kern[0.4]{#mus}];CRTHits", 40,332.5,334.5);
   TH1F *ts0_to_bes_bucket_hist = new TH1F("ts0_to_bes_bucket_hist", ";CRT t0 - TDC BES | Mod (" + period_s + ") [ns];CRTHits", 20, 0, 20);
+  TH1F *ts0_to_bes_t1_comparison_hist = new TH1F("ts0_to_bes_t1_comparison_hist", ";(CRT t0 -TDC BES) - CRT t1 [ns];CRTHits", 20, -10, 10);
 
   TH1F *ts0_to_rwm_hist = new TH1F("ts0_to_rwm_hist", ";CRT t0 - TDC RWM [#kern[0.4]{#mus}];CRTHits", 40,-1.45,.55);
   TH1F *ts0_to_rwm_bucket_hist = new TH1F("ts0_to_rwm_bucket_hist", ";CRT t0 - TDC RWM | Mod (" + period_s + ") [ns];CRTHits", 20, 0, 20);
+  TH1F *ts0_to_rwm_t1_comparison_hist = new TH1F("ts0_to_rwm_t1_comparison_hist", ";(CRT t0 -TDC RWM) - CRT t1 [ns];CRTHits", 75,-334000, -333850);
 
   for(unsigned i = 0; i < N; ++i)
     {
@@ -57,84 +61,94 @@ void BucketStructureT0TDC(const double period = 18.94)
       uint64_t bes = 0, rwm = 0, ftrig = 0;
 
       for(unsigned ii = 0; ii < tdc_channel->size(); ++ii)
-	{
-	  if(tdc_channel->at(ii) == 1)
-	    {
-	      found_bes = true;
-	      bes = tdc_timestamp->at(ii);
-	    }
-	  else if(tdc_channel->at(ii) == 2)
-	    {
-	      found_rwm = true;
-	      rwm = tdc_timestamp->at(ii);
-	    }
-	  else if(tdc_channel->at(ii) == 3)
-	    {
-	      found_ftrig = true;
-	      ftrig = tdc_timestamp->at(ii);
-	    }
+        {
+          if(tdc_channel->at(ii) == 1)
+            {
+              found_bes = true;
+              bes = tdc_timestamp->at(ii);
+            }
+          else if(tdc_channel->at(ii) == 2)
+            {
+              found_rwm = true;
+              rwm = tdc_timestamp->at(ii);
+            }
+          else if(tdc_channel->at(ii) == 3)
+            {
+              found_ftrig = true;
+              ftrig = tdc_timestamp->at(ii);
+            }
 
-	}
+        }
 
       if(!found_bes)
-	{
-	  std::cout << "Failed to find BES for event: " << i << std::endl;
-	  continue;
-	}
+        {
+          std::cout << "Failed to find BES for event: " << i << std::endl;
+          continue;
+        }
       if(!found_rwm)
-	{
-	  std::cout << "Failed to find RWM for event: " << i << std::endl;
-	  continue;
-	}
+        {
+          std::cout << "Failed to find RWM for event: " << i << std::endl;
+          continue;
+        }
       if(!found_ftrig)
-	{
-	  std::cout << "Failed to find FTRIG for event: " << i << std::endl;
-	  continue;
-	}
+        {
+          std::cout << "Failed to find FTRIG for event: " << i << std::endl;
+          continue;
+        }
 
       // if(ftrig < rwm)
-      // 	continue;
+      //        continue;
 
       for(unsigned ii = 0; ii < chit_t0->size(); ++ii)
-	{
-	  bool saturation = false;
-	  for(auto adc : chit_sipm_raw_adc->at(ii))
-	    if(adc > 4080) saturation = true;
+        {
+          bool saturation = false;
+          for(auto adc : chit_sipm_raw_adc->at(ii))
+            if(adc > 4080) saturation = true;
 
-	  if(saturation)
-	    continue;
+          if(saturation)
+            continue;
 
-	  double t0 = chit_t0->at(ii);
-	  double unixs = chit_unix_s->at(ii);
+          bool bad_feb = false;
+          for(auto mac5 : chit_sipm_feb_mac5->at(ii))
+            if(mac5 == 3) bad_feb = true;
+          
+          // if(bad_feb)
+          //   continue;
 
-	  if(std::abs(bes / 1e9 - unixs) < std::numeric_limits<double>::epsilon())
-	    std::cout << std::setprecision(9) << "Disagreement between unit part of BES: " << bes / 1e9 << " & UnixS: " << unixs << std::endl;
+          double t0 = chit_t0->at(ii);
+          double t1 = chit_t1->at(ii);
+          double unixs = chit_unix_s->at(ii);
 
-	  double bes_time = t0 - (bes % 1000000000 - 193);
-	  ts0_to_bes_hist->Fill(1e-3 * bes_time);
+          if(std::abs(bes / 1e9 - unixs) < std::numeric_limits<double>::epsilon())
+            std::cout << std::setprecision(9) << "Disagreement between unit part of BES: " << bes / 1e9 << " & UnixS: " << unixs << std::endl;
 
-	  if(bes_time > beam_start && bes_time < beam_end)
-	    {
-	      double bes_time_wrt_beam_start = bes_time - beam_start;
-	      int bes_bucketnumber = bes_time_wrt_beam_start / period;
-	      double bes_remainder = bes_time_wrt_beam_start - period * bes_bucketnumber;
-	      ts0_to_bes_bucket_hist->Fill(bes_remainder);
-	    }
+          double bes_time = t0 - (bes % 1000000000 - 193);
+          ts0_to_bes_hist->Fill(1e-3 * bes_time);
 
-	  if(std::abs(rwm / 1e9 - unixs) < std::numeric_limits<double>::epsilon())
-	    std::cout << std::setprecision(9) << "Disagreement between unit part of RWM: " << rwm / 1e9 << " & UnixS: " << unixs << std::endl;
+          if(bes_time > beam_start && bes_time < beam_end)
+            {
+              double bes_time_wrt_beam_start = bes_time - beam_start;
+              int bes_bucketnumber = bes_time_wrt_beam_start / period;
+              double bes_remainder = bes_time_wrt_beam_start - period * bes_bucketnumber;
+              ts0_to_bes_bucket_hist->Fill(bes_remainder);
+              ts0_to_bes_t1_comparison_hist->Fill(bes_time - t1);
+            }
 
-	  double rwm_time = t0 - (rwm % 1000000000 - 193);
-	  ts0_to_rwm_hist->Fill(1e-3 * rwm_time);
+          if(std::abs(rwm / 1e9 - unixs) < std::numeric_limits<double>::epsilon())
+            std::cout << std::setprecision(9) << "Disagreement between unit part of RWM: " << rwm / 1e9 << " & UnixS: " << unixs << std::endl;
 
-	  if(rwm_time > beam_start_rwm && rwm_time < beam_end_rwm)
-	    {
-	      double rwm_time_wrt_beam_start = rwm_time - beam_start_rwm;
-	      int rwm_bucketnumber = rwm_time_wrt_beam_start / period;
-	      double rwm_remainder = rwm_time_wrt_beam_start - period * rwm_bucketnumber;
-	      ts0_to_rwm_bucket_hist->Fill(rwm_remainder);
-	    }
-	}
+          double rwm_time = t0 - (rwm % 1000000000 - 193);
+          ts0_to_rwm_hist->Fill(1e-3 * rwm_time);
+
+          if(rwm_time > beam_start_rwm && rwm_time < beam_end_rwm)
+            {
+              double rwm_time_wrt_beam_start = rwm_time - beam_start_rwm;
+              int rwm_bucketnumber = rwm_time_wrt_beam_start / period;
+              double rwm_remainder = rwm_time_wrt_beam_start - period * rwm_bucketnumber;
+              ts0_to_rwm_bucket_hist->Fill(rwm_remainder);
+              ts0_to_rwm_t1_comparison_hist->Fill(rwm_time - t1);
+            }
+        }
     }
 
   TCanvas *ts0_to_bes_canvas = new TCanvas("ts0_to_bes_canvas", "ts0_to_bes_canvas");
@@ -158,7 +172,7 @@ void BucketStructureT0TDC(const double period = 18.94)
   ts0_to_bes_bucket_canvas->SetTopMargin(.1);
   ts0_to_bes_bucket_hist->SetLineColor(kCyan+2);
   ts0_to_bes_bucket_hist->SetMinimum(0);
-  ts0_to_bes_bucket_hist->Draw("hist");
+  ts0_to_bes_bucket_hist->Draw("histE");
 
   TPaveText *pt = new TPaveText(.18,.79,.31,.87, "NDC");
   pt->AddText("SBND CRT##");
@@ -174,17 +188,19 @@ void BucketStructureT0TDC(const double period = 18.94)
       ts0_to_bes_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_bes_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + ".pdf");
     }
 
-  TF1 *gausFunc = new TF1("gausFunc", "gaus", 0, period);
-  TFitResultPtr fit = ts0_to_bes_bucket_hist->Fit("gausFunc", "S");
+  TF1 *gausFunc = new TF1("gausFunc", "[0]*exp(-0.5*((x-[1])/[2])^2) + [3]", 0, period);
+  gausFunc->SetParameters(200,10,4,100);
   gausFunc->SetLineColor(kRed+2);
   gausFunc->SetLineWidth(4);
+  TFitResultPtr fit = ts0_to_bes_bucket_hist->Fit("gausFunc", "S", "same");
   gausFunc->Draw("same");
 
   TPaveText *ptFit = new TPaveText(.65,.73,.8,.85, "NDC");
   ptFit->AddText("Gaussian Fit");
-  ptFit->AddText(Form("Mean:     %.3f", gausFunc->GetParameter("Mean")));
-  ptFit->AddText(Form("Sigma:    %.3f", gausFunc->GetParameter("Sigma")));
-  ptFit->AddText(Form("#chi^{2} / dof:  %.3f / %i", fit->Chi2(), fit->Ndf()));
+  ptFit->AddText(Form("Mean:      %.3f", gausFunc->GetParameter(1)));
+  ptFit->AddText(Form("Sigma:     %.3f", gausFunc->GetParameter(2)));
+  ptFit->AddText(Form("Bkgd Rate: %.3f", gausFunc->GetParameter(3)));
+  ptFit->AddText(Form("#chi^{2} / dof:   %.3f / %i", fit->Chi2(), fit->Ndf()));
   ptFit->SetFillColor(kWhite);
   ptFit->SetTextColor(kBlack);
   ptFit->SetTextAlign(11);
@@ -197,6 +213,20 @@ void BucketStructureT0TDC(const double period = 18.94)
     {
       ts0_to_bes_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_bes_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + "_fit.png");
       ts0_to_bes_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_bes_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + "_fit.pdf");
+    }
+
+  TCanvas *ts0_to_bes_t1_comparison_canvas = new TCanvas("ts0_to_bes_t1_comparison_canvas", "ts0_to_bes_t1_comparison_canvas");
+  ts0_to_bes_t1_comparison_canvas->cd();
+  ts0_to_bes_t1_comparison_canvas->SetRightMargin(.17);
+  ts0_to_bes_t1_comparison_canvas->SetLeftMargin(.18);
+  ts0_to_bes_t1_comparison_canvas->SetTopMargin(.13);
+  ts0_to_bes_t1_comparison_hist->SetLineColor(kMagenta+2);
+  ts0_to_bes_t1_comparison_hist->Draw("hist");
+
+  if(save)
+    {
+      ts0_to_bes_t1_comparison_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_bes_t1_comparison.png");
+      ts0_to_bes_t1_comparison_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_bes_t1_comparison.pdf");
     }
 
   TCanvas *ts0_to_rwm_canvas = new TCanvas("ts0_to_rwm_canvas", "ts0_to_rwm_canvas");
@@ -220,7 +250,7 @@ void BucketStructureT0TDC(const double period = 18.94)
   ts0_to_rwm_bucket_canvas->SetTopMargin(.1);
   ts0_to_rwm_bucket_hist->SetLineColor(kCyan+2);
   ts0_to_rwm_bucket_hist->SetMinimum(0);
-  ts0_to_rwm_bucket_hist->Draw("hist");
+  ts0_to_rwm_bucket_hist->Draw("histE");
 
   TPaveText *pt_rwm = new TPaveText(.18,.79,.31,.87, "NDC");
   pt_rwm->AddText("SBND CRT##");
@@ -236,7 +266,8 @@ void BucketStructureT0TDC(const double period = 18.94)
       ts0_to_rwm_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_rwm_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + ".pdf");
     }
 
-  TF1 *gausFunc_rwm = new TF1("gausFunc_rwm", "gaus", 0, period);
+  TF1 *gausFunc_rwm = new TF1("gausFunc_rwm", "[0]*exp(-0.5*((x-[1])/[2])^2) + [3]", 0, period);
+  gausFunc_rwm->SetParameters(200,10,4,40);
   TFitResultPtr fit_rwm = ts0_to_rwm_bucket_hist->Fit("gausFunc_rwm", "S");
   gausFunc_rwm->SetLineColor(kRed+2);
   gausFunc_rwm->SetLineWidth(4);
@@ -244,9 +275,10 @@ void BucketStructureT0TDC(const double period = 18.94)
 
   TPaveText *ptFit_rwm = new TPaveText(.65,.73,.8,.85, "NDC");
   ptFit_rwm->AddText("Gaussian Fit");
-  ptFit_rwm->AddText(Form("Mean:     %.3f", gausFunc_rwm->GetParameter("Mean")));
-  ptFit_rwm->AddText(Form("Sigma:    %.3f", gausFunc_rwm->GetParameter("Sigma")));
-  ptFit_rwm->AddText(Form("#chi^{2} / dof:  %.3f / %i", fit_rwm->Chi2(), fit_rwm->Ndf()));
+  ptFit_rwm->AddText(Form("Mean:      %.3f", gausFunc_rwm->GetParameter(1)));
+  ptFit_rwm->AddText(Form("Sigma:     %.3f", gausFunc_rwm->GetParameter(2)));
+  ptFit_rwm->AddText(Form("Bkgd Rate: %.3f", gausFunc_rwm->GetParameter(3)));
+  ptFit_rwm->AddText(Form("#chi^{2} / dof:   %.3f / %i", fit_rwm->Chi2(), fit_rwm->Ndf()));
   ptFit_rwm->SetFillColor(kWhite);
   ptFit_rwm->SetTextColor(kBlack);
   ptFit_rwm->SetTextAlign(11);
@@ -259,5 +291,19 @@ void BucketStructureT0TDC(const double period = 18.94)
     {
       ts0_to_rwm_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_rwm_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + "_fit.png");
       ts0_to_rwm_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_rwm_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + "_fit.pdf");
+    }
+
+  TCanvas *ts0_to_rwm_t1_comparison_canvas = new TCanvas("ts0_to_rwm_t1_comparison_canvas", "ts0_to_rwm_t1_comparison_canvas");
+  ts0_to_rwm_t1_comparison_canvas->cd();
+  ts0_to_rwm_t1_comparison_canvas->SetRightMargin(.17);
+  ts0_to_rwm_t1_comparison_canvas->SetLeftMargin(.18);
+  ts0_to_rwm_t1_comparison_canvas->SetTopMargin(.13);
+  ts0_to_rwm_t1_comparison_hist->SetLineColor(kMagenta+2);
+  ts0_to_rwm_t1_comparison_hist->Draw("hist");
+
+  if(save)
+    {
+      ts0_to_rwm_t1_comparison_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_rwm_t1_comparison.png");
+      ts0_to_rwm_t1_comparison_canvas->SaveAs(saveDir + "/" + run_name + "/ts0_to_rwm_t1_comparison.pdf");
     }
 }
--- sbndcode/CRTSharps/Analysis/PlottingScripts/BucketStructureT1.C
+++ sbndcode/CRTSharps/Analysis/PlottingScripts/BucketStructureT1.C
@@ -8,8 +8,8 @@ void BucketStructureT1(const double period = 18.94)
   gROOT->ForceStyle();
 
   const TString run_name = "run4500";
-  const double beam_start = 332899.25;
-  const double beam_end   = beam_start + 1500;
+  const double beam_start = 332899.1;
+  const double beam_end   = beam_start + 81 * period;
 
   std::stringstream ss; ss.precision(4); ss << period;
   const TString period_s = ss.str();
@@ -26,10 +26,11 @@ void BucketStructureT1(const double period = 18.94)
   gSystem->Exec("mkdir -p " + saveDir + "/" + run_name);
 
   std::vector<double> *chit_t1 = 0;
-  std::vector<std::vector<uint16_t>> *chit_sipm_raw_adc = 0;
+  std::vector<std::vector<uint16_t>> *chit_sipm_raw_adc = 0, *chit_sipm_feb_mac5 = 0;
 
   tree->SetBranchAddress("chit_t1", &chit_t1);
   tree->SetBranchAddress("chit_sipm_raw_adc", &chit_sipm_raw_adc);
+  tree->SetBranchAddress("chit_sipm_feb_mac5", &chit_sipm_feb_mac5);
 
   const unsigned N = tree->GetEntries();
   std::cout << "==== TOTAL ENTRIES: " << N << std::endl;
@@ -43,25 +44,25 @@ void BucketStructureT1(const double period = 18.94)
       tree->GetEntry(i);
       
       for(unsigned ii = 0; ii < chit_t1->size(); ++ii)
-	{
+        {
           bool saturation = false;
           for(auto adc : chit_sipm_raw_adc->at(ii))
             if(adc > 4080) saturation = true;
 
-	  if(saturation)
-	    continue;
+          if(saturation)
+            continue;
 
-	  double time = chit_t1->at(ii);
-	  ts1_hist->Fill(1e-3 * time);
-	  if(time > beam_start && time < beam_end)
-	    {
-	      double time_wrt_beam_start = time - beam_start;
-	      int bucketnumber = time_wrt_beam_start / period;
-	      double remainder = time_wrt_beam_start - period * bucketnumber;
-	      
-	      ts1_bucket_hist->Fill(remainder);
-	    }
-	}
+          double time = chit_t1->at(ii);
+          ts1_hist->Fill(1e-3 * time);
+          if(time > beam_start && time < beam_end)
+            {
+              double time_wrt_beam_start = time - beam_start;
+              int bucketnumber = time_wrt_beam_start / period;
+              double remainder = time_wrt_beam_start - period * bucketnumber;
+              
+              ts1_bucket_hist->Fill(remainder);
+            }
+        }
     }
 
   TCanvas *ts1_canvas = new TCanvas("ts1_canvas", "ts1_canvas");
@@ -85,7 +86,7 @@ void BucketStructureT1(const double period = 18.94)
   ts1_bucket_canvas->SetTopMargin(.1);
   ts1_bucket_hist->SetLineColor(kCyan+2);
   ts1_bucket_hist->SetMinimum(0);
-  ts1_bucket_hist->Draw("hist");
+  ts1_bucket_hist->Draw("histE");
 
   TPaveText *pt = new TPaveText(.18,.79,.31,.87, "NDC");
   pt->AddText("SBND CRT##");
@@ -101,17 +102,19 @@ void BucketStructureT1(const double period = 18.94)
       ts1_bucket_canvas->SaveAs(saveDir + "/" + run_name + "/ts1_overlapped_buckets_period_" + period_int_s + "p" + period_frac_s + ".pdf");
     }
 
-  TF1 *gausFunc = new TF1("gausFunc", "gaus", 0, period);
+  TF1 *gausFunc = new TF1("gausFunc", "[0]*exp(-0.5*((x-[1])/[2])^2) + [3]", 0, period);
+  gausFunc->SetParameters(500,10,4,100);
   TFitResultPtr fit = ts1_bucket_hist->Fit("gausFunc", "S");
   gausFunc->SetLineColor(kRed+2);
   gausFunc->SetLineWidth(4);
   gausFunc->Draw("same");
 
-  TPaveText *ptFit = new TPaveText(.65,.73,.8,.85, "NDC");
+  TPaveText *ptFit = new TPaveText(.65,.71,.8,.85, "NDC");
   ptFit->AddText("Gaussian Fit");
-  ptFit->AddText(Form("Mean:     %.3f", gausFunc->GetParameter("Mean")));
-  ptFit->AddText(Form("Sigma:    %.3f", gausFunc->GetParameter("Sigma")));
-  ptFit->AddText(Form("#chi^{2} / dof:  %.3f / %i", fit->Chi2(), fit->Ndf()));
+  ptFit->AddText(Form("Mean:      %.3f", gausFunc->GetParameter(1)));
+  ptFit->AddText(Form("Sigma:     %.3f", gausFunc->GetParameter(2)));
+  ptFit->AddText(Form("Bkgd Rate: %.3f", gausFunc->GetParameter(3)));
+  ptFit->AddText(Form("#chi^{2} / dof:   %.3f / %i", fit->Chi2(), fit->Ndf()));
   ptFit->SetFillColor(kWhite);
   ptFit->SetTextColor(kBlack);
   ptFit->SetTextAlign(11);
