8b08db24e03a0a91506db728e968033f5b32af55
Henry Lay <h.lay@lancaster.ac.uk>
2022-12-08 11:12:58 -0600

    Add reco ana script for run4500

---
+++ sbndcode/CRTSharps/Analysis/BashScripts/run_reco_ana_run4500.sh
@@ -0,0 +1,8 @@
+for FILE in `ls /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4500/imported_data/*run45*.root`
+do
+    export FILE=$FILE
+    export base_file_name=$(basename $FILE "_imported_data.root")
+    echo $base_file_name
+    lar -c reco_data_crt_sharps_sbnd.fcl -s /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4500/imported_data/${base_file_name}_imported_data.root -o /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4500/reco/${base_file_name}_reco.root
+    lar -c ana_crt_sharps_sbnd.fcl -s /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4500/reco/${base_file_name}_reco.root -T /pnfs/sbnd/scratch/users/hlay/crt_sharps_data/run4500/ana/${base_file_name}_ana.root
+done
