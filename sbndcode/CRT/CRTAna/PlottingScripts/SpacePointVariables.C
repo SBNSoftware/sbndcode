ce39eaaf45f6f8d7d4cb26c6d339c6a04ed46321
Henry Lay <h.lay@lancaster.ac.uk>
2023-01-10 16:25:43 -0600

    Develop space point plotting script

--- sbndcode/CRT/CRTAna/PlottingScripts/SpacePointVariables.C
+++ sbndcode/CRT/CRTAna/PlottingScripts/SpacePointVariables.C
@@ -40,6 +40,7 @@ void SpacePointVariables()
     double ylow;
     double yhigh;
     TCut req = "";
+    bool zaxislog = false;
   };
 
   const int plotcolour = kMagenta+2;
@@ -76,15 +77,33 @@ void SpacePointVariables()
                               100, -50, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete && cl_tagger!=1 && cl_tagger!=2"},
                              {"cl_z_accuracy_no_front_back_log", "cl_sp_z - cl_truth_z", ";(reco - true) z [cm];SpacePoints",
                               100, -50, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete && cl_tagger!=1 && cl_tagger!=2", false, true},
+                             {"cl_pos_accuracy", "sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];SpacePoints",
+                              55, -5, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"cl_pos_accuracy_multi_strip_clusters", "sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];SpacePoints",
+                              55, -5, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete && cl_nhits>2"},
                              {"cl_time_accuracy", "cl_sp_time - (cl_truth_time + 1.7e6)", ";(reco - (true - G4RefTime)) t [ns];SpacePoints",
                               100, -50, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
   };
 
   std::vector<plttwod> twodplots = {
     {"cl_pe_energy_relation", "cl_truth_energy:cl_sp_pe", ";PE;True Energy (MeV);SpacePoints",
-     50, 0, 700, 50, 0, 0.1},
+     50, 0, 700, 50, 0, 0.1, "cl_has_sp && cl_sp_complete"},
     {"cl_pe_energy_relation_zoom", "cl_truth_energy:cl_sp_pe", ";PE;True Energy (MeV);SpacePoints",
-     50, 0, 700, 50, 0, 0.02},
+     50, 0, 700, 50, 0, 0.02, "cl_has_sp && cl_sp_complete"},
+    {"cl_pe_energy_relation_log", "cl_truth_energy:cl_sp_pe", ";PE;True Energy (MeV);SpacePoints",
+     50, 0, 700, 50, 0, 0.1, "cl_has_sp && cl_sp_complete", true},
+    {"cl_pe_energy_relation_zoom_log", "cl_truth_energy:cl_sp_pe", ";PE;True Energy (MeV);SpacePoints",
+     50, 0, 700, 50, 0, 0.02, "cl_has_sp && cl_sp_complete", true},
+    {"cl_pos_accuracy_nhits", "cl_nhits:sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];# Hits in Cluster;SpacePoints",
+     55, -5, 50, 10, 0, 10, "cl_has_sp && cl_sp_complete", true},
+    {"cl_pos_accuracy_completeness", "cl_truth_completeness:sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];Cluster completeness;SpacePoints",
+     55, -5, 50, 50, 0, 1 + std::numeric_limits<double>::epsilon(), "cl_has_sp && cl_sp_complete", true},
+    {"cl_pos_accuracy_purity", "cl_truth_purity:sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];Cluster purity;SpacePoints",
+     55, -5, 50, 50, 0, 1 + std::numeric_limits<double>::epsilon(), "cl_has_sp && cl_sp_complete", true},
+    {"cl_pos_accuracy_completeness_multi_strip_clusters", "cl_truth_completeness:sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];Cluster completeness;SpacePoints",
+     55, -5, 50, 50, 0, 1 + std::numeric_limits<double>::epsilon(), "cl_has_sp && cl_sp_complete && cl_nhits>2", true},
+    {"cl_pos_accuracy_purity_multi_strip_clusters", "cl_truth_purity:sqrt((cl_sp_x - cl_truth_x)^2 + (cl_sp_y - cl_truth_y)^2 + (cl_sp_z - cl_truth_z)^2)", ";(reco - true) position [cm];Cluster purity;SpacePoints",
+     55, -5, 50, 50, 0, 1 + std::numeric_limits<double>::epsilon(), "cl_has_sp && cl_sp_complete && cl_nhits>2", true},
   };
 
   gSystem->Exec("mkdir -p " + saveDir);
@@ -121,6 +140,7 @@ void SpacePointVariables()
       gStyle->SetNdivisions(505, "x");
       TCanvas *canvas = new TCanvas("c_" + plot.name, "c_" + plot.name);
       canvas->cd();
+      if(plot.zaxislog) canvas->SetLogz();
       canvas->SetRightMargin(0.25);
       gStyle->SetPalette(kBlueRedYellow);
       TH2F* hist = new TH2F(plot.name, plot.axes_labels, plot.nbinsx, plot.xlow, plot.xhigh,
