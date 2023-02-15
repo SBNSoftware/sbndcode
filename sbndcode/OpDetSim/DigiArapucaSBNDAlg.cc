261883a9f25099cede6813f69e9bdae7ce19a1f7
Rodrigo Alvarez Garrote <rodrigoa@fnal.gov>
2023-02-15 10:02:38 -0600

    pnfs is dead

--- sbndcode/OpDetSim/DigiArapucaSBNDAlg.cc
+++ sbndcode/OpDetSim/DigiArapucaSBNDAlg.cc
@@ -34,7 +34,7 @@ namespace opdet {
     cet::search_path sp("FW_SEARCH_PATH");
     sp.find_file(fParams.ArapucaDataFile, fname);
     // TFile* file = TFile::Open(fname.c_str(), "READ");
-    TFile* file = TFile::Open("/cvmfs/sbnd.osgstorage.org/pnfs/fnal.gov/usr/sbnd/persistent/stash/users/rodrigoa/digi_arapuca_sbnd.root", "READ");//temporarly patch
+    TFile* file = TFile::Open("/sbnd/app/users/rodrigoa/detsim_PR/digi_arapuca_sbnd.root", "READ");//temporary patch
     //Note: TPB time now implemented at digitization module for both coated pmts and (x)arapucas
     //OpDetSim/digi_arapuca_sbnd.root updated in sbnd_data (now including the TPB times vector)
 
@@ -58,7 +58,7 @@ namespace opdet {
     if(fParams.ArapucaSinglePEmodel) {
       mf::LogDebug("DigiArapucaSBNDAlg") << " using testbench pe response";
       // TFile* file =  TFile::Open(fname.c_str(), "READ");
-      TFile* file = TFile::Open("/cvmfs/sbnd.osgstorage.org/pnfs/fnal.gov/usr/sbnd/persistent/stash/users/rodrigoa/digi_arapuca_sbnd.root", "READ");//temporarly patch
+      TFile* file = TFile::Open("/sbnd/app/users/rodrigoa/detsim_PR/digi_arapuca_sbnd.root", "READ");//temporarly patch
       std::vector<double>* SinglePEVec_40ftCable_Daphne;
       std::vector<double>* SinglePEVec_40ftCable_Apsaia;
       file->GetObject("SinglePEVec_40ftCable_Apsaia", SinglePEVec_40ftCable_Apsaia);
