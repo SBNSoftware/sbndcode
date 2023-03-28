e99810b0d7e59dcff4898386cc0b0040bc3b4df7
Rodrigo Alvarez Garrote <rodrigoa@fnal.gov>
2023-03-28 08:56:05 -0500

    testing cubic spline and linear interpolation

--- sbndcode/OpDetSim/DigiPMTSBNDAlg.cc
+++ sbndcode/OpDetSim/DigiPMTSBNDAlg.cc
@@ -37,7 +37,8 @@ namespace opdet {
     std::string fname;
     cet::search_path sp("FW_SEARCH_PATH");
     sp.find_file(fParams.PMTDataFile, fname);
-    TFile* file = TFile::Open(fname.c_str(), "READ");
+    // TFile* file = TFile::Open(fname.c_str(), "READ");
+    TFile* file = TFile::Open("/sbnd/app/users/rodrigoa/detsim_PR/new_digi_pmt_sbnd.root", "READ");
 
     // TPB emission time histogram for pmt_coated histogram
     std::vector<double>* timeTPB_p;
@@ -49,7 +50,7 @@ namespace opdet {
     if (fParams.PMTSinglePEmodel) {
       mf::LogDebug("DigiPMTSBNDAlg") << " using testbench pe response";
       std::vector<double>* SinglePEVec_p;
-      file->GetObject("SinglePEVec_HD", SinglePEVec_p);
+      file->GetObject("SinglePEVec_HD_cubic", SinglePEVec_p);
       fSinglePEWave = *SinglePEVec_p;
 
       // Prepare HD waveforms
