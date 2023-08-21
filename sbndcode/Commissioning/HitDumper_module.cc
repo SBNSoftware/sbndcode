b5fee971c16ead149889fde3c3e9a38aa14cbeb3
Brinden Carlson <brindenc@sbndgpvm04.fnal.gov>
2023-08-21 12:12:58 -0500

    Added option to read MCParticle information

--- sbndcode/Commissioning/HitDumper_module.cc
+++ sbndcode/Commissioning/HitDumper_module.cc
@@ -151,6 +151,8 @@ private:
   void ResizeMCNeutrino(int nNeutrinos);
   /// Resize the data structure for Genie primaries
   void ResizeGenie(int nPrimaries);
+  /// Resize the data structure for MCParticles
+  void ResizeMCParticle(int nParticles);
 
   opdet::sbndPDMapAlg _pd_map;
 
@@ -324,6 +326,31 @@ private:
   std::vector<Int_t>     genie_ND;
   std::vector<Int_t>     genie_mother;
 
+  //MCParticle Info
+  size_t MaxMCParticles = 0;
+  Int_t     mcpart_no_primaries;                 
+  std::vector<Int_t>    mcpart_pdg;              
+  std::vector<Int_t>    mcpart_status;           
+  std::vector<Float_t>  mcpart_Eng;              
+  std::vector<Float_t>  mcpart_EndE;
+  std::vector<Float_t>  mcpart_Mass;
+  std::vector<Float_t>  mcpart_Px;
+  std::vector<Float_t>  mcpart_Py;
+  std::vector<Float_t>  mcpart_Pz;
+  std::vector<Float_t>  mcpart_P;
+  std::vector<Float_t>  mcpart_StartPointx;
+  std::vector<Float_t>  mcpart_StartPointy;
+  std::vector<Float_t>  mcpart_StartPointz;
+  std::vector<Float_t>  mcpart_StartT;  
+  std::vector<Float_t>  mcpart_EndT;          
+  std::vector<Float_t>  mcpart_EndPointx;
+  std::vector<Float_t>  mcpart_EndPointy;
+  std::vector<Float_t>  mcpart_EndPointz;
+  std::vector<Float_t>  mcpart_theta_xz;    
+  std::vector<Float_t>  mcpart_theta_yz;    
+  std::vector<Int_t>    mcpart_NumberDaughters;
+  std::vector<Int_t>    mcpart_TrackId;
+  std::vector<Int_t>    mcpart_Mother;
 
   TTree* _sr_tree; ///< A tree filled per subrun (for POT accounting)
   int _sr_run, _sr_subrun;
@@ -348,6 +375,7 @@ private:
   std::string fMuonTrackModuleLabel;  ///< Label for MuonTrack dataproduct (to be set via fcl)
   std::string fDigitModuleLabel;    ///< Label for digitizer (to be set via fcl)
   std::string fGenieGenModuleLabel; ///< Label for Genie dataproduct (to be set via fcl)
+  std::string fMCParticleModuleLabel; ///< Label for MCParticle dataproduct (to be set via fcl)
   std::vector<std::string> fOpHitsModuleLabels; ///< Labels for OpHit dataproducts (to be set via fcl)
 
   // double fSelectedPDG;
@@ -360,6 +388,7 @@ private:
   bool freadMuonTracks;    ///< Add MuonTracks to output (to be set via fcl)
   bool freadMuonHits;      ///< Add MuonTrack hits to output(to be set via fcl)
   bool freadTruth;         ///< Add Truth info to output (to be set via fcl)
+  bool freadMCParticle;    ///< Add MCParticle info to output (to be set via fcl)
   bool freadpmtTrigger;    ///< Add pmt hardware trigger info to output (to be set via fcl)
   bool freadpmtSoftTrigger;///< Add pmt software trigger info to output (to be set via fcl)
   bool freadcrtSoftTrigger;///< Add crt software trigger info to output (to be set via fcl)
@@ -418,6 +447,7 @@ void Hitdumper::reconfigure(fhicl::ParameterSet const& p)
   fcrtSoftTriggerModuleLabel = p.get<std::string>("crtSoftTriggerModuleLabel", "MetricProducer");
   fMuonTrackModuleLabel  = p.get<std::string>("MuonTrackModuleLabel", "MuonTrackProducer");
   fGenieGenModuleLabel = p.get<std::string>("GenieGenModuleLabel", "generator");
+  fMCParticleModuleLabel    = p.get<std::string>("MCParticleModuleLabel ", "largeant");
 
   fkeepCRThits       = p.get<bool>("keepCRThits",true);
   fkeepCRTstrips     = p.get<bool>("keepCRTstrips",false);
@@ -431,6 +461,7 @@ void Hitdumper::reconfigure(fhicl::ParameterSet const& p)
   freadMuonHits      = p.get<bool>("readMuonHits",false);
   fcheckTransparency = p.get<bool>("checkTransparency",false);
   freadTruth         = p.get<bool>("readTruth",true);
+  freadMCParticle    = p.get<bool>("readTruth",false);
   fsavePOTInfo       = p.get<bool>("savePOTinfo",true);
   fUncompressWithPed = p.get<bool>("UncompressWithPed",false);
 
@@ -1060,7 +1091,48 @@ void Hitdumper::analyze(const art::Event& evt)
   }// end if fCheckTrasparency
 
 
+  if (freadMCParticle){
+    //MCParticle
+    art::Handle<std::vector<simb::MCParticle>> MCParticleListHandle;
+    std::vector<art::Ptr<simb::MCParticle>> MCParticleList;
+    if (evt.getByLabel(fMCParticleModuleLabel,MCParticleListHandle)){
+      art::fill_ptr_vector(MCParticleList,MCParticleListHandle);
+      mcpart_no_primaries = MCParticleList.size();
+      ResizeMCParticle(MCParticleList.size()); //Set vectors
+      //for(auto const & pPart : MCParticleList){
+      for (size_t iMCPart = 0; iMCPart < MCParticleList.size(); iMCPart++){
+        art::Ptr<simb::MCParticle> pPart = MCParticleList[iMCPart]; //get particle pointer
+        //Geant info
+        mcpart_Mother[iMCPart] = pPart->Mother();
+        mcpart_TrackId[iMCPart] = pPart->TrackId();
+        mcpart_pdg[iMCPart] = pPart->PdgCode();
+        mcpart_status[iMCPart] =  pPart->StatusCode();
+        mcpart_Eng[iMCPart] = pPart->E();
+        mcpart_EndE[iMCPart] = pPart->EndE();
+        mcpart_Mass[iMCPart] = pPart->Mass();
+        mcpart_Px[iMCPart] = pPart->Px();
+        mcpart_Py[iMCPart] = pPart->Py();
+        mcpart_Pz[iMCPart] = pPart->Pz();
+        mcpart_P[iMCPart] = pPart->Momentum().Vect().Mag();
+        mcpart_StartPointx[iMCPart] = pPart->Vx();
+        mcpart_StartPointy[iMCPart] = pPart->Vy();
+        mcpart_StartPointz[iMCPart] = pPart->Vz();
+        mcpart_StartT[iMCPart] = pPart->T();
+        mcpart_EndPointx[iMCPart] = pPart->EndPosition()[0];
+        mcpart_EndPointy[iMCPart] = pPart->EndPosition()[1];
+        mcpart_EndPointz[iMCPart] = pPart->EndPosition()[2];
+        mcpart_EndT[iMCPart] = pPart->EndT();
+        mcpart_theta_xz[iMCPart] =  std::atan2(pPart->Px(), pPart->Pz());
+        mcpart_theta_yz[iMCPart] =  std::atan2(pPart->Py(), pPart->Pz());
+        mcpart_NumberDaughters[iMCPart] = pPart->NumberDaughters();
+      }
+    }//endif get label
+    else {
+      std::cout << "Failed to get MCParticle data product." << std::endl;
+    }
+  } // end read mcparticle
   if (freadTruth){
+    //Genie
     int nGeniePrimaries = 0, nMCNeutrinos = 0;
     art::Handle< std::vector<simb::MCTruth> > mctruthListHandle;
     std::vector<art::Ptr<simb::MCTruth> > mclist;
@@ -1186,6 +1258,9 @@ void Hitdumper::analyze(const art::Event& evt)
         } // for particle
       } //if neutrino set
     }//if (mcevts_truth)
+
+
+
   }//if (fReadTruth){
 
 
@@ -1372,6 +1447,31 @@ void Hitdumper::analyze(const art::Event& evt)
     fTree->Branch("genie_ND",&genie_ND);
     fTree->Branch("genie_mother",&genie_mother);
   }
+  if (freadMCParticle){
+    //MCParticle
+    fTree->Branch("mcpart_pdg",&mcpart_pdg);
+    fTree->Branch("mcpart_status",&mcpart_status);    
+    fTree->Branch("mcpart_Eng",&mcpart_Eng);
+    fTree->Branch("mcpart_EndE",&mcpart_EndE);
+    fTree->Branch("mcpart_Mass",&mcpart_Mass);
+    fTree->Branch("mcpart_Px",&mcpart_Px);
+    fTree->Branch("mcpart_Py",&mcpart_Py);
+    fTree->Branch("mcpart_Pz",&mcpart_Pz);
+    fTree->Branch("mcpart_P",&mcpart_P);
+    fTree->Branch("mcpart_StartPointx",&mcpart_StartPointx);
+    fTree->Branch("mcpart_StartPointy",&mcpart_StartPointy);
+    fTree->Branch("mcpart_StartPointz",&mcpart_StartPointz);
+    fTree->Branch("mcpart_StartT",&mcpart_StartT);
+    fTree->Branch("mcpart_EndT",&mcpart_EndT);
+    fTree->Branch("mcpart_EndPointx",&mcpart_EndPointx);
+    fTree->Branch("mcpart_EndPointy",&mcpart_EndPointy);
+    fTree->Branch("mcpart_EndPointz",&mcpart_EndPointz);         
+    fTree->Branch("mcpart_theta_xz",&mcpart_theta_xz);
+    fTree->Branch("mcpart_theta_yz",&mcpart_theta_yz);   
+    fTree->Branch("mcpart_NumberDaughters",&mcpart_NumberDaughters);
+    fTree->Branch("mcpart_TrackId",&mcpart_TrackId);
+    fTree->Branch("mcpart_Mother",&mcpart_Mother);
+  }
 
   if (fsavePOTInfo) {
     _sr_tree = tfs->make<TTree>("pottree","");
@@ -1541,6 +1641,7 @@ void Hitdumper::ResetVars() {
 
   mcevts_truth = 0;
   genie_no_primaries = 0;
+  mcpart_no_primaries = 0;
 }
 
 void Hitdumper::ResizeMCNeutrino(int nNeutrinos) {
@@ -1591,6 +1692,36 @@ void Hitdumper::ResizeGenie(int nPrimaries) {
   genie_trackID.assign(MaxGeniePrimaries, DEFAULT_VALUE);
   genie_ND.assign(MaxGeniePrimaries, DEFAULT_VALUE);
   genie_mother.assign(MaxGeniePrimaries, DEFAULT_VALUE);
+
+}
+
+void Hitdumper::ResizeMCParticle(int nParticles) {
+
+  // minimum size is 1, so that we always have an address
+  MaxMCParticles = (size_t) std::max(nParticles, 1);
+              
+  mcpart_pdg.assign(MaxMCParticles,DEFAULT_VALUE);              
+  mcpart_status.assign(MaxMCParticles,DEFAULT_VALUE);           
+  mcpart_Eng.assign(MaxMCParticles,DEFAULT_VALUE);              
+  mcpart_EndE.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_Mass.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_Px.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_Py.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_Pz.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_P.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_StartPointx.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_StartPointy.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_StartPointz.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_StartT.assign(MaxMCParticles,DEFAULT_VALUE);  
+  mcpart_EndT.assign(MaxMCParticles,DEFAULT_VALUE);          
+  mcpart_EndPointx.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_EndPointy.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_EndPointz.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_theta_xz.assign(MaxMCParticles,DEFAULT_VALUE);    
+  mcpart_theta_yz.assign(MaxMCParticles,DEFAULT_VALUE);    
+  mcpart_NumberDaughters.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_TrackId.assign(MaxMCParticles,DEFAULT_VALUE);
+  mcpart_Mother.assign(MaxMCParticles,DEFAULT_VALUE);
 
 }
 
