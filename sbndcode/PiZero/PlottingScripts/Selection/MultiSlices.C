8b368ae0543081232faf4abe390f94f50f5a37e9
Henry Lay <h.lay@lancaster.ac.uk>
2023-09-22 06:06:33 -0500

    Evaluate OpT0 metrics in multi slice investigation script

--- sbndcode/PiZero/PlottingScripts/Selection/MultiSlices.C
+++ sbndcode/PiZero/PlottingScripts/Selection/MultiSlices.C
@@ -1,17 +1,18 @@
 void MultiSlices()
 {
-  const TString file = "/sbnd/data/users/hlay/pizero/ncpizeroana_sbnd.root";
+  const TString file = "/sbnd/data/users/hlay/ncpizero/production/NCPiZeroA/NCPiZeroA_test_rockbox.root";
 
   gROOT->SetStyle("henrySBND");
   gROOT->ForceStyle();
 
   TChain *events = new TChain("ncpizeroana/events");
-  events->Add("/sbnd/data/users/hlay/pizero/ncpizeroana_sbnd.root");
+  events->Add(file);
 
   std::vector<int> *nu_event_type = 0, *slc_true_event_type = 0, *slc_n_trks = 0, *slc_n_shws = 0,
     *slc_n_dazzle_muons_cut_based = 0;
   std::vector<size_t> *slc_true_mctruth_id = 0;
-  std::vector<double> *slc_comp = 0, *slc_pur = 0, *slc_vtx_x = 0, *slc_vtx_y = 0, *slc_vtx_z = 0;
+  std::vector<double> *slc_comp = 0, *slc_pur = 0, *slc_vtx_x = 0, *slc_vtx_y = 0, *slc_vtx_z = 0,
+    *slc_opt0_measPE = 0, *slc_opt0_hypPE = 0;
 
   events->SetBranchAddress("nu_event_type", &nu_event_type);
   events->SetBranchAddress("slc_true_event_type", &slc_true_event_type);
@@ -24,6 +25,8 @@ void MultiSlices()
   events->SetBranchAddress("slc_vtx_x", &slc_vtx_x);
   events->SetBranchAddress("slc_vtx_y", &slc_vtx_y);
   events->SetBranchAddress("slc_vtx_z", &slc_vtx_z);
+  events->SetBranchAddress("slc_opt0_measPE", &slc_opt0_measPE);
+  events->SetBranchAddress("slc_opt0_hypPE", &slc_opt0_hypPE);
 
   size_t recoverables = 0;
 
@@ -71,7 +74,8 @@ void MultiSlices()
                             << " and purity " << slc_pur->at(j) << "\n\t"
                             << "Trks: " << slc_n_trks->at(j) << "\n\t"
                             << "Shws: " << slc_n_shws->at(j) << "\n\t"
-                            << "DazzleMuons: " << slc_n_dazzle_muons_cut_based->at(j)
+                            << "DazzleMuons: " << slc_n_dazzle_muons_cut_based->at(j) << "\n\t"
+                            << "OpT0Frac: " << (slc_opt0_hypPE->at(j) - slc_opt0_measPE->at(j)) / slc_opt0_measPE->at(j)
                             << std::endl;
                 }
             }
