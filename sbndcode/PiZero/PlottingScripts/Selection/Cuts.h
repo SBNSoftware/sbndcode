2f2242536c0c9d21ed258beb8cc730fb57bc0771
Henry Lay <h.lay@lancaster.ac.uk>
2023-10-16 04:15:25 -0500

    Add no pion optional cuts

--- sbndcode/PiZero/PlottingScripts/Selection/Cuts.h
+++ sbndcode/PiZero/PlottingScripts/Selection/Cuts.h
@@ -20,6 +20,17 @@ std::vector<Cut> dazzle_muons_razzled_photons_cuts = {
   { "has_two_razzled_photons", "slc_n_razzled_photons>1", "Has Two Razzled Photons" },
 };
 
+std::vector<Cut> dazzle_muons_pions_razzled_photons_cuts = {
+  { "no_cut", "", "No Cut" },
+  { "not_clear_cosmic", "!slc_is_clear_cosmic", "Not Clear Cosmic" },
+  { "fv", "slc_is_fv", "FV" },
+  { "crumbs", "slc_crumbs_score>-0.025", "CRUMBS Cut" },
+  { "no_dazzle_muons", "slc_n_dazzle_muons==0", "No Dazzle Muons" },
+  { "no_dazzle_pions", "slc_n_dazzle_pions==0", "No Dazzle Pions" },
+  { "has_two_pfps", "slc_n_pfps>2", "Has Two PFPs" }, // note the 'neutrino' counts as a PFP so really we're requiring 3
+  { "has_two_razzled_photons", "slc_n_razzled_photons>1", "Has Two Razzled Photons" },
+};
+
 std::vector<Cut> razzled_muons_razzle_photons_cuts = {
   { "no_cut", "", "No Cut" },
   { "not_clear_cosmic", "!slc_is_clear_cosmic", "Not Clear Cosmic" },
@@ -36,6 +47,17 @@ std::vector<Cut> razzled_cuts = {
   { "fv", "slc_is_fv", "FV" },
   { "crumbs", "slc_crumbs_score>-0.025", "CRUMBS Cut" },
   { "no_razzled_muons", "slc_n_razzled_muons==0", "No Razzled Muons" },
+  { "has_two_pfps", "slc_n_pfps>2", "Has Two PFPs" }, // note the 'neutrino' counts as a PFP so really we're requiring 3
+  { "has_two_razzled_photons", "slc_n_razzled_photons>1", "Has Two Razzled Photons" },
+};
+
+std::vector<Cut> razzled_cuts_no_pion = {
+  { "no_cut", "", "No Cut" },
+  { "not_clear_cosmic", "!slc_is_clear_cosmic", "Not Clear Cosmic" },
+  { "fv", "slc_is_fv", "FV" },
+  { "crumbs", "slc_crumbs_score>-0.025", "CRUMBS Cut" },
+  { "no_razzled_muons", "slc_n_razzled_muons==0", "No Razzled Muons" },
+  { "no_razzled_pions", "slc_n_razzled_pions==0", "No Razzled Pions" },
   { "has_two_pfps", "slc_n_pfps>2", "Has Two PFPs" }, // note the 'neutrino' counts as a PFP so really we're requiring 3
   { "has_two_razzled_photons", "slc_n_razzled_photons>1", "Has Two Razzled Photons" },
 };
