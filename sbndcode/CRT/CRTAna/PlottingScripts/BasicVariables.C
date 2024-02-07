50918f76ac14124d1c3caee3e22b19500435f2de
Henry Lay <h.lay@lancaster.ac.uk>
2024-02-07 07:17:35 -0600

    Import scripts

---
+++ sbndcode/CRT/CRTAna/PlottingScripts/BasicVariables.C
@@ -0,0 +1,236 @@
+void BasicVariables()
+{
+  const TString saveDir = "/sbnd/data/users/hlay/crt/clustering/plots/large_samples/combined/basicvariables";
+  gSystem->Exec("mkdir -p " + saveDir);
+  const bool save = true;
+
+  using namespace std;
+  gROOT->SetStyle("henrySBND");
+  gROOT->ForceStyle();
+
+  TChain *tree = new TChain("crtana/tree");
+  tree->Add("/sbnd/data/users/hlay/crt/clustering/production/ana/crtana_sbnd.root");
+
+  struct datacut {
+    TCut cut;
+    TString name;
+  };
+
+  struct plt {
+    TString name;
+    TString var;
+    TString axes_labels;
+    int nbins;
+    double xlow;
+    double xhigh;
+    int colour;
+    std::vector<TString> binlabels = {};
+    TCut req = "";
+    bool yaxisfromzero = false;
+    bool logy = false;
+  };
+
+  struct plttwod {
+    TString name;
+    TString var;
+    TString axes_labels;
+    int nbinsx;
+    double xlow;
+    double xhigh;
+    int nbinsy;
+    double ylow;
+    double yhigh;
+    TCut req = "";
+  };
+
+  const int plotcolour = kMagenta+2;
+  
+  std::vector<plt> plots = { {"n_strip_hits", "@sh_channel.size()", ";nStripHits;Events",
+                              50, 0, 500, plotcolour},
+                             {"n_clusters", "@cl_ts0.size()", ";nClusters;Events",
+                              50, 0, 500, plotcolour},
+                             {"n_tracks", "@tr_time.size()", ";nTracks;Events",
+                              50, 0, 100, plotcolour},
+                             {"sh_channel","sh_channel", ";Channel ID;Strip Hits",
+                              10000, 0, 10000, plotcolour},
+                             {"sh_ts0","sh_ts0", ";Ts0 [ns];Strip Hits",
+                              120, 0, 1.2e9, plotcolour},
+                             {"sh_ts1","sh_ts1", ";Ts1 [ns];Strip Hits",
+                              100, 0, 1e7, plotcolour},
+                             {"sh_pos","sh_pos", ";Lateral Position [cm];Strip Hits",
+                              30, 0, 15, plotcolour},
+                             {"sh_err","sh_err", ";Lateral Position Error [cm];Strip Hits",
+                              30, 0, 15, plotcolour},
+                             {"sh_adc1","sh_adc1", ";ADC1;Strip Hits",
+                              50, 0, 5000, plotcolour},
+                             {"sh_adc2","sh_adc2", ";ADC2;Strip Hits",
+                              50, 0, 5000, plotcolour},
+                             {"sh_saturated1","sh_saturated1", ";SiPM 1;Strip Hits",
+                              2, 0, 2, plotcolour, {"Not Saturated", "Saturated"}, "", true},
+                             {"sh_saturated2","sh_saturated2", ";SiPM 2;Strip Hits",
+                              2, 0, 2, plotcolour, {"Not Saturated", "Saturated"}, "", true},
+                             {"sh_saturated","sh_saturated1 || sh_saturated2", ";Either SiPM;Strip Hits",
+                              2, 0, 2, plotcolour, {"Not Saturated", "Saturated"}, "", true},
+                             {"sh_completeness","sh_truth_completeness", ";Completeness;Strip Hits",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour},
+                             {"sh_purity","sh_truth_purity", ";Purity;Strip Hits",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour},
+                             {"cl_ts0","cl_ts0", ";Ts0 [ns];Clusters",
+                              120, 0, 1.2e9, plotcolour},
+                             {"cl_ts1","cl_ts1", ";Ts1 [ns];Clusters",
+                              100, 0, 1e7, plotcolour},
+                             {"cl_nhits","cl_nhits", ";Hits per cluster;Clusters",
+                              10, 0, 10, plotcolour},
+                             {"cl_composition","cl_composition", ";Composition;Clusters",
+                              8, 0, 8, plotcolour, {"Undefined","X", "Y", "XY", "Z", "XZ", "YZ", "XYZ"}, "", true},
+                             {"cl_composition_log","cl_composition", ";Composition;Clusters",
+                              8, 0, 8, plotcolour, {"Undefined","X", "Y", "XY", "Z", "XZ", "YZ", "XYZ"}, "", true, true},
+                             {"cl_completeness","cl_truth_completeness", ";Completeness;Clusters",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour},
+                             {"cl_purity","cl_truth_purity", ";Purity;Clusters",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour},
+                             {"cl_completeness_multi_strip_clusters","cl_truth_completeness", ";Completeness;Clusters",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour, {}, "cl_nhits>2"},
+                             {"cl_purity_multi_strip_clusters","cl_truth_purity", ";Purity;Clusters",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour, {}, "cl_nhits>2"},
+                             {"sp_x","cl_sp_x", ";x [cm];Space Points",
+                              100, -500, 500, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_ex","cl_sp_ex", ";x error [cm];Space Points",
+                              50, 0, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_y","cl_sp_y", ";y [cm];Space Points",
+                              100, -500, 1500, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_ey","cl_sp_ey", ";y error [cm];Space Points",
+                              50, 0, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_z","cl_sp_z", ";z [cm];Space Points",
+                              100, -250, 850, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_ez","cl_sp_ez", ";z error [cm];Space Points",
+                              50, 0, 50, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_pe","cl_sp_pe", ";PE;Space Points",
+                              100, 0, 800, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_time","cl_sp_time", ";Time in readout [ns];Space Points",
+                              100, -1.8e6, 1.7e6, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"sp_etime","cl_sp_etime", ";Time error [ns];Space Points",
+                              50, 0, 100, plotcolour, {}, "cl_has_sp && cl_sp_complete"},
+                             {"cl_has_sp","cl_has_sp", ";Has SpacePoint?;Clusters",
+                              2, 0, 2, plotcolour, {"No","Yes"}, "", true},
+                             {"cl_sp_complete","cl_sp_complete", ";Is SpacePoint Complete?;Clusters",
+                              2, 0, 2, plotcolour, {"No","Yes"}, "cl_has_sp"},
+                             {"tr_start_x","tr_start_x", ";x start [cm];Tracks",
+                              100, -500, 500, plotcolour},
+                             {"tr_start_y","tr_start_y", ";y start [cm];Tracks",
+                              100, -500, 1500, plotcolour},
+                             {"tr_start_z","tr_start_z", ";z start [cm];Tracks",
+                              100, -250, 850, plotcolour},
+                             {"tr_end_x","tr_end_x", ";x end [cm];Tracks",
+                              100, -500, 500, plotcolour},
+                             {"tr_end_y","tr_end_y", ";y end [cm];Tracks",
+                              100, -500, 1500, plotcolour},
+                             {"tr_end_z","tr_end_z", ";z end [cm];Tracks",
+                              100, -250, 850, plotcolour},
+                             {"tr_dir_x","tr_dir_x", ";x direction [cm];Tracks",
+                              50, -1, 1, plotcolour},
+                             {"tr_dir_y","tr_dir_y", ";y direction [cm];Tracks",
+                              50, -1, 1, plotcolour},
+                             {"tr_dir_z","tr_dir_z", ";z direction [cm];Tracks",
+                              50, -1, 1, plotcolour},
+                             {"tr_time","tr_time", ";Time in readout [ns];Tracks",
+                              100, -1.8e6, 1.7e6, plotcolour},
+                             {"tr_pe","tr_pe", ";PE;Tracks",
+                              100, 0, 2000, plotcolour},
+                             {"tr_length", "tr_length", ";Length [cm];Tracks",
+                              100, 0, 1500, plotcolour},
+                             {"tr_tof", "tr_tof", ";ToF [ns];Tracks",
+                              100, 0, 100, plotcolour},
+                             {"tr_theta", "tr_theta", ";#theta [#circ];Tracks",
+                              90, 0, 180, plotcolour},
+                             {"tr_phi", "tr_phi", ";#phi [#circ];Tracks",
+                              90, -180, 180, plotcolour},
+                             {"tr_triple", "tr_triple", ";Has 3 SP?;Tracks",
+                              2, 0, 2, plotcolour, {"No", "Yes"}, "", true},
+                             {"tr_completeness","tr_truth_completeness", ";Completeness;Tracks",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour},
+                             {"tr_purity","tr_truth_purity", ";Purity;Tracks",
+                              50, 0, 1 + std::numeric_limits<double>::epsilon(), plotcolour},
+  };
+
+  const auto copy_of_plots = plots;
+
+  for(auto const &plot : copy_of_plots)
+    {
+      if(plot.name.BeginsWith("tr_"))
+        {
+          plt a = plot;
+          plt b = plot;
+
+          a.req = "!tr_triple";
+          a.name.Append("_two_sp");
+
+          b.req = "tr_triple";
+          b.name.Append("_three_sp");
+
+          plots.push_back(a);
+          plots.push_back(b);
+        }
+    }
+  
+  std::vector<plttwod> twodplots = {
+    {"cl_completeness_nhits","cl_nhits:cl_truth_completeness", ";Completeness;Hits per cluster;Clusters",
+     50, 0, 1 + std::numeric_limits<double>::epsilon(), 10, 0, 10},
+    {"cl_purity_nhits","cl_nhits:cl_truth_purity", ";Purity;Hits per cluster;Clusters",
+     50, 0, 1 + std::numeric_limits<double>::epsilon(), 10, 0, 10},
+  };
+
+  for(auto const &plot : plots)
+    {
+      TCanvas *canvas = new TCanvas("c_" + plot.name, "c_" + plot.name);
+      canvas->cd();
+
+      TH1F* hist = new TH1F(plot.name, plot.axes_labels,
+                            plot.nbins, plot.xlow, plot.xhigh);
+      hist->SetLineColor(plot.colour);
+      hist->GetYaxis()->SetTitleOffset(1.3);
+      hist->GetYaxis()->SetNdivisions(507);
+      if(plot.yaxisfromzero) hist->SetMinimum(0);
+      tree->Draw(plot.var + ">>" + plot.name, plot.req,"histE");
+
+      if(plot.logy)
+        {
+          hist->SetMinimum(1);
+          canvas->SetLogy();
+        }
+
+      if(plot.binlabels.size() == plot.nbins)
+        {
+          for(int bin = 1; bin <= hist->GetXaxis()->GetNbins(); ++bin)
+            hist->GetXaxis()->SetBinLabel(bin, plot.binlabels[bin-1]);
+        }
+
+      if(save)
+        {
+          canvas->SaveAs(saveDir + "/" + plot.name + ".png");
+          canvas->SaveAs(saveDir + "/" + plot.name + ".pdf");
+        }
+      delete canvas, hist;
+    }
+
+  for(auto const &plot : twodplots)
+    {
+      gStyle->SetNdivisions(505, "x");
+      TCanvas *canvas = new TCanvas("c_" + plot.name, "c_" + plot.name);
+      canvas->cd();
+      canvas->SetRightMargin(0.25);
+      gStyle->SetPalette(kBlueRedYellow);
+      TH2F* hist = new TH2F(plot.name, plot.axes_labels, plot.nbinsx, plot.xlow, plot.xhigh,
+                            plot.nbinsy, plot.ylow, plot.yhigh);
+      tree->Draw(plot.var + ">>" + plot.name, plot.req, "colz");
+      hist->GetYaxis()->SetTitleOffset(1.25);
+      hist->GetZaxis()->SetTitleOffset(1.3);
+
+      if(save)
+        {
+          canvas->SaveAs(saveDir + "/" + plot.name + ".png");
+          canvas->SaveAs(saveDir + "/" + plot.name + ".pdf");
+        }
+      delete canvas, hist;
+    }
+}
---
+++ sbndcode/CRT/CRTAna/PlottingScripts/ReconstructionEfficiency.C
@@ -0,0 +1,200 @@
+void ReconstructionEfficiency()
+{
+  const TString saveDir = "/sbnd/data/users/hlay/crt/clustering/plots/large_samples/combined/reconstructionefficiency";
+  gSystem->Exec("mkdir -p " + saveDir);
+  const bool save = true;
+
+  using namespace std;
+  gROOT->SetStyle("henrySBND");
+  gROOT->ForceStyle();
+
+  TChain *tree = new TChain("crtana/tree");
+  tree->Add("/sbnd/data/users/hlay/crt/clustering/production/ana/crtana_sbnd.root");
+
+  std::vector<double> *td_tag_energy = 0;
+  std::vector<bool> *td_tag_reco_status = 0;
+  std::vector<double> *td_energy = 0;
+  std::vector<bool> *td_reconstructable = 0;
+  std::vector<bool> *td_reco_status = 0;
+  std::vector<bool> *td_reco_triple = 0;
+
+  tree->SetBranchAddress("td_tag_energy", &td_tag_energy);
+  tree->SetBranchAddress("td_tag_reco_status", &td_tag_reco_status);
+
+  tree->SetBranchAddress("td_energy", &td_energy);
+  tree->SetBranchAddress("td_reconstructable", &td_reconstructable);
+  tree->SetBranchAddress("td_reco_status", &td_reco_status);
+  tree->SetBranchAddress("td_reco_triple", &td_reco_triple);
+
+  double bins[23];
+  for(int i = 0; i < 16; ++i)
+    bins[i] = i * .5;
+  for(int i = 16; i < 19; ++i)
+    bins[i] = 8 + (i - 16) * 1;
+  bins[19] = 11; bins[20] = 15; bins[21] = 20; bins[22] = 25;
+
+  double binstracks[23];
+  for(int i = 0; i < 23; ++i)
+    binstracks[i] = 2 * bins[i];
+
+  TH1F *hTrueDepositEnergy = new TH1F("hTrueDepositEnergy", ";True energy (MeV);Deposits", 22, bins);
+  TH1F *hTrueDepositEnergyReco = new TH1F("hTrueDepositEnergyReco", ";True energy (MeV);Deposits", 22, bins);
+  TH1F *hTrueTrackDepositEnergy = new TH1F("hTrueTrackDepositEnergy", ";True energy (MeV);Deposits", 22, binstracks);
+  TH1F *hTrueTrackDepositEnergyReco = new TH1F("hTrueTrackDepositEnergyReco", ";True energy (MeV);Deposits", 22, binstracks);
+  TH1F *hTrue2TrackDepositEnergyReco = new TH1F("hTrue2TrackDepositEnergyReco", ";True energy (MeV);Deposits", 22, binstracks);
+  TH1F *hTrue3TrackDepositEnergyReco = new TH1F("hTrue3TrackDepositEnergyReco", ";True energy (MeV);Deposits", 22, binstracks);
+
+  const unsigned N = tree->GetEntries();
+
+  for(unsigned i = 0; i < N; ++i)
+    {
+      tree->GetEntry(i);
+
+      for(unsigned ii = 0; ii < td_tag_energy->size(); ++ii)
+        {
+          hTrueDepositEnergy->Fill(1e3*td_tag_energy->at(ii));
+
+          if(td_tag_reco_status->at(ii))
+            hTrueDepositEnergyReco->Fill(1e3*td_tag_energy->at(ii));
+        }
+
+      for(unsigned ii = 0; ii < td_energy->size(); ++ii)
+        {
+          if(!td_reconstructable->at(ii))
+            continue;
+
+          hTrueTrackDepositEnergy->Fill(1e3*td_energy->at(ii));
+
+          if(td_reco_status->at(ii))
+            hTrueTrackDepositEnergyReco->Fill(1e3*td_energy->at(ii));
+
+          if(td_reco_triple->at(ii) && td_reco_status->at(ii))
+            hTrue3TrackDepositEnergyReco->Fill(1e3*td_energy->at(ii));
+
+          if(!td_reco_triple->at(ii) && td_reco_status->at(ii))
+            hTrue2TrackDepositEnergyReco->Fill(1e3*td_energy->at(ii));
+        }
+    }
+
+  for(unsigned i = 1; i <= hTrueDepositEnergy->GetNbinsX(); ++i)
+    {
+      hTrueDepositEnergy->SetBinContent(i, hTrueDepositEnergy->GetBinContent(i) / hTrueDepositEnergy->GetBinWidth(i));
+      hTrueDepositEnergyReco->SetBinContent(i, hTrueDepositEnergyReco->GetBinContent(i) / hTrueDepositEnergyReco->GetBinWidth(i));
+      hTrueTrackDepositEnergy->SetBinContent(i, hTrueTrackDepositEnergy->GetBinContent(i) / hTrueTrackDepositEnergy->GetBinWidth(i));
+      hTrueTrackDepositEnergyReco->SetBinContent(i, hTrueTrackDepositEnergyReco->GetBinContent(i) / hTrueTrackDepositEnergyReco->GetBinWidth(i));
+      hTrue2TrackDepositEnergyReco->SetBinContent(i, hTrue2TrackDepositEnergyReco->GetBinContent(i) / hTrue2TrackDepositEnergyReco->GetBinWidth(i));
+      hTrue3TrackDepositEnergyReco->SetBinContent(i, hTrue3TrackDepositEnergyReco->GetBinContent(i) / hTrue3TrackDepositEnergyReco->GetBinWidth(i));
+    }
+
+  TCanvas *cEnergyRecoEff = new TCanvas("cEnergyRecoEff", "cEnergyRecoEff");
+  cEnergyRecoEff->cd();
+
+  TH1F *hTrueDepositEnergyClone = (TH1F*) hTrueDepositEnergy->Clone("hTrueDepositEnergyClone");
+  hTrueDepositEnergyClone->Scale(1 / hTrueDepositEnergyClone->GetMaximum());
+  hTrueDepositEnergyClone->SetLineColor(kGray+2);
+
+  TEfficiency *eEnergyRecoEff = new TEfficiency(*hTrueDepositEnergyReco, *hTrueDepositEnergy);
+  eEnergyRecoEff->SetTitle(";True energy (MeV);Efficiency");
+  eEnergyRecoEff->SetLineColor(kRed+2);
+  eEnergyRecoEff->SetMarkerColor(kRed+2);
+  eEnergyRecoEff->SetLineWidth(2);
+
+  eEnergyRecoEff->Draw();
+  gPad->Update();
+  eEnergyRecoEff->GetPaintedGraph()->GetYaxis()->SetRangeUser(0, 1.2);
+  hTrueDepositEnergyClone->Draw("samehist");
+
+  TLegend *lEnergyRecoEff = new TLegend(.6,.45,.85,.55);
+  lEnergyRecoEff->SetBorderSize(0);
+  lEnergyRecoEff->AddEntry(eEnergyRecoEff, "Efficiency","ple");
+  lEnergyRecoEff->AddEntry(hTrueDepositEnergyClone, "True Distribution","l");
+  lEnergyRecoEff->Draw();
+
+  if(save)
+    {
+      cEnergyRecoEff->SaveAs(saveDir + "/true_energy_deposit_reco_eff.png");
+      cEnergyRecoEff->SaveAs(saveDir + "/true_energy_deposit_reco_eff.pdf");
+    }
+
+  TCanvas *cTrackEnergyRecoEff = new TCanvas("cTrackEnergyRecoEff", "cTrackEnergyRecoEff");
+  cTrackEnergyRecoEff->cd();
+
+  TH1F *hTrueTrackDepositEnergyClone = (TH1F*) hTrueTrackDepositEnergy->Clone("hTrueTrackDepositEnergyClone");
+  hTrueTrackDepositEnergyClone->Scale(1 / hTrueTrackDepositEnergyClone->GetMaximum());
+  hTrueTrackDepositEnergyClone->SetLineColor(kGray+2);
+
+  TEfficiency *eTrackEnergyRecoEff = new TEfficiency(*hTrueTrackDepositEnergyReco, *hTrueTrackDepositEnergy);
+  eTrackEnergyRecoEff->SetTitle(";True energy (MeV);Efficiency");
+  eTrackEnergyRecoEff->SetLineColor(kRed+2);
+  eTrackEnergyRecoEff->SetMarkerColor(kRed+2);
+  eTrackEnergyRecoEff->SetLineWidth(2);
+
+  eTrackEnergyRecoEff->Draw();
+  gPad->Update();
+  eTrackEnergyRecoEff->GetPaintedGraph()->GetYaxis()->SetRangeUser(0, 1.2);
+  hTrueTrackDepositEnergyClone->Draw("samehist");
+
+  TLegend *lTrackEnergyRecoEff = new TLegend(.6,.45,.85,.55);
+  lTrackEnergyRecoEff->SetBorderSize(0);
+  lTrackEnergyRecoEff->AddEntry(eTrackEnergyRecoEff, "Efficiency","ple");
+  lTrackEnergyRecoEff->AddEntry(hTrueTrackDepositEnergyClone, "True Distribution","l");
+  lTrackEnergyRecoEff->Draw();
+
+  if(save)
+    {
+      cTrackEnergyRecoEff->SaveAs(saveDir + "/true_track_energy_deposit_reco_eff.png");
+      cTrackEnergyRecoEff->SaveAs(saveDir + "/true_track_energy_deposit_reco_eff.pdf");
+    }
+
+  TCanvas *c2TrackEnergyRecoEff = new TCanvas("c2TrackEnergyRecoEff", "c2TrackEnergyRecoEff");
+  c2TrackEnergyRecoEff->cd();
+
+  TEfficiency *e2TrackEnergyRecoEff = new TEfficiency(*hTrue2TrackDepositEnergyReco, *hTrueTrackDepositEnergy);
+  e2TrackEnergyRecoEff->SetTitle(";True energy (MeV);Efficiency");
+  e2TrackEnergyRecoEff->SetLineColor(kRed+2);
+  e2TrackEnergyRecoEff->SetMarkerColor(kRed+2);
+  e2TrackEnergyRecoEff->SetLineWidth(2);
+
+  e2TrackEnergyRecoEff->Draw();
+  gPad->Update();
+  e2TrackEnergyRecoEff->GetPaintedGraph()->GetYaxis()->SetRangeUser(0, 1.2);
+  hTrueTrackDepositEnergyClone->Draw("samehist");
+
+  TLegend *l2TrackEnergyRecoEff = new TLegend(.6,.45,.85,.55);
+  l2TrackEnergyRecoEff->SetBorderSize(0);
+  l2TrackEnergyRecoEff->AddEntry(e2TrackEnergyRecoEff, "Efficiency","ple");
+  l2TrackEnergyRecoEff->AddEntry(hTrueTrackDepositEnergyClone, "True Distribution","l");
+  l2TrackEnergyRecoEff->Draw();
+
+  if(save)
+    {
+      c2TrackEnergyRecoEff->SaveAs(saveDir + "/true_track_two_sp_energy_deposit_reco_eff.png");
+      c2TrackEnergyRecoEff->SaveAs(saveDir + "/true_track_two_sp_energy_deposit_reco_eff.pdf");
+    }
+
+  TCanvas *c3TrackEnergyRecoEff = new TCanvas("c3TrackEnergyRecoEff", "c3TrackEnergyRecoEff");
+  c3TrackEnergyRecoEff->cd();
+
+  TEfficiency *e3TrackEnergyRecoEff = new TEfficiency(*hTrue3TrackDepositEnergyReco, *hTrueTrackDepositEnergy);
+  e3TrackEnergyRecoEff->SetTitle(";True energy (MeV);Efficiency");
+  e3TrackEnergyRecoEff->SetLineColor(kRed+2);
+  e3TrackEnergyRecoEff->SetMarkerColor(kRed+2);
+  e3TrackEnergyRecoEff->SetLineWidth(2);
+
+  e3TrackEnergyRecoEff->Draw();
+  gPad->Update();
+  e3TrackEnergyRecoEff->GetPaintedGraph()->GetYaxis()->SetRangeUser(0, 1.2);
+  hTrueTrackDepositEnergyClone->Draw("samehist");
+
+  TLegend *l3TrackEnergyRecoEff = new TLegend(.6,.45,.85,.55);
+  l3TrackEnergyRecoEff->SetBorderSize(0);
+  l3TrackEnergyRecoEff->AddEntry(e3TrackEnergyRecoEff, "Efficiency","ple");
+  l3TrackEnergyRecoEff->AddEntry(hTrueTrackDepositEnergyClone, "True Distribution","l");
+  l3TrackEnergyRecoEff->Draw();
+
+  if(save)
+    {
+      c3TrackEnergyRecoEff->SaveAs(saveDir + "/true_track_three_sp_energy_deposit_reco_eff.png");
+      c3TrackEnergyRecoEff->SaveAs(saveDir + "/true_track_three_sp_energy_deposit_reco_eff.pdf");
+    }
+}
