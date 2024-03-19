f3faf6b30dfd998f6e768bcefbedbab9fd45dfb1
Linyan Wan <linyanwan2009@gmail.com>
2024-03-19 14:10:29 -0500

    Add support for CRT channel map

---
+++ sbndcode/ChannelMaps/CRT/CRTChannelMapService.h
@@ -0,0 +1,61 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Class:       SBND::CRTChannelMapService
+// Module type: service
+// File:        CRTChannelMapService.h
+// Author:      Linyan Wan, Mar 2024
+//
+// Implementation of hardware-offline channel mapping reading from a file or from the hardware database 
+// SBND CRT
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SBNDCRTChannelMapService_H
+#define SBNDCRTChannelMapService_H
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+
+namespace SBND {
+  class CRTChannelMapService;
+}
+
+  
+class SBND::CRTChannelMapService {
+
+public:
+
+  CRTChannelMapService(fhicl::ParameterSet const& pset);
+  CRTChannelMapService(fhicl::ParameterSet const& pset, art::ActivityRegistry&);
+
+  typedef struct ChanInfo {
+    std::string  Wall;        // "East", "North", "Bottom"
+    unsigned int MAC5;           // MAC5
+    unsigned int offlchan;        // in gdml and channel sorting convention
+    bool valid;          // true if valid, false if not
+  } ChanInfo_t;
+
+  ChanInfo_t GetChanInfoFromMAC5(
+					unsigned int mac5) const;
+
+  ChanInfo_t GetChanInfoFromOfflChan(unsigned int offlchan) const;
+
+private:
+
+  // look up channel info by offline channel number
+  
+  std::unordered_map<unsigned int, ChanInfo_t> fChanInfoFromOfflChan;
+
+  // look up channel info by FEMCrate, FEM, and FEMCh
+  
+  std::unordered_map< unsigned int, ChanInfo_t > fChanInfoFromMAC5;
+
+};
+
+
+DECLARE_ART_SERVICE(SBND::CRTChannelMapService, LEGACY)
+
+#endif
---
+++ sbndcode/ChannelMaps/CRT/CRTChannelMapService_service.cc
@@ -0,0 +1,93 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Class:       SBND::CRTChannelMapService
+// Module type: service
+// File:        SBND::CRTChannelMapService_service.cc
+// Author:      Linyan Wan, Mar 2024.  
+//
+// Implementation of hardware-offline channel mapping reading from a file.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+
+#include "CRTChannelMapService.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+  
+SBND::CRTChannelMapService::CRTChannelMapService(fhicl::ParameterSet const& pset) {
+
+  bool useDB = pset.get<bool>("UseHWDB",false);
+  bool useFile = pset.get<bool>("ReadMapFromFile",true);
+  if (useDB && useFile) {
+    throw cet::exception("SBND::CRTChannelMapService: UseHWDB and ReadMapFromFile are both true");
+  }
+  if (!useDB && !useFile) {
+    throw cet::exception("SBND::CRTChannelMapService: UseHWDB and ReadMapFromFile are both false");
+  }
+  if (useFile) {
+    std::string channelMapFile = pset.get<std::string>("FileName");
+
+    std::string fullname;
+    cet::search_path sp("FW_SEARCH_PATH");
+    sp.find_file(channelMapFile, fullname);
+
+    if (fullname.empty()) {
+      std::cout << "SBND::CRTChannelMapService Input file " << channelMapFile << " not found" << std::endl;
+      throw cet::exception("File not found");
+    }
+    std::cout << "SBND CRT Channel Map: Building CRT wiremap from file " << channelMapFile << std::endl;
+    std::ifstream inFile(fullname, std::ios::in);
+    std::string line;
+
+    while (std::getline(inFile,line)) {
+      std::stringstream linestream(line);
+    
+      SBND::CRTChannelMapService::ChanInfo_t c;
+      linestream 
+	>> c.offlchan
+	>> c.Wall
+	>> c.MAC5;
+
+      c.valid = true;
+
+      fChanInfoFromMAC5[c.MAC5] = c;
+      fChanInfoFromOfflChan[c.offlchan] = c;
+    }
+    inFile.close();
+  }
+  else
+    {
+      throw cet::exception("SBND:CRTChannelMapService: Database access to be implemented.");
+    }
+}
+
+SBND::CRTChannelMapService::CRTChannelMapService(fhicl::ParameterSet const& pset, art::ActivityRegistry&) : SBND::CRTChannelMapService
+													    (pset) {
+}
+
+SBND::CRTChannelMapService::ChanInfo_t SBND::CRTChannelMapService::GetChanInfoFromMAC5(unsigned int mac5) const {
+
+  SBND::CRTChannelMapService::ChanInfo_t badinfo{};
+  badinfo.valid = false;
+
+  // look up one map at a time in order to handle cases where the item is not found
+  // without throwing and catching exception which can make debugging hard
+  
+  auto fm1 = fChanInfoFromMAC5.find(mac5);
+  if (fm1 == fChanInfoFromMAC5.end()) return badinfo;
+  return fm1->second;
+}
+
+
+SBND::CRTChannelMapService::ChanInfo_t SBND::CRTChannelMapService::GetChanInfoFromOfflChan(unsigned int offlineChannel) const {
+
+  SBND::CRTChannelMapService::ChanInfo_t badinfo{};
+  badinfo.valid = false;
+  auto fm = fChanInfoFromOfflChan.find(offlineChannel);
+  if (fm == fChanInfoFromOfflChan.end()) return badinfo;
+  return fm->second;
+}
+
+DEFINE_ART_SERVICE(SBND::CRTChannelMapService)
