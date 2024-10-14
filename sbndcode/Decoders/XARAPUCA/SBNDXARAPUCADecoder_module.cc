94cc62e2d3f23597704d14175b1f8be3a439b9be
Alicia Vázquez-Ramos <aliciavr@ugr.es>
2024-10-14 06:08:07 -0500

    Add art::Handle for artdaq::Fragment checking if there are valid CAEN Fragments and add the needed parameters to the xarapucadecoder.fcl file.

--- sbndcode/Decoders/XARAPUCA/SBNDXARAPUCADecoder_module.cc
+++ sbndcode/Decoders/XARAPUCA/SBNDXARAPUCADecoder_module.cc
@@ -17,6 +17,9 @@
 
 #include <memory>
 
+#include "artdaq-core/Data/Fragment.hh"
+#include "sbndaq-artdaq-core/Overlays/Common/CAENV1740Fragment.hh"
+
 #include "lardataobj/RawData/OpDetWaveform.h"
 
 namespace sbndaq {
@@ -42,7 +45,15 @@ public:
 private:
 
   // Declare member data here.
+  uint event_counter;
+
+  uint fnum_caen_boards;
+
+  std::string fcaen_module_label;
+  std::vector<std::string> fcaen_fragment_name;
+
   std::string fch_instance_name;
+
 };
 
 
@@ -50,13 +61,23 @@ sbndaq::SBNDXARAPUCADecoder::SBNDXARAPUCADecoder(fhicl::ParameterSet const& p)
   : EDProducer{p}  // ,
   // More initializers here.
 {
-
   std::cout << "Entering the constructor" << std::endl;
+ 
+  event_counter = 0;
+  
+  fnum_caen_boards = p.get<uint> ("num_caen_boards");
+  
+  fcaen_module_label = p.get<std::string> ("caen_module_label");
+  fcaen_fragment_name = p.get<std::vector <std::string> > ("caen_fragment_name");
+  
+  fch_instance_name = p.get<std::string> ("xarapucaInstanceName");
+  
+  std::cout << event_counter << "\t - xarapucaInstanceName: " << fch_instance_name << "\t - caen module label: " << fcaen_module_label << std::endl;
+  std::cout << "Num CAEN boards: " << fnum_caen_boards << std::endl;
 
-  fch_instance_name = p.get<std::string>("xarapucaInstanceName");
-  std::cout << "xarapucaInstanceName: " << fch_instance_name << std::endl;
   // Call appropriate produces<>() functions here.
   produces< std::vector <raw::OpDetWaveform> > (fch_instance_name);
+  
   // Call appropriate consumes<>() for any products to be retrieved by this module.
 }
 
@@ -64,9 +85,38 @@ void sbndaq::SBNDXARAPUCADecoder::produce(art::Event& e)
 {
   std::cout << "Entering the produce function" << std::endl;
   // Implementation of required member function here.
-  auto wvfms = std::make_unique <std::vector <raw::OpDetWaveform> >();
+  event_counter++;
 
-  e.put(std::move(wvfms), fch_instance_name);
+  // Initialize output product
+  auto wvfms = std::make_unique <std::vector <raw::OpDetWaveform> > ();
+
+  // Create a vector for all fragments for <fnum_caen_boards> boards
+  std::vector< std::vector < artdaq::Fragment> > fragments (fnum_caen_boards);
+
+  bool found_caen = false;
+
+  for (const std::string &caen_name: fcaen_fragment_name) {
+    art::Handle<std::vector <artdaq::Fragment> > fragment_handle;
+    e.getByLabel(fcaen_module_label, caen_name, fragment_handle);
+
+    if (!fragment_handle.isValid()) {
+      std::cout << "\tHandle not valid for " << caen_name << std::endl;
+    } else if (fragment_handle->size() == 0) {
+      std::cout << "\tHandle with size " << fragment_handle->size() << " for " << caen_name << std::endl;
+    } else {
+      found_caen |= true;
+      std::cout << "\tHandle valid for " << caen_name << " with size " << fragment_handle->size() << std::endl;
+    }
+  }
+
+  if (!found_caen) {
+    std::cout << "\tNo CAEN V1740 fragments of any type found, pushing empty waveforms." << std::endl;
+    e.put(std::move(wvfms), fch_instance_name);      
+  } else {
+    e.put(std::move(wvfms), fch_instance_name);
+  }
+
+  std::cout << std::endl;
 }
 
 DEFINE_ART_MODULE(sbndaq::SBNDXARAPUCADecoder)
