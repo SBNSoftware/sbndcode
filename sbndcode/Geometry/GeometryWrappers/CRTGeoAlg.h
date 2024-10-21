f70225d1bbf78360a79c5dc3ffc9ca4d8a41668d
Henry Lay <h.lay@sheffield.ac.uk>
2024-10-21 09:48:26 -0500

    Update wrappers accordingly

--- sbndcode/Geometry/GeometryWrappers/CRTGeoAlg.h
+++ sbndcode/Geometry/GeometryWrappers/CRTGeoAlg.h
@@ -166,7 +166,7 @@ namespace sbnd::crt {
       double modulePosMother[3];
       moduleNode->LocalToMaster(origin, modulePosMother);
 
-      if(_minos || _adID == 70)
+      if(_minos || _adID == 70 || _adID == 139)
         orientation = (modulePosMother[2] < 0);
       else
         orientation = (modulePosMother[2] > 0);
