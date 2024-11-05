abc0ac10628c949cc745144fa5662c7e049c1c46
Lauren Yates <lyates@mit.edu>
2024-11-04 20:45:04 -0600

    final (hopefully) version of filter module

--- sbndcode/Filters/CRTTrackFilter_module.cc
+++ sbndcode/Filters/CRTTrackFilter_module.cc
@@ -24,33 +24,46 @@ public:
   //void    reconfigure(fhicl::ParameterSet const& p);
   
 private:
+  // fcl parameters
   std::string fCRTTrackModuleLabel;
-  double fXPosMin;
-  double fXPosMax;
+  double fXPosMin;     // minimum absolute value of x position of CRT track endpoints, in cm
+  double fXPosMax;     // maximum absolute value of x position of CRT track endpoints, in cm
+  double fThetaXZMax;  // maximum angle theta_XZ of CRT track, in degrees
+  bool fUseLengthMin;  // flag to use the fancy calculation of projected CRT track length the active volume, and to cut on that
+  double fLengthMin;   // if above is true, then this is minimum length in the active volume, in cm
   bool fVerbose;
+  // useful constants
   const std::set<sbnd::crt::CRTTagger> fNSTaggers = { sbnd::crt::kSouthTagger, sbnd::crt::kNorthTagger };
+  const double PI = 3.14159265358979323;
+  const double z_AV_S   =    0.;
+  const double z_AV_N   =  500.;
+  const double y_AV_top =  200.;
+  const double y_AV_bot = -200.;
 };
 
 CRTTrackFilter::CRTTrackFilter(fhicl::ParameterSet const& p) : EDFilter{p} {
   fCRTTrackModuleLabel = p.get<std::string>("CRTTrackModuleLabel","crttracks");
-  fXPosMin = p.get<double>("XPosMin",150.);
-  fXPosMax = p.get<double>("XPosMax",200.);
-  fVerbose = p.get<bool>("Verbose",true);
+  fXPosMin             = p.get<double>("XPosMin",150.);
+  fXPosMax             = p.get<double>("XPosMax",200.);
+  fThetaXZMax          = p.get<double>("ThetaXZMax",10.);
+  fUseLengthMin        = p.get<bool>("UseLengthMin",true);
+  fLengthMin           = p.get<double>("LengthMin",300.);
+  fVerbose             = p.get<bool>("Verbose",true);
 }
 
 bool CRTTrackFilter::filter(art::Event& e) {
-
+  
+  if(fVerbose) std::cout << "Event " << e.id().event() << "..." << std::endl;
+  
   // Get the CRT track data product
   art::Handle<std::vector<sbnd::crt::CRTTrack>> CRTTrackHandle;
   e.getByLabel(fCRTTrackModuleLabel, CRTTrackHandle);
   auto const& CRTTrackVec(*CRTTrackHandle);
-
-  if(fVerbose) std::cout << "Event " << e.id().event() << "..." << std::endl;
-
+  
   // Loop over CRT tracks in the event...
   for(size_t i_ct=0; i_ct<CRTTrackVec.size(); ++i_ct) {
     
-    std::cout << "  Track " << i_ct << "..." << std::endl;
+    if(fVerbose) std::cout << "  Track " << i_ct << "..." << std::endl;
 
     // Get this CRT track
     auto const& crt_trk = CRTTrackVec[i_ct];
@@ -63,11 +76,11 @@ bool CRTTrackFilter::filter(art::Event& e) {
     }
     if(crt_trk.Taggers() != fNSTaggers) continue;
     if(fVerbose) std::cout << "    Found a north-south crossing muon track" << std::endl;
-
-    // Get the (x, y, z) coordinates of the south and north CRT track points
+    
+    // Get (x, y, z) coordinates of the south and north CRT track points
     double x_S, y_S, z_S, x_N, y_N, z_N;
     for(auto point : crt_trk.Points()) {
-      if(fVerbose) std::cout << "      Point N: " << point.X() << " " << point.Y() << " " << point.Z() << std::endl;
+      if(fVerbose) std::cout << "      Point: " << point.X() << " " << point.Y() << " " << point.Z() << std::endl;
       if(point.Z() < 0.) {
 	x_S = point.X();
 	y_S = point.Y();
@@ -83,23 +96,75 @@ bool CRTTrackFilter::filter(art::Event& e) {
       std::cout << "    South point: " << x_S << " " << y_S << " " << z_S << std::endl;
       std::cout << "    North point: " << x_N << " " << y_N << " " << z_N << std::endl;
     }
+    
+    // Calculate (signed) difference between north and south endpoints of the CRT track
+    double Delta_x = x_N-x_S;
+    double Delta_y = y_N-y_S;
+    double Delta_z = z_N-z_S;
 
     // Check whether both CRT track points satisfy the x position requirements
-    if( not(abs(x_N) > fXPosMin and abs(x_N) < fXPosMax) ) continue;
-    if(fVerbose) std::cout << "    Found track that meets x position requirements on north point" << std::endl;
     if( not(abs(x_S) > fXPosMin and abs(x_S) < fXPosMax) ) continue;
+    if(fVerbose) std::cout << "    Found track that meets x position requirements on south point" << std::endl;
+    if( not(abs(x_N) > fXPosMin and abs(x_N) < fXPosMax) ) continue;
     if(fVerbose) std::cout << "    Found track that meets x position requirements on both points" << std::endl;
     // Check whether Delta x between the CRT track points satisfies the requirement: Delta x < tan(10deg) * Delta z
-    if( not(abs(x_N-x_S) < 0.176327*abs(z_N-z_S)) ) continue;
+    if( not(abs(Delta_x) < tan(fThetaXZMax*PI/180)*abs(Delta_z)) ) continue;
     if(fVerbose) std::cout << "    Found track that meets Delta x requirement" << std::endl;
+    // If not using the fancy cut on a minimum length in the active volume, then at least...
     // Check whether at least one CRT track point is below the top of the TPC active volume
-    // ... part of trying to make sure that there's a decent length of track in the active volume
-    if( not(y_N < 200. or y_S < 200.) ) continue;
-    if(fVerbose) std::cout << "    Found track that meets y position requirement" << std::endl;
-
-    // If we've made it this far, then this track is good
+    if( not(fUseLengthMin) ) {
+      if( not(y_N < y_AV_top or y_S < y_AV_top) ) continue;
+      if(fVerbose) std::cout << "    Found track that meets simple y position requirement" << std::endl;
+    }
+    // ...Else, do the fancy thing...
+    // Check whether track length inside the active volume satisfies the minimum length requirement...
+    //   Note: All of the below assumes that the track does not exit through the anode, which *should* be true given fXPosMax = 200cm
+    if(fUseLengthMin) {
+      if(fVerbose) std::cout << "      Evaluating points where CRT track is projected to cross active volume boundaries" << std::endl;
+      // Initialize vector for points where the CRT track crosses the boundary of the active volume in (y,z) space
+      std::vector<geo::Point_t> crossing_points;
+      // Calculate if/where the track crosses the south face of the TPC
+      double y_AV_S = (Delta_y/Delta_z)*(z_AV_S-z_S)+y_S;
+      if( y_AV_S > y_AV_bot and y_AV_S < y_AV_top ) {
+	double x_AV_S = (Delta_x/Delta_z)*(z_AV_S-z_S)+x_S;
+	crossing_points.push_back( {x_AV_S, y_AV_S, z_AV_S} );
+	if(fVerbose) std::cout << "      Found crossing point on south face of active volume at (" << x_AV_S << ", " << y_AV_S << ", " << z_AV_S << ")" << std::endl;
+      }
+      // Calculate if/where the track crosses the north face of the TPC
+      double y_AV_N = (Delta_y/Delta_z)*(z_AV_N-z_S)+y_S;
+      if( y_AV_N > y_AV_bot and y_AV_N < y_AV_top ) {
+	double x_AV_N = (Delta_x/Delta_z)*(z_AV_N-z_S)+x_S;
+	crossing_points.push_back( {x_AV_N, y_AV_N, z_AV_N} );
+	if(fVerbose) std::cout << "      Found crossing point on north face of active volume at (" << x_AV_N << ", " << y_AV_N << ", " << z_AV_N << ")" << std::endl;
+      }
+      // Calculate if/where the track crosses the top of the TPC
+      double z_AV_top = (Delta_z/Delta_y)*(y_AV_top-y_S)+z_S;
+      if( z_AV_top > z_AV_S and z_AV_top < z_AV_N ) {
+	double x_AV_top = (Delta_x/Delta_y)*(y_AV_top-y_S)+x_S;
+	crossing_points.push_back( {x_AV_top, y_AV_top, z_AV_top} );
+	if(fVerbose) std::cout << "      Found crossing point on top face of active volume at (" << x_AV_top << ", " << y_AV_top << ", " << z_AV_top << ")" << std::endl;
+      }
+      // Calculate if/where the track crosses the bottom of the TPC
+      double z_AV_bot = (Delta_z/Delta_y)*(y_AV_bot-y_S)+z_S;
+      if( z_AV_bot > z_AV_S and z_AV_bot < z_AV_N ) {
+	double x_AV_bot = (Delta_x/Delta_y)*(y_AV_bot-y_S)+x_S;
+	crossing_points.push_back( {x_AV_bot, y_AV_bot, z_AV_bot} );
+	if(fVerbose) std::cout << "      Found crossing point on bottom face of active volume at (" << x_AV_bot << ", " << y_AV_bot << ", " << z_AV_bot << ")" << std::endl;
+      }
+      // Print a warning if there aren't exactly zero or two crossing points, which would imply a bug in the above
+      if( not(crossing_points.size()==0 or crossing_points.size()==2)) std::cout << "    WARNING: found " << crossing_points.size() << " active volume crossings (expected exactly 0 or exactly 2)" << std::endl;
+      // Calculate the length of the track inside the active volume
+      double trk_len_AV = 0.;
+      if(crossing_points.size()==2) trk_len_AV = (crossing_points[1] - crossing_points[0]).R();
+      if(fVerbose) std::cout << "      Estimated track length in active volume is " << trk_len_AV << "cm" << std::endl;
+      // Check whether track length inside the active volume satisfies the minimum length requirement
+      if( trk_len_AV < fLengthMin ) continue;
+      if(fVerbose) std::cout << "    Found track that meets track length in active volume requirement" << std::endl;
+    }
+    
+    // If we've made it this far, then this track is good and this event passes the filter
     if(fVerbose) std::cout << "    Found a good track!" << std::endl;
-    if(fVerbose) std::cout << "    ToF " << crt_trk.ToF() << "ns" << std::endl;
+    //if(fVerbose) std::cout << "    ToF " << crt_trk.ToF() << "ns" << std::endl;
     return true;
 
   } // end loop over CRT tracks
