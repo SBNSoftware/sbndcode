bdca27d26f0dd324c9dd8590b773f91278b6c205
Tingjun Yang <tjyang@fnal.gov>
2024-11-21 14:21:27 -0600

    Add association between recob::Slice and lcvn::SBNDPixelMap.

--- sbndcode/SBNDCVN/module_helpers/classes.h
+++ sbndcode/SBNDCVN/module_helpers/classes.h
@@ -1,6 +1,7 @@
 #include "canvas/Persistency/Common/Wrapper.h"
 #include "canvas/Persistency/Common/Assns.h"
 #include "sbndcode/SBNDCVN/module_helpers/SBNDPixelMap.h"
+#include "lardataobj/RecoBase/Slice.h"
 #include <vector>
 #include <map>
 #include <utility>
