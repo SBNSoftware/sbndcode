ac2a79ebedcddfd0183f694f510d125971a2f11a
Tingjun Yang <tjyang@fnal.gov>
2024-11-21 14:49:55 -0600

    Add association between lcvn::SBNDPixelMap and lcvn::Result.

--- sbndcode/SBNDCVN/module_helpers/classes.h
+++ sbndcode/SBNDCVN/module_helpers/classes.h
@@ -2,6 +2,7 @@
 #include "canvas/Persistency/Common/Assns.h"
 #include "sbndcode/SBNDCVN/module_helpers/SBNDPixelMap.h"
 #include "lardataobj/RecoBase/Slice.h"
+#include "larrecodnn/CVN/func/Result.h"
 #include <vector>
 #include <map>
 #include <utility>
