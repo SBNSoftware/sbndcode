97c4a823c83c0b5511d8107514be50b3bae287fc
Jjm321814 <mclaughlinjacob1996@gmail.com>
2025-04-08 10:52:57 -0500

    Feature/blip real producer module (#715)
    * Added some blip saving
    
    * typo
    
    * Updated way new associations are generated. Left old space point association alone for now
    
    * Messed up declaration in art association
    
    * Messed up declaration in art association
    
    * Maybe this Art:PTRMaker call works?
    
    * Updating art dictionary. May need to adjust producer still based on error messages
    
    * PFParticle is not supported anymore
    
    * reordering the way things are built. Changing one function call
    
    * trying other add library call
    
    * trying other add library call
    
    * Fixed typo
    
    * Larsoft tells me to update classes def
    
    * Larsoft tells me to update classes def
    
    * trying to reorder compilations
    
    * Im just guessing
    
    * typo
    
    * typo
    
    * missing import
    
    * Larsoft wants more templates
    
    * Too many templates
    
    * Too many templates
    
    * Removing space point association
    
    * New unused variable
    
    * New unused variable
    
    * Trying push back instead?
    
    * Oh wrong producesgit add sbndcode/BlipRecoSBND/BlipRecoProducer_module.cc
    
    * Typo
    
    * It works! Now fix the larsoft exit for the producer
    
    * typo
    
    * typo
    
    * Cleaning up some work from yesterday
    
    * Adding back one class larsoft asked for

--- sbndcode/BlipRecoSBND/Utils/classes.h
+++ sbndcode/BlipRecoSBND/Utils/classes.h
@@ -18,6 +18,7 @@
 #include "lardataobj/RecoBase/Hit.h"
 #include "nusimdata/SimulationBase/MCTruth.h"
 #include "sbndcode/BlipRecoSBND/Utils/DataTypes.h"
+#include "lardataobj/RecoBase/SpacePoint.h"
 
 //
 // Only include objects that we would like to be able to put into the event.
@@ -25,5 +26,11 @@
 //
 
 template class art::Assns<recob::Hit,blip::Blip,void>;
-template class art::Wrapper<std::vector<blip::Blip> >;
+template class art::Wrapper<art::Assns<recob::Hit,blip::Blip,void> >;
 template class std::vector<blip::Blip>;
+template class art::Wrapper<std::vector<blip::Blip> >;
+template class std::map<int, TVector3>;
+template class art::Assns<blip::Blip,recob::Hit,void>;
+template class art::Wrapper<art::Assns<blip::Blip,recob::Hit,void> >;
+template class art::Assns<blip::Blip,recob::SpacePoint,void>;
+template class art::Wrapper<art::Assns<blip::Blip,recob::SpacePoint,void> >;
