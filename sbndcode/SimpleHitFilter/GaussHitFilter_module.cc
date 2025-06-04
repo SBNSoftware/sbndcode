0989f1a9132a2d9867d0f8658268faa6eda971da
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2025-06-04 13:54:34 -0500

    debug

--- sbndcode/SimpleHitFilter/GaussHitFilter_module.cc
+++ sbndcode/SimpleHitFilter/GaussHitFilter_module.cc
@@ -95,6 +95,7 @@ void GaussHitFilter::produce(art::Event& e)
     int startIndex = thisHit->StartTick();
     int endIndex = thisHit->EndTick() + 1;
     std::cout << " start  " << startIndex << "  end  " << endIndex << std::endl;
+    std::cout << *(thisWire->Signal().begin()+startIndex) << "  " << *(thisWire->Signal().begin() + endIndex) << std::endl;
     double MaxVal = *std::max_element(thisWire->Signal().begin() + startIndex, thisWire->Signal().begin() + endIndex);
     //We don't want to compare to peak amplitude but rather a certain index in the roi? better match to gausshitfinder
     //Could explain the small excess in my filtered values. Doesn't really explain the <1% of baseline events we cut away
