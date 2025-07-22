a7d853fa3caa5da7e2f09cce42c62c286d4c2ab8
sergio-dv <Sergio.Dominguez@ciemat.es>
2025-07-22 05:23:15 -0500

    developing PosRecoCVN

---
+++ sbndcode/PosRecoCVN/module/classes.h
@@ -0,0 +1,8 @@
+#include "sbndcode/PosRecoCVN/module/PixelMapVars.h"
+#include "canvas/Persistency/Common/Wrapper.h"
+
+// This file is needed to tell ROOT about our custom data structures
+// so they can be written to and read from ROOT files properly
+
+// Explicit instantiation of wrapper for our custom types
+template class art::Wrapper<PixelMapVars>;
