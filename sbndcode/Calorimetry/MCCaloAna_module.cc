493c91b8189a1899de54b47e9aafc835bac4e7b8
lynnt20 <lynnt@uchicago.edu>
2025-11-13 13:00:04 -0600

    add more features to mccalo, now assumes only ONE neutrino interaction per event

--- sbndcode/Calorimetry/MCCaloAna_module.cc
+++ sbndcode/Calorimetry/MCCaloAna_module.cc
@@ -54,14 +54,49 @@ private:
   int _subrun;
   int _event;
 
-  std::vector<double> _nu_E;
-  std::vector<int> _nu_CCNC;
-  std::vector<double> _true_gamma;
-  std::vector<double> _true_charge;
-  std::vector<double> _true_energy;
+  double _nu_E;
+  double _nu_P; 
+  int    _nu_CCNC;
+  double _nu_X;
+  double _nu_Y;
+  double _nu_Z;
+
+  double _min_x, _max_x, _min_y, _max_y, _min_z, _max_z; 
+
+  double _true_gamma;
+  double _true_charge;
+  double _true_energy;
 
   // Declare member data here.
 
+  std::vector<double> _chargecorr_gamma;
+  std::vector<double> _chargecorr_visibility;
+  std::vector<double> _chargecorr_visibility_witheff;
+  std::vector<double> _lightcorr_gamma; 
+  std::vector<double> _lightcorr_gamma_resim;
+  std::vector<double> _lightcorr_visibility;
+  std::vector<double> _lightcorr_visibility_witheff;
+
+  std::vector<float> _opdet_vuv_eff;
+  std::vector<float> _opdet_vis_eff;
+
+  std::unique_ptr<phot::SemiAnalyticalModel> _semi_model;
+  fhicl::ParameterSet _vuv_params;
+  fhicl::ParameterSet _vis_params;
+  opdet::sbndPDMapAlg _opdetmap; //map for photon detector types
+  unsigned int _nchan = _opdetmap.size();
+
+
+  std::vector<std::vector<double>> CalcVisibility(std::vector<geo::Point_t> xyz_v, 
+                                                  std::vector<double> charge_v);
+  std::vector<std::vector<double>> CalcPE(std::vector<geo::Point_t> xyz_v, 
+                                                  std::vector<double> light_v);
+  std::vector<std::vector<double>> FillSimPhotonsLite(std::vector<art::Handle<std::vector<sim::SimPhotonsLite> >> photonHandle_list);
+  void CalcLight(std::vector<double> flash_pe_v,
+                                   std::vector<double> dir_visibility,
+                                   std::vector<double> ref_visibility,
+                                   std::vector<double> &total_gamma_v,
+                                   std::vector<double> &total_visibility_v);
 };
 
 
@@ -71,14 +106,34 @@ sbnd::MCCaloAna::MCCaloAna(fhicl::ParameterSet const& p)
 {
   art::ServiceHandle<art::TFileService> fs;
   _tree = fs->make<TTree>("mccalo_tree","");
-  _tree->Branch("run",             &_run,        "run/I");
-  _tree->Branch("subrun",          &_subrun,     "subrun/I");
-  _tree->Branch("event",           &_event,      "event/I");
-  _tree->Branch("nu_E",           "std::vector<double>",  &_nu_E);
-  _tree->Branch("nu_CCNC",        "std::vector<int>",     &_nu_CCNC);
-  _tree->Branch("true_gamma",      "std::vector<double>",  &_true_gamma);
-  _tree->Branch("true_charge",     "std::vector<double>",  &_true_charge);
-  _tree->Branch("true_energy",     "std::vector<double>",  &_true_energy);
+  _tree->Branch("run",             &_run,         "run/I");
+  _tree->Branch("subrun",          &_subrun,      "subrun/I");
+  _tree->Branch("event",           &_event,       "event/I");
+  _tree->Branch("nu_E",            &_nu_E,        "nu_E/D");
+  _tree->Branch("nu_P",            &_nu_P,        "nu_P/D");
+  _tree->Branch("nu_CCNC",         &_nu_CCNC,     "nu_CCNC/I");
+  _tree->Branch("nu_X",            &_nu_X,        "nu_X/D");
+  _tree->Branch("nu_Y",            &_nu_Y,        "nu_Y/D");
+  _tree->Branch("nu_Z",            &_nu_Z,        "nu_Z/D");
+  _tree->Branch("max_x",           &_max_x,       "max_x/D");
+  _tree->Branch("max_y",           &_max_y,       "max_y/D");
+  _tree->Branch("max_z",           &_max_z,       "max_z/D");
+  _tree->Branch("min_x",           &_min_x,       "min_x/D");
+  _tree->Branch("min_y",           &_min_y,       "min_y/D");
+  _tree->Branch("min_z",           &_min_z,       "min_z/D");
+  _tree->Branch("true_gamma",      &_true_gamma,  "true_gamma/D");
+  _tree->Branch("true_charge",     &_true_charge, "true_charge/D");
+  _tree->Branch("true_energy",     &_true_energy, "true_energy/D");
+  _tree->Branch("true_gamma_tpc",  "std::vector<double>",  &_true_gamma_tpc);
+  _tree->Branch("true_measphotons","std::vector<double>", &_true_measphotons);
+  _tree->Branch("true_resimphotons","std::vector<double>", &_true_resimphotons);
+  _tree->Branch("chargecorr_gamma", "std::vector<double>", &_chargecorr_gamma);
+  _tree->Branch("chargecorr_visibility","std::vector<double>", &_chargecorr_visibility);
+  _tree->Branch("chargecorr_visibility_witheff","std::vector<double>", &_chargecorr_visibility_witheff);
+  _tree->Branch("lightcorr_gamma", "std::vector<double>", &_lightcorr_gamma);
+  _tree->Branch("lightcorr_gamma_resim","std::vector<double>",&_lightcorr_gamma_resim);
+  _tree->Branch("lightcorr_visibility","std::vector<double>", &_lightcorr_visibility);
+  _tree->Branch("lightcorr_visibility_witheff","std::vector<double>", &_lightcorr_visibility_witheff);
 }
 
 void sbnd::MCCaloAna::analyze(art::Event const& e)
@@ -87,11 +142,32 @@ void sbnd::MCCaloAna::analyze(art::Event const& e)
   _subrun = e.id().subRun();
   _event  = e.id().event();
 
-  _nu_E.clear();
-  _nu_CCNC.clear();
-  _true_gamma.clear();
-  _true_charge.clear();
-  _true_energy.clear();
+  _nu_E = 0;
+  _nu_P = 0;
+  _nu_CCNC = -1;
+  _nu_X = -999;
+  _nu_Y = -999;
+  _nu_Z = -999;
+  _true_gamma = 0;
+  _true_charge = 0;
+  _true_energy = 0;
+
+  _min_x = _min_y = _min_z = 1e9;
+  _max_x = _max_y = _max_z = -1e9;
+
+<<<<<<< HEAD
+=======
+  _true_gamma_tpc.clear();
+  _true_measphotons.clear();
+  _true_resimphotons.clear();
+  
+  _chargecorr_gamma.clear();
+  _chargecorr_visibility.clear();
+  _chargecorr_visibility_witheff.clear();
+  _lightcorr_gamma.clear();
+  _lightcorr_gamma_resim.clear();
+  _lightcorr_visibility.clear();
+  _lightcorr_visibility_witheff.clear();
 
   art::ServiceHandle<cheat::ParticleInventoryService> piserv;
 
@@ -117,27 +193,289 @@ void sbnd::MCCaloAna::analyze(art::Event const& e)
   _true_charge.resize(mctruths.size(), 0);
   _true_energy.resize(mctruths.size(), 0);
 
+  std::vector<art::Handle<std::vector<sim::SimPhotonsLite> >> fLitePhotonHandle_list;
+  fLitePhotonHandle_list = e.getMany<std::vector<sim::SimPhotonsLite>>();    
+  auto opdet_simphotons = FillSimPhotonsLite(fLitePhotonHandle_list);
+
+  std::vector<double> edep_photons;
+  std::vector<double> edep_electrons;
+  std::vector<double> edep_energy;
+  std::vector<geo::Point_t> edep_points;
+  // std::vector<std::vector<double>> edep_x, edep_y, edep_z; 
+
+  _true_measphotons.resize(_nchan,0);
+  _true_resimphotons.resize(_nchan,0);
+
+  _true_gamma_tpc.resize(2,0);
+  
+  edep_photons.resize(mctruths.size());
+  edep_electrons.resize(mctruths.size());
+  edep_energy.resize(mctruths.size());
+  edep_points.resize(mctruths.size());
+
   for (size_t n_dep=0; n_dep < energyDeps.size(); n_dep++){
     auto energyDep = energyDeps[n_dep];
     const auto trackID = energyDep->TrackID();
 
     art::Ptr<simb::MCTruth> mctruth = piserv->TrackIdToMCTruth_P(trackID);
-    if (mctruth->Origin() != simb::kBeamNeutrino) continue;
 
-    auto it = std::find(mctruths.begin(), mctruths.end(), mctruth);
-    if (it == mctruths.end()) {
-      std::cout << "No matching MCTruth found for trackID: " << trackID << std::endl;
-      continue;
+    auto nparticles = mctruth->NParticles();
+    std::cout << "nparticles: " << nparticles << std::endl;
+    if (mctruth->Origin() == simb::kBeamNeutrino) 
+      _nu_CCNC = mctruth->GetNeutrino().CCNC();
+    for (int p=0; p < nparticles; p++){
+      simb::MCParticle const& par = mctruth->GetParticle(p);    
+      std::cout << "pdg: " << par.PdgCode() << std::endl;
+      if (par.StatusCode()==0 && mctruth->Origin()==simb::kBeamNeutrino){
+        _nu_E = par.E();
+        _nu_P = par.P();
+        _nu_X = par.Vx();
+        _nu_Y = par.Vy();
+        _nu_Z = par.Vz();
+        break;
+      }
+      else if (par.StatusCode()==1 && mctruth->Origin()==4){
+        _nu_E = par.E();
+        _nu_P = par.P();
+        _nu_X = par.Vx();
+        _nu_Y = par.Vy();
+        _nu_Z = par.Vz();
+        break;
+      }
     }
 
-    // get the index of the mctruth in the set 
-    auto mctruth_index = std::distance(mctruths.begin(), it);
+    // auto it = std::find(mctruths.begin(), mctruths.end(), mctruth);
+    // if (it == mctruths.end()) {
+    //   std::cout << "No matching MCTruth found for trackID: " << trackID << std::endl;
+    //   continue;
+    // }
 
+<<<<<<< HEAD
     _true_gamma[mctruth_index]  += energyDep->NumPhotons();
     _true_charge[mctruth_index] += energyDep->NumElectrons();
     _true_energy[mctruth_index] += energyDep->Energy();
   }  
   _tree->Fill();
+}
+
+=======
+    // // get the index of the mctruth in the set 
+    // auto mctruth_index = std::distance(mctruths.begin(), it);
+
+    _true_gamma  += energyDep->NumPhotons();
+    _true_charge += energyDep->NumElectrons();
+    _true_energy += energyDep->Energy();
+
+    edep_photons.push_back(energyDep->NumPhotons());
+    edep_electrons.push_back(energyDep->NumElectrons());
+    edep_energy.push_back(energyDep->Energy());
+    edep_points.push_back(energyDep->Start());
+
+    if (energyDep->Start().X() < 0)
+      _true_gamma_tpc.at(0) += energyDep->NumPhotons();
+    else
+      _true_gamma_tpc.at(1) += energyDep->NumPhotons();
+
+    if (energyDep->Start().X() > _max_x) _max_x = energyDep->Start().X(); 
+    if (energyDep->Start().Y() > _max_y) _max_y = energyDep->Start().Y(); 
+    if (energyDep->Start().Z() > _max_z) _max_z = energyDep->Start().Z(); 
+    if (energyDep->Start().X() < _min_x) _min_x = energyDep->Start().X(); 
+    if (energyDep->Start().Y() < _min_y) _min_y = energyDep->Start().Y(); 
+    if (energyDep->Start().Z() < _min_z) _min_z = energyDep->Start().Z(); 
+  }
+
+
+  auto resim_pe  = CalcPE(edep_points,edep_photons);
+
+  for (size_t ich=0; ich < opdet_simphotons.at(0).size(); ich++){
+    std::string pd_type = _opdetmap.pdType(ich);
+    if(pd_type=="xarapuca_vis" || pd_type=="xarapuca_vuv") continue;
+    _true_measphotons.at(ich) = opdet_simphotons.at(0).at(ich) + opdet_simphotons.at(1).at(ich);
+    _true_resimphotons.at(ich) = resim_pe.at(0).at(ich) + resim_pe.at(1).at(ich);
+    // std::cout << "ich " << ich <<  " (dir sim/resim): " << opdet_simphotons.at(0).at(ich) << ", " << resim_pe.at(0).at(ich) << std::endl;
+    // std::cout << "ich " << ich <<  " (ref sim/resim): " << opdet_simphotons.at(1).at(ich) << ", " << resim_pe.at(1).at(ich) << std::endl;
+  }
+
+  for (size_t nnu; nnu < mctruths.size(); nnu++){
+    auto true_vis_charge = CalcVisibility(edep_points,edep_electrons);
+    auto true_vis_light  = CalcVisibility(edep_points,edep_photons);
+
+    _chargecorr_gamma.resize(_nchan,0);
+    _chargecorr_visibility.resize(_nchan,0);
+    _chargecorr_visibility_witheff.resize(_nchan,0);
+    _lightcorr_gamma.resize(_nchan,0);
+    _lightcorr_gamma_resim.resize(_nchan,0);
+    _lightcorr_visibility.resize(_nchan,0);
+    _lightcorr_visibility_witheff.resize(_nchan,0);
+
+    CalcLight(_true_measphotons,  true_vis_charge.at(0), true_vis_charge.at(1),_chargecorr_gamma,_chargecorr_visibility);
+    CalcLight(_true_measphotons,  true_vis_light.at(0), true_vis_light.at(1),_lightcorr_gamma,_lightcorr_visibility);
+    CalcLight(_true_resimphotons, true_vis_light.at(0), true_vis_light.at(1),_lightcorr_gamma_resim,_lightcorr_visibility);
+
+    for (uint ch = 0; ch < _nchan; ch++){
+      auto vuv_eff = _opdet_vuv_eff.at(ch);
+      auto vis_eff = _opdet_vis_eff.at(ch);
+      _chargecorr_visibility_witheff[ch] = vuv_eff*true_vis_charge.at(0)[ch] + vis_eff*true_vis_charge.at(1)[ch];
+      _lightcorr_visibility_witheff[ch] = vuv_eff*true_vis_light.at(0)[ch] + vis_eff*true_vis_light.at(1)[ch];
+    }
+  }
+
+  _tree->Fill();
+}
+
+std::vector<std::vector<double>> sbnd::MCCaloAna::CalcVisibility(std::vector<geo::Point_t> xyz_v,
+                                                                     std::vector<double> charge_v){
+  // returns of two vectors (len is # of opdet) for the visibility for every opdet                                     
+  if (xyz_v.size() != charge_v.size()) std::cout << "spacepoint coord and charge vector size mismatch" << std::endl;
+
+  std::vector<double> dir_visibility_map(_nchan, 0);
+  std::vector<double> ref_visibility_map(_nchan, 0);
+  double sum_charge0 = 0;
+  double sum_charge1 = 0;
+
+  // std::cout << "initial dir_visibility_map: " << dir_visibility_map[7] << std::endl;
+  // std::cout << "initial ref_visibility_map: " << ref_visibility_map[7] << std::endl;
+  for (size_t i=0; i<xyz_v.size(); i++){
+    geo::Point_t const xyz = xyz_v[i];
+    auto charge = charge_v[i];
+    if (charge<=0) continue;
+
+    if (xyz.X() < 0) sum_charge0+= charge;
+    else sum_charge1 += charge; 
+
+    std::vector<double> direct_visibility;
+    std::vector<double> reflect_visibility;
+    _semi_model->detectedDirectVisibilities(direct_visibility, xyz);
+    _semi_model->detectedReflectedVisibilities(reflect_visibility, xyz);
+    if (dir_visibility_map.size() != direct_visibility.size()) std::cout << "mismatch of visibility vector size" << std::endl;
+    // if (i < 10){
+    //   std::cout << "xyz: " << xyz << std::endl;
+    //   std::cout << "charge: " << charge << std::endl;
+    //   std::cout << "dir_visibility    : " << direct_visibility[7] << std::endl;
+    //   std::cout << "ref_visibility    : " << reflect_visibility[7] << std::endl;
+    //   std::cout << "dir_visibility_map: " << dir_visibility_map[7] << std::endl;
+    //   std::cout << "ref_visibility_map: " << ref_visibility_map[7] << std::endl;
+    // }
+    // weight by charge
+    for (size_t ch=0; ch<direct_visibility.size(); ch++){
+      dir_visibility_map[ch] += charge*direct_visibility[ch];
+      ref_visibility_map[ch] += charge*reflect_visibility[ch];
+    }    
+  } // end spacepoint loop
+  // std::cout << "sum_charge0:" << sum_charge0 << std::endl;
+  // std::cout << "sum_charge1:" << sum_charge1 << std::endl;
+  // normalize by the total charge in each TPC
+  for (size_t ch=0; ch < dir_visibility_map.size(); ch++){
+    if (ch%2 == 0){
+      dir_visibility_map[ch] /= sum_charge0;
+      ref_visibility_map[ch] /= sum_charge0;
+    }
+    if (ch%2 == 1){
+      dir_visibility_map[ch] /= sum_charge1;
+      ref_visibility_map[ch] /= sum_charge1;
+    }
+  }
+  return {dir_visibility_map, ref_visibility_map};
+}
+
+std::vector<std::vector<double>> sbnd::MCCaloAna::CalcPE(std::vector<geo::Point_t> xyz_v,
+                                                                     std::vector<double> light_v){
+  // returns of two vectors (len is # of opdet) for the visibility for every opdet                                     
+
+  std::vector<std::vector<double>> pe_v(2);
+  for (size_t i=0; i<2;i++){
+    pe_v.at(i).resize(_nchan,0);
+  }
+
+  for (size_t i=0; i<xyz_v.size(); i++){
+    geo::Point_t const xyz = xyz_v[i];
+    auto light = light_v[i];
+    if (light <= 0) continue;
+
+    std::vector<double> direct_visibility;
+    std::vector<double> reflect_visibility;
+    _semi_model->detectedDirectVisibilities(direct_visibility, xyz);
+    _semi_model->detectedReflectedVisibilities(reflect_visibility, xyz);
+
+    for (size_t ich=0;ich<_nchan;ich++){
+      pe_v.at(0).at(ich) += light*direct_visibility.at(ich);
+      pe_v.at(1).at(ich) += light*reflect_visibility.at(ich);
+    }
+  }
+  return pe_v;
+}
+
+std::vector<std::vector<double>> sbnd::MCCaloAna::FillSimPhotonsLite(std::vector<art::Handle<std::vector<sim::SimPhotonsLite> >> photonHandle_list){
+  std::vector<std::string> fPDTypes{"pmt_coated", "pmt_uncoated"};
+  std::vector<std::vector<double>> opdet_simphotons;
+  opdet_simphotons.resize(2);
+  for (size_t i=0; i<2; i++){
+    opdet_simphotons.at(i).resize(_nchan,0);
+  }
+
+  for ( const art::Handle<std::vector<sim::SimPhotonsLite>>& litePhotonHandle: (photonHandle_list) ){
+
+    std::string spLabel = litePhotonHandle.provenance()->moduleLabel();
+    std::vector<std::string> fSimPhotonsModuleLabel{"pdfastsim"};
+    if(std::find(fSimPhotonsModuleLabel.begin(), fSimPhotonsModuleLabel.end(), spLabel)==fSimPhotonsModuleLabel.end()) continue;
+
+    // Reflected light
+    bool reflected = (litePhotonHandle.provenance()->productInstanceName() == "Reflected");
+
+    // Loop over the SimPhotonsLite
+    for ( auto const& fLitePhotons : (*litePhotonHandle) ){
+      
+      int opch=fLitePhotons.OpChannel;
+
+      std::string pd_type = _opdetmap.pdType(opch);
+      // Channels not sensitive to reflected light
+      if(reflected && pd_type=="xarapuca_vuv") continue;
+      // Channels not sensitive to direct light
+      if(!reflected && (pd_type=="xarapuca_vis" || pd_type=="pmt_uncoated")) continue;
+      
+      // Only save the PD types specified in the fhicl list
+      if(std::find(fPDTypes.begin(), fPDTypes.end(), pd_type ) != fPDTypes.end() ){
+        
+        std::map<int, int> fLitePhotons_map = fLitePhotons.DetectedPhotons;
+        int nphotons=0;
+        
+        for(auto fphoton = fLitePhotons_map.begin(); fphoton!= fLitePhotons_map.end(); fphoton++){
+          nphotons+=fphoton->second;
+        }
+
+        // Fill #photons per OpChannel
+        if(reflected){
+          opdet_simphotons.at(1).at(opch)+=nphotons;
+        }
+        else{
+          opdet_simphotons.at(0).at(opch)+=nphotons;
+        }
+      }
+    }
+  }
+
+  return opdet_simphotons;
+}
+
+
+void sbnd::MCCaloAna::CalcLight(std::vector<double> flash_pe_v,
+                                   std::vector<double> dir_visibility,
+                                   std::vector<double> ref_visibility,
+                                   std::vector<double> &total_gamma_v,
+                                   std::vector<double> &tot_visibility_v){
+  for (size_t ch = 0; ch < flash_pe_v.size(); ch++){
+    auto pe = flash_pe_v[ch];
+    // auto vuv_eff = _opdet_vuv_eff.at(ch);
+    // auto vis_eff = _opdet_vis_eff.at(ch);
+    auto vuv_eff = 1.0;
+    auto vis_eff = 1.0;
+    auto tot_visibility = vuv_eff*dir_visibility[ch] + vis_eff*ref_visibility[ch];
+    tot_visibility_v.at(ch) = tot_visibility;
+    if((pe == 0) || std::isinf(1/tot_visibility))
+      continue;
+    // deposited light is inverse of visibility * PE count 
+    total_gamma_v[ch] += (1/tot_visibility)*pe;
+  }
 }
 
 DEFINE_ART_MODULE(sbnd::MCCaloAna)
