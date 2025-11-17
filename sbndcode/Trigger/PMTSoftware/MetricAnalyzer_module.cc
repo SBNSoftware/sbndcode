28647f5714abc0b833883ab77309275cdc1e4568
lynnt20 <lynnt@uchicago.edu>
2025-11-17 12:54:06 -0600

    clean up fcls, update metric labels to reflect current daq config - producer labels should be `pmtmetricbnb` and `pmtmetricoffbeam`

--- sbndcode/Trigger/PMTSoftware/MetricAnalyzer_module.cc
+++ sbndcode/Trigger/PMTSoftware/MetricAnalyzer_module.cc
@@ -69,12 +69,10 @@ sbndaq::MetricAnalyzer::MetricAnalyzer(fhicl::ParameterSet const& p)
   // More initializers here.
 {
   fmetric_module_labels = p.get<std::vector<std::string>>("metric_module_labels",{"pmtmetricproducer",
-                                                                                  "pmtmetricbnbzero",
-                                                                                  "pmtmetricbnblight",
-                                                                                  "pmtmetricoffbeamzero",
-                                                                                  "pmtmetricoffbeamlight",
-                                                                                  "pmtmetriccrossingsmuon"});
-  fstream_labels = p.get<std::vector<int>>("stream_labels",{0,1,2,3,4,5});
+                                                                                  "pmtmetricbnb",
+                                                                                  "pmtmetricoffbeam",
+                                                                                  "pmtmetriccrossingmuon"});
+  fstream_labels = p.get<std::vector<int>>("stream_labels",{0,1,2,3});
   fmetric_instance_name = p.get<std::string>("metric_instance_name","");
   fspectdc_module_label = p.get<std::string>("spectdc_module_name","tdcdecoder");
   fspectdc_etrig_ch = p.get<uint32_t>("spectdc_etrig_ch",4);
