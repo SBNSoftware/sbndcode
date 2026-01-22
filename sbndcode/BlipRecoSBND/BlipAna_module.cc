6a7d42f7beb7a7dad632c097bb0057a4a6ea1809
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2026-01-22 16:03:00 -0600

    debug

--- sbndcode/BlipRecoSBND/BlipAna_module.cc
+++ sbndcode/BlipRecoSBND/BlipAna_module.cc
@@ -1582,6 +1582,8 @@ void BlipAna::PrintClusterInfo(const blip::HitClust& hc){
     hc.EdepID,
     hc.isMatched
   );
+  printf("G4 IDs contib");
+  for(g4ID : hc.G4IDs) printf("%i", g4ID);
 }
 
 void BlipAna::PrintBlipInfo(const blip::Blip& bl){
