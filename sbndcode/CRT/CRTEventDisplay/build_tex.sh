380b27fcfb124f16d2d01df7dec8bb1c7fdf0778
Henry Lay <h.lay@sheffield.ac.uk>
2026-02-20 10:41:52 -0600

    Make tex script more configurable

--- sbndcode/CRT/CRTEventDisplay/build_tex.sh
+++ sbndcode/CRT/CRTEventDisplay/build_tex.sh
@@ -1,18 +1,26 @@
+version=$1
+gdml=$2
+author=$3
+email=$4
+edition=$5
+
+mkdir /exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/tex_work
+
 echo "\documentclass{article}
 \usepackage[a4paper, margin=3cm]{geometry}
 \usepackage{graphicx}
 \usepackage{pgffor}
 \usepackage[hidelinks]{hyperref}
 
-\title{SBND CRT Channel Mapping Displays \\\\ \vspace{1em} \small \textit{Produced using} \texttt{sbndcode v10\_04\_01} \textit{\&} \texttt{sbnd\_v02\_05.gdml}}
-\author{Henry Lay \\\\ \small h.lay@sheffield.ac.uk}" > crt_channel_mapping_evds.tex
+\title{SBND CRT Channel Mapping Displays \\\\ \vspace{1em} \small \textit{Produced using} \texttt{sbndcode ${version}} \textit{\&} \texttt{${gdml}}}
+\author{${author} \\\\ \small ${email}}" > crt_channel_mapping_evds.tex
 
 walls=(bottom south north west east toplow tophigh)
 wallnames=(Bottom South North West East "Top Low" "Top High")
 
 for wall in "${walls[@]}"
 do
-    list=$(ls /exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/${wall}_wall/*_front.pdf)
+    list=$(ls /exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/${wall}_wall/*_front.pdf)
     echo -n "\newcommand*{\\"$wall"ids}{" >> crt_channel_mapping_evds.tex
 
     for item in ${list}
@@ -33,10 +41,10 @@ echo "\begin{document}
 \centering
 \vspace{2em}
 
-\includegraphics[width=.6\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/UOSLogo_Primary_Violet_RGB.png}
+\includegraphics[width=.6\textwidth]{/nashome/h/hlay/UOSLogo_Primary_Violet_RGB.png}
 \vspace{2em}
 
-\includegraphics[width=.5\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/sbnd_pride_transparent.png}
+\includegraphics[width=.5\textwidth]{/nashome/h/hlay/sbnd_pride_transparent.png}
 \flushleft
 \newpage
 \tableofcontents
@@ -55,9 +63,9 @@ do
     \newpage
     \subsection{volCRTModule\x\_\x}
     \begin{center}
-            \includegraphics[width=.85\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/${walls[i]}_wall/volCRTModule\x_\x_front.pdf}\\\\
-            \includegraphics[width=.85\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/${walls[i]}_wall/volCRTModule\x_\x_top.pdf}\\\\
-            \includegraphics[width=.85\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/${walls[i]}_wall/volCRTModule\x_\x_side.pdf}
+            \includegraphics[width=.85\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/${walls[i]}_wall/volCRTModule\x_\x_front.pdf}\\\\
+            \includegraphics[width=.85\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/${walls[i]}_wall/volCRTModule\x_\x_top.pdf}\\\\
+            \includegraphics[width=.85\textwidth]{/exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/${walls[i]}_wall/volCRTModule\x_\x_side.pdf}
     \end{center}
 }
 \endgroup" >> crt_channel_mapping_evds.tex
@@ -65,5 +73,5 @@ done
 
 echo "\end{document}" >> crt_channel_mapping_evds.tex
 
-pdflatex --shell-escape -output-directory /exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/tex_work crt_channel_mapping_evds.tex
-pdflatex --shell-escape -output-directory /exp/sbnd/data/users/hlay/crt_channel_mapping/mar2025/tex_work crt_channel_mapping_evds.tex
+pdflatex --shell-escape -output-directory /exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/tex_work crt_channel_mapping_evds.tex
+pdflatex --shell-escape -output-directory /exp/sbnd/data/users/hlay/crt_channel_mapping/${edition}/tex_work crt_channel_mapping_evds.tex
