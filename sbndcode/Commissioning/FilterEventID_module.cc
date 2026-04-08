64aaa0c537ab162cb2febd0b0514d427a8679634
lynnt20 <lynnt@uchicago.edu>
2026-04-08 14:44:50 -0500

    add runlist

--- sbndcode/Commissioning/FilterEventID_module.cc
+++ sbndcode/Commissioning/FilterEventID_module.cc
@@ -1,31 +1,35 @@
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-#include <vector>
-#include <numeric>
-#include <getopt.h>
-#include <chrono>
-#include <float.h>
+// #include <iostream>
+// #include <stdlib.h>
+// #include <string>
+// #include <vector>
+// #include <numeric>
+// #include <getopt.h>
+// #include <chrono>
+// #include <float.h>
 
 
 //some ROOT includes
-#include "TInterpreter.h"
-#include "TROOT.h"
-#include "TH1F.h"
-#include "TH2D.h"
-#include "TCanvas.h"
-#include "TTree.h"
-#include "TFile.h"
-#include "TStyle.h"
-#include "TSystem.h"
-#include "TGraph.h"
-#include "TFFTReal.h"
-#include "TTimeStamp.h"
-#include "TLatex.h"
-#include "TLine.h"
+// #include "TInterpreter.h"
+// #include "TROOT.h"
+// #include "TH1F.h"
+// #include "TH2D.h"
+// #include "TCanvas.h"
+// #include "TTree.h"
+// #include "TFile.h"
+// #include "TStyle.h"
+// #include "TSystem.h"
+// #include "TGraph.h"
+// #include "TFFTReal.h"
+// #include "TTimeStamp.h"
+// #include "TLatex.h"
+// #include "TLine.h"
 
 
 // art includes
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
 #include "canvas/Utilities/InputTag.h"
 #include "canvas/Persistency/Common/FindOneP.h"
 #include "canvas/Persistency/Common/FindManyP.h"
@@ -37,17 +41,17 @@
 #include "art/Framework/Principal/Run.h" 
 #include "art/Framework/Principal/SubRun.h" 
 #include "fhiclcpp/ParameterSet.h" 
-#include "messagefacility/MessageLogger/MessageLogger.h"  
-#include "lardataobj/RecoBase/Hit.h"
-#include "lardataobj/RawData/RawDigit.h"
-#include "lardataobj/RawData/RDTimeStamp.h"
-#include "lardataobj/RawData/raw.h"
-#include "larcore/Geometry/Geometry.h"
-#include "canvas/Persistency/Provenance/Timestamp.h"
+// #include "messagefacility/MessageLogger/MessageLogger.h"  
+// #include "lardataobj/RecoBase/Hit.h"
+// #include "lardataobj/RawData/RawDigit.h"
+// #include "lardataobj/RawData/RDTimeStamp.h"
+// #include "lardataobj/RawData/raw.h"
+// #include "larcore/Geometry/Geometry.h"
+// #include "canvas/Persistency/Provenance/Timestamp.h"
 
-#include "art_root_io/TFileService.h"
+// #include "art_root_io/TFileService.h"
 
-#include "sbndcode/ChannelMaps/TPC/TPCChannelMapService.h"
+// #include "sbndcode/ChannelMaps/TPC/TPCChannelMapService.h"
 
 
 namespace filt{
@@ -66,6 +70,7 @@ namespace filt{
       int subrun;
       int event;
       std::vector<int> filter_evts;
+      std::vector<int> filter_runs;
     
   };
 
@@ -75,6 +80,11 @@ namespace filt{
   {
     this->reconfigure(pset);
     filter_evts = pset.get<std::vector<int> >("filterevts");
+    filter_runs = pset.get<std::vector<int> >("filterruns", std::vector<int>{});
+
+    if (filter_runs.size() != filter_evts.size()) {
+      throw std::runtime_error("FilterEventID requires filterruns and filterevts to have the same length");
+    }
   }
 
   void FilterEventID::reconfigure(fhicl::ParameterSet const& pset){
@@ -87,8 +97,14 @@ namespace filt{
     subrun = evt.id().subRun();
     event = evt.id().event();
 
-    bool is_in_list = std::find(filter_evts.begin(), filter_evts.end(), event) != filter_evts.end();
-    std::cout << "event: " << event << " is_in_list: " << is_in_list << std::endl;
+    bool is_in_list = false;
+    for (std::size_t index = 0; index < filter_evts.size(); ++index) {
+      if (filter_runs[index] == run && filter_evts[index] == event) {
+        is_in_list = true;
+        break;
+      }
+    }
+    std::cout << "run: " << run << " event: " << event << " is_in_list: " << is_in_list << std::endl;
     return is_in_list;
     
   }
