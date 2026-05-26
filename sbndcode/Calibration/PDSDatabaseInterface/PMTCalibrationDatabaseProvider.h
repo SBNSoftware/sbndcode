708e979dfd0b85527e256e3bdbc23e3e461ed0f0
Thomas Wester <twester@uchicago.edu>
2026-05-26 13:38:44 -0500

    add production fhicl

--- sbndcode/Calibration/PDSDatabaseInterface/PMTCalibrationDatabaseProvider.h
+++ sbndcode/Calibration/PDSDatabaseInterface/PMTCalibrationDatabaseProvider.h
@@ -153,7 +153,6 @@ class sbndDB::PMTCalibrationDatabaseProvider : public PMTCalibrationDatabase {
         };
             
         const PMTCalibrationDB CorrectionDefaults = {0, 0, 0, true, false, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, {}};
-        // const PMTCalibrationDBScales ScaleDefaults = {true, true, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, {}};
 
 	    /// Map of corrections by channel
         std::map<unsigned int, PMTCalibrationDB> fPMTCalibrationData;
