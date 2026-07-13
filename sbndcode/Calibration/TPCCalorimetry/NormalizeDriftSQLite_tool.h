fedb0a844517843421d1fd7e81b68ffa272d5c10
Jacob McLaughlin <jmclaugh@sbndgpvm07.fnal.gov>
2026-07-13 14:58:03 -0500

    Think this is a slightly nicer implementation. Will check if it runs

--- sbndcode/Calibration/TPCCalorimetry/NormalizeDriftSQLite_tool.h
+++ sbndcode/Calibration/TPCCalorimetry/NormalizeDriftSQLite_tool.h
@@ -1,5 +1,5 @@
 #ifndef NORMALIZEDRIFTSQLITE_TOOL_H
 #define NORMALIZEDRIFTSQLITE_TOOL_H
 #include "sbndcode/Calibration/TPCCalorimetry/NormalizeDriftSQLite_Definitions.h"
-sbnd::calo::DEFINE_ART_CLASS_TOOL(NormalizeDriftSQLite)
+DEFINE_ART_CLASS_TOOL(sbnd::calo::NormalizeDriftSQLite)
 #endif
