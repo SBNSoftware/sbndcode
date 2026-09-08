ef4781d8b448be0bc00c98996512d6cad88b983b
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2026-09-08 12:29:51 -0500

    just throwing ideas at wall now

--- sbndcode/FragmentBlender/ArtDAQFragmentBlender_module.cc
+++ sbndcode/FragmentBlender/ArtDAQFragmentBlender_module.cc
@@ -26,9 +26,9 @@
 #include "lardataobj/RawData/RawDigit.h"
 #include "sbndcode/Decoders/TPC/SBNDTPCDecoder.h"
 #include "sbndcode/Decoders/TPC/sbndntb.h"
-//#include "sbndaq-artdaq-core/Overlays/SBND/NevisTPCFragment.hh"
-//#include "sbndaq-artdaq-core/Overlays/SBND/NevisTPC/NevisTPCTypes.hh"
-//#include "sbndaq-artdaq-core/Overlays/SBND/NevisTPC/NevisTPCUtilities.hh"
+#include "sbndaq-artdaq-core/Overlays/SBND/NevisTPCFragment.hh"
+#include "sbndaq-artdaq-core/Overlays/SBND/NevisTPC/NevisTPCTypes.hh"
+#include "sbndaq-artdaq-core/Overlays/SBND/NevisTPC/NevisTPCUtilities.hh"
 
 
 #include <memory>
@@ -52,7 +52,7 @@ public:
   void produce(art::Event& e) override;
 
 private:
-  daq::SBNDTPCDecoder *tpcDecoderBusiness;
+  //daq::SBNDTPCDecoder *tpcDecoderBusiness;
   std::string fNoiseFileList;// Declare member data here.
   std::string fTPCDAQLabel;
   int fNumberNoiseFiles;
@@ -66,8 +66,8 @@ ArtDAQFragmentBlender::~ArtDAQFragmentBlender()
 }
 
 ArtDAQFragmentBlender::ArtDAQFragmentBlender(fhicl::ParameterSet const& p)
-  : EDProducer{p}, 
-  tpcDecoderBusiness{p}  // More initializers here.
+  : EDProducer{p}//, 
+  //tpcDecoderBusiness{p}  // More initializers here.
 {
   //tpcDecoderBusiness = new daq::SBNDTPCDecoder(p);
   fNoiseFileList = p.get<std::string>("NoiseFileList");
@@ -104,13 +104,13 @@ void ArtDAQFragmentBlender::produce(art::Event& e)
   art::PtrMaker<raw::RawDigit> rdpm(e);
   art::PtrMaker<raw::RDTimeStamp> tspm(e);
   std::unique_ptr<std::vector<raw::RawDigit>> ScrambledFragments(new std::vector<raw::RawDigit>);
-  std::unique_ptr<std::vector<raw::RawDigit>> NominalFragments = tpcDecoderBusiness->produce2(*NominalFragHandle, rdpm, tspm);
-  art::InputTag TempTag(fTPCDAQLabel);
-  const auto& NoiseTPCfragmentList = *(noiseGalleryEvent->getValidHandle< std::vector<artdaq::Fragment> >(TempTag));
-  std::unique_ptr<std::vector<raw::RawDigit>> NoiseFragments = tpcDecoderBusiness->produce2(NoiseTPCfragmentList, rdpm, tspm);
+  //std::unique_ptr<std::vector<raw::RawDigit>> NominalFragments = tpcDecoderBusiness->produce2(*NominalFragHandle, rdpm, tspm);
+  //art::InputTag TempTag(fTPCDAQLabel);
+  //const auto& NoiseTPCfragmentList = *(noiseGalleryEvent->getValidHandle< std::vector<artdaq::Fragment> >(TempTag));
+  //std::unique_ptr<std::vector<raw::RawDigit>> NoiseFragments = tpcDecoderBusiness->produce2(NoiseTPCfragmentList, rdpm, tspm);
   //Loop through fragments and find the right IDs to add in 
   //Should update this logic to use ranges of channel IDs
-  
+  /*
   for(int i=0; i<int(NominalFragments->size()); i++)
   {
     int ChannelID = (*NominalFragments)[i].Channel();
@@ -123,7 +123,7 @@ void ArtDAQFragmentBlender::produce(art::Event& e)
       ScrambledFragments->push_back((*NoiseFragments)[i]);
     }
   }
-
+  */
   std::cout << "Scrambled size " << ScrambledFragments->size() << std::endl;
   //Add the new collection to the event
   std::cout << " adding in scrambled fragments " << std::endl;
